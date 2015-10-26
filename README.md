66b60672bec390eb5e05b646d5ce2b06a671328b
LuchenTan <tan.luchen@gmail.com>
2015-10-26 15:32:18 -0400

    Add -docIdName filed

--- README.md
+++ README.md
@@ -117,6 +117,6 @@ Output all the document IDs in a Lucene Index.
 
 ```sh
 sh target/appassembler/bin/DumpDocids -indexPath /path/to/index \
--docIdPath /path/to/save/docIds
+-docIdPath /path/to/save/docIds -docIdName "name of docID field"
 ```
 =======
--- src/main/java/io/anserini/util/DumpDocids.java
+++ src/main/java/io/anserini/util/DumpDocids.java
@@ -5,59 +5,83 @@ import java.io.IOException;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.store.FSDirectory;
+
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
-import org.kohsuke.args4j.OptionHandlerFilter;
+import org.kohsuke.args4j.Option;
 import org.kohsuke.args4j.ParserProperties;
 
 public class DumpDocids {
+  private static final Logger LOG = LogManager.getLogger(DumpDocids.class);
 
-  public void readIndex(String indexDir, String docIdPath) throws IOException {
+  public static class Args {
+    @Option(name = "-indexPath", metaVar = "[Path]", required = true, usage = "Directory contains index files")
+    String indexPath;
+
+    @Option(name = "-docIdPath", metaVar = "[Path]", required = true, usage = "Path of dumped document ID list")
+    String docIdPath;
+
+    @Option(name = "-docIdName", metaVar = "[String]", required = false, usage = "Field Name of document ID")
+    String docId = "docname";
+  }
+
+  public class IDNameException extends Exception {
+	
+
+	public IDNameException(String message) {
+		  super(message);
+	  }
+  }
+		  
+  public void readIndex(String indexDir, String docIdPath, String docId) throws IOException, IDNameException {
     FSDirectory dir = FSDirectory.open(new File(indexDir).toPath());
     DirectoryReader reader = DirectoryReader.open(dir);
 
     FileWriter fw = new FileWriter(new File(docIdPath));
     BufferedWriter bw = new BufferedWriter(fw);
     int len = reader.numDocs();
+    if (len > 0) {
+    	String docName = reader.document(0).get(docId);
+    	if (docName == null) {
+    		LOG.info(docId + " is a wrong document ID field name!");
+    		bw.close();
+    		throw new IDNameException(docId + " is a wrong document ID field name!");
+    	}
+    }
+    else {
+    	bw.close();
+    	throw new IDNameException("No document is in the index!");
+    }
     for (int i = 0; i < len; i++) {
-      String docName = reader.document(i).get("docname");
+      String docName = reader.document(i).get(docId);
       bw.write(docName + "\n");
-      // System.out.println("IndexCounter: " + i + " docs got");
-      if ((i & 65535) == 0) {
-        System.out.println("IndexCounter: " + i + " docs got");
+      if ((i % 100000) == 0) {
+        LOG.info("DumpDocids: " + i + " docs got");
       }
-      // System.out.println(docName);
-
     }
     bw.close();
   }
 
-  public static void main(String[] clArgs) {
+  public static void main(String[] clArgs) throws IOException, IDNameException{
     Args indexArgs = new Args();
-
     CmdLineParser parser = new CmdLineParser(indexArgs, ParserProperties
         .defaults().withUsageWidth(90));
-
     try {
       parser.parseArgument(clArgs);
     } catch (CmdLineException e) {
-      System.err.println(e.getMessage());
+      LOG.error(e.getMessage());
       parser.printUsage(System.err);
       return;
     }
-
-    final String indexDir = indexArgs.indexPath + "/index";
-    final String docIdPath = indexArgs.docIdPath;
-
-    System.out.println("Index path: " + indexDir);
-    System.out.println("DocId path: " + docIdPath);
     final DumpDocids ic = new DumpDocids();
     try {
-      ic.readIndex(indexDir, docIdPath);
-    } catch (IOException e) {
-      e.printStackTrace();
+      ic.readIndex(indexArgs.indexPath, indexArgs.docIdPath, indexArgs.docId);
+    } catch (Exception e) {
+      LOG.error(e.getMessage());
     }
   }
 }
