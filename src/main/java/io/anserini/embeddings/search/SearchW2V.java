79cc8caa914658d01621797d3f481abf1b4a7616
rdsequie <rdsequie@scspc631.cs.uwaterloo.ca>
2016-12-08 14:52:47 -0500

    fixing indentation

--- src/main/java/io/anserini/embeddings/search/SearchW2V.java
+++ src/main/java/io/anserini/embeddings/search/SearchW2V.java
@@ -2,69 +2,92 @@ package io.anserini.embeddings.search;
 
 import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
 import org.deeplearning4j.models.embeddings.wordvectors.WordVectors;
+
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.OptionHandlerFilter;
 import org.kohsuke.args4j.ParserProperties;
+import org.kohsuke.args4j.Option;
 
 import java.io.*;
 import java.util.Arrays;
 import java.util.Collection;
 
 public class SearchW2V {
-    public static void main(String[] args) {
-        SearchW2V_Args searchW2VArgs = new SearchW2V_Args();
-        CmdLineParser parser = new CmdLineParser(searchW2VArgs, ParserProperties.defaults().withUsageWidth(90));
+  public static class SearchW2V_Args {
+    @Option(name = "-model", metaVar = "[file]", required = true, usage = "path to model file")
+    public String model;
 
-        try {
-            parser.parseArgument(args);
-        } catch (CmdLineException e) {
-            System.err.println(e.getMessage());
-            parser.printUsage(System.err);
-            System.err.println("Example: " + SearchW2V.class.getSimpleName() + parser.printExample(OptionHandlerFilter.REQUIRED));
-            return;
-        }
+    //optional arguments
+    @Option(name = "-nearest", metaVar = "[Number]", usage = "number of nearest words")
+    public int nearest = 10;
 
-        File model = new File(searchW2VArgs.model);
-        WordVectors vec = null;
-        try {
-            if (searchW2VArgs.gModel) {
-                vec = WordVectorSerializer.loadGoogleModel(model, true, true);
-            } else {
-                vec = WordVectorSerializer.readWord2Vec(model);
-            }
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
-        }
+    @Option(name = "-term", metaVar = "[String]", usage = "input word")
+    public String term = "";
 
-        if (searchW2VArgs.term.isEmpty() && searchW2VArgs.input_file.isEmpty()) {
-            System.err.println("You should enter either a word or a file to be searched");
-        } else if(!searchW2VArgs.term.isEmpty() && !searchW2VArgs.input_file.isEmpty()){
-            System.err.println("You should search for either a term or a file, not both");
-        } else{
-            if(!searchW2VArgs.term.isEmpty()){
-                Collection<String> near_words = vec.wordsNearest(searchW2VArgs.term, searchW2VArgs.nearest);
-                for (String word : near_words){
-                    System.out.println(word + " " + vec.similarity(word, searchW2VArgs.term));
-                }
-            }
-            else{
-                try (BufferedReader br = new BufferedReader(new FileReader(searchW2VArgs.input_file))) {
-                    String line;
-                    PrintWriter bf = new PrintWriter(new FileWriter(searchW2VArgs.output_file));
-                    while ((line = br.readLine()) != null) {
-                        String term = line.trim();
-                        Collection<String> near_words = vec.wordsNearest(term, searchW2VArgs.nearest);
-                        for(String word: near_words) {
-                            bf.println(term  + "\t" + word + "\t" + vec.similarity(word, term) + "\t" + Arrays.toString(vec.getWordVector(word)));
-                        }
-                    }
-                } catch (IOException e){
-                    System.err.println(e.getMessage());
-                }
-            }
+    @Option(name = "-input", metaVar = "[file]", usage = "input file with one word per line")
+    public String input_file = "";
 
-        }
+    @Option(name = "-gmodel", usage = "specify if the model was trained in Google format")
+    public Boolean gModel = false;
 
+    @Option(name = "-output", metaVar = "[file]", usage = "path of the output file")
+    public String output_file = "";
+  }
+
+  public static void main(String[] args) {
+    final SearchW2V_Args searchW2VArgs = new SearchW2V_Args();
+    CmdLineParser parser = new CmdLineParser(searchW2VArgs, ParserProperties.defaults().withUsageWidth(90));
+
+    try {
+      parser.parseArgument(args);
+    } catch (CmdLineException e) {
+      System.err.println(e.getMessage());
+      parser.printUsage(System.err);
+      System.err.println("Example: " + SearchW2V.class.getSimpleName() + parser.printExample(OptionHandlerFilter.REQUIRED));
+      return;
     }
+
+    File model = new File(searchW2VArgs.model);
+    WordVectors vec = null;
+
+    try {
+      if (searchW2VArgs.gModel) {
+        vec = WordVectorSerializer.loadGoogleModel(model, true, true);
+      } else {
+        vec = WordVectorSerializer.readWord2Vec(model);
+      }
+    } catch (IOException e) {
+      System.err.println(e.getMessage());
+    }
+
+    if (searchW2VArgs.term.isEmpty() && searchW2VArgs.input_file.isEmpty()) {
+      System.err.println("You should enter either a word or a file to be searched");
+    } else if(!searchW2VArgs.term.isEmpty() && !searchW2VArgs.input_file.isEmpty()){
+      System.err.println("You should search for either a term or a file, not both");
+    } else {
+
+      if(!searchW2VArgs.term.isEmpty()){
+        Collection<String> near_words = vec.wordsNearest(searchW2VArgs.term, searchW2VArgs.nearest);
+        for (String word : near_words){
+          System.out.println(word + " " + vec.similarity(word, searchW2VArgs.term));
+        }
+      } else {
+        try (BufferedReader br = new BufferedReader(new FileReader(searchW2VArgs.input_file))) {
+          String line;
+          PrintWriter bf = new PrintWriter(new FileWriter(searchW2VArgs.output_file));
+
+          while ((line = br.readLine()) != null) {
+            String term = line.trim();
+            Collection<String> near_words = vec.wordsNearest(term, searchW2VArgs.nearest);
+            for(String word: near_words) {
+              bf.println(term  + "\t" + word + "\t" + vec.similarity(word, term) + "\t" + Arrays.toString(vec.getWordVector(word)));
+            }
+          }
+        } catch (IOException e){
+          System.err.println(e.getMessage());
+        }
+      }
+    }
+  }
 }
--- src/main/java/io/anserini/embeddings/train/TrainW2V.java
+++ src/main/java/io/anserini/embeddings/train/TrainW2V.java
@@ -7,57 +7,83 @@ import org.deeplearning4j.text.sentenceiterator.SentenceIterator;
 import org.deeplearning4j.text.tokenization.tokenizer.preprocessor.CommonPreprocessor;
 import org.deeplearning4j.text.tokenization.tokenizerfactory.DefaultTokenizerFactory;
 import org.deeplearning4j.text.tokenization.tokenizerfactory.TokenizerFactory;
+
+import org.kohsuke.args4j.Option;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.OptionHandlerFilter;
 import org.kohsuke.args4j.ParserProperties;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TrainW2V {
+  public static class TrainArgs {
+    @Option(name = "-input", metaVar = "[file]", required = true, usage = "raw file")
+    public String input;
 
-    private static Logger log = LoggerFactory.getLogger(TrainW2V.class);
+    @Option(name = "-output", metaVar = "[file]", required = true, usage = "path of the output file")
+    public String output;
 
-    public static void main(String[] args) throws Exception {
-        TrainArgs trainArgs = new TrainArgs();
-        CmdLineParser parser = new CmdLineParser(trainArgs, ParserProperties.defaults().withUsageWidth(90));
+    //optional arguments
+    @Option(name = "-window", metaVar = "[Number]",  usage = "window size")
+    public int window = 5;
 
-        try {
-            parser.parseArgument(args);
-        } catch (CmdLineException e) {
-            System.err.println(e.getMessage());
-            parser.printUsage(System.err);
-            System.err.println("Example: "+ TrainW2V.class.getSimpleName() + parser.printExample(OptionHandlerFilter.REQUIRED));
-            return;
-        }
+    @Option(name = "-iter", metaVar = "[Number]",  usage = "number of iterations")
+    public int iter = 1;
 
+    @Option(name = "-dimension", metaVar = "[Number]",  usage = "dimension size")
+    public int dimension = 100;
 
-        log.info("Load & Vectorize Sentences....");
-        // Strip white space before and after for each line
-        SentenceIterator sent_iter = new BasicLineIterator(trainArgs.input);
+    @Option(name = "-minfreq", metaVar = "[Number]", usage = "minimum term frequency")
+    public int min_word_freq = 5;
 
-         // Use the Lucene Standard Tokenizer here
-        TokenizerFactory t = new DefaultTokenizerFactory();
-        t.setTokenPreProcessor(new CommonPreprocessor());
+    @Option(name = "-seed", metaVar = "[Number]", usage = "seed size")
+    public int seed = 42;
 
-        log.info("Building model....");
-        Word2Vec vec = new Word2Vec.Builder()
-                .minWordFrequency(trainArgs.min_word_freq)
-                .iterations(trainArgs.iter)
-                .layerSize(trainArgs.dimension)
-                .seed(trainArgs.seed)
-                .windowSize(trainArgs.window)
-                .iterate(sent_iter)
-                .tokenizerFactory(t)
-                .build();
+  }
 
-        log.info("Fitting Word2Vec model....");
-        vec.fit();
+  private static Logger log = LoggerFactory.getLogger(TrainW2V.class);
+  public static void main(String[] args) throws Exception {
+    TrainArgs trainArgs = new TrainArgs();
+    final CmdLineParser parser = new CmdLineParser(trainArgs, ParserProperties.defaults().withUsageWidth(90));
 
-        log.info("Writing word vectors to text file....");
-
-        // Write word vectors
-        WordVectorSerializer.writeWord2Vec(vec, trainArgs.output);
-
+    try {
+      parser.parseArgument(args);
+    } catch (CmdLineException e) {
+      System.err.println(e.getMessage());
+      parser.printUsage(System.err);
+      System.err.println("Example: "+ TrainW2V.class.getSimpleName() + parser.printExample(OptionHandlerFilter.REQUIRED));
+      return;
     }
+
+
+    log.info("Load & Vectorize Sentences....");
+    // Strip white space before and after for each line
+    SentenceIterator sent_iter = new BasicLineIterator(trainArgs.input);
+
+    // Use the Lucene Standard Tokenizer here
+    TokenizerFactory t = new DefaultTokenizerFactory();
+    t.setTokenPreProcessor(new CommonPreprocessor());
+
+    log.info("Building model....");
+    Word2Vec vec = new Word2Vec.Builder()
+        .minWordFrequency(trainArgs.min_word_freq)
+        .iterations(trainArgs.iter)
+        .layerSize(trainArgs.dimension)
+        .seed(trainArgs.seed)
+        .windowSize(trainArgs.window)
+        .iterate(sent_iter)
+        .tokenizerFactory(t)
+        .build();
+
+    log.info("Fitting Word2Vec model....");
+    vec.fit();
+
+    log.info("Writing word vectors to text file....");
+
+    // Write word vectors
+    WordVectorSerializer.writeWord2Vec(vec, trainArgs.output);
+
+  }
 }
