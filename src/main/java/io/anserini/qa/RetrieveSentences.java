0a9bc05e77c5e058fb8a026d2c6f55e4a07e344e
rdsequie <rdsequie@uwaterloo.ca>
2017-03-12 18:36:51 -0400

    wmd fixes

--- src/main/java/io/anserini/qa/RetrieveSentences.java
+++ src/main/java/io/anserini/qa/RetrieveSentences.java
@@ -1,3 +1,18 @@
+/**
+ * Anserini: An information retrieval toolkit built on Lucene
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.anserini.qa;
 
 import edu.stanford.nlp.simple.Sentence;
@@ -56,7 +71,7 @@ public class RetrieveSentences {
     public int hits = 100;
 
     //Todo: add more passage scorer
-    @Option(name = "-scorer", metaVar = "[Idf]", usage = "passage scores")
+    @Option(name = "-scorer", metaVar = "[Idf|Wmd]", usage = "passage scores")
     public String scorer;
 
     @Option(name = "-k", metaVar = "[number]", usage = "top-k passages to be retrieved")
