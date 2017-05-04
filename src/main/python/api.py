de5672ac103de146cd2c64b0b8db41d90231066e
rdsequie <rdsequie@uwaterloo.ca>
2017-05-04 12:44:19 -0400

    updated demo

--- src/main/python/api.py
+++ src/main/python/api.py
@@ -6,15 +6,10 @@ import sys
 from flask import Flask, jsonify, request
 # FIXME: separate this out to a classifier class where we can switch out the models
 from pyserini import Pyserini
-from sm_model.bridge import SMModelBridge
-
-path_to_castorini = os.getcwd() + "/.."
-model = SMModelBridge(path_to_castorini + '/models/sm_model/sm_model.TrecQA.TRAIN-ALL.2017-04-02.castor',
-                        path_to_castorini + '/data/word2vec/aquaint+wiki.txt.gz.ndim=50.cache',
-                        path_to_castorini + '/data/TrecQA/stopwords.txt',
-                        path_to_castorini + '/data/TrecQA/word2dfs.p')
+from sm_cnn.bridge import SMModelBridge
 
 app = Flask(__name__)
+
 
 @app.route("/", methods=['GET'])
 def hello():
@@ -33,6 +28,7 @@ def answer():
         answer_dict = {"answers": answers}
         return jsonify(answer_dict)
     except Exception as e:
+        print(e)
         error_dict = {"error": "ERROR - could not parse the question or get answer. "}
         return jsonify(error_dict)
 
@@ -44,23 +40,40 @@ def wit_ai_config():
 def get_answers(question, num_hits, k):
     pyserini = Pyserini(app.config['Flask']['index'])
     candidate_passages_scores = pyserini.ranked_passages(question, num_hits, k)
-    candidate_passages = []
+
+    candidate_sent_scores = []
+    candidate_passages_sm = []
 
     for ps in candidate_passages_scores:
         ps_split = ps.split('\t')
-        candidate_passages.append(ps_split[0])
+        candidate_passages_sm.append(ps_split[0])
+        candidate_sent_scores.append((float(ps_split[1]), ps_split[0]))
 
-    answers_list = model.rerank_candidate_answers(question, candidate_passages)
+    if app.config['Flask']['model'] == "sm":
+        path_to_castorini = os.getcwd() + "/.."
+        model = SMModelBridge(path_to_castorini + '/models/sm_model/sm_model.fixed_ext_feats_paper.puncts_stay',
+                              path_to_castorini + '/data/word2vec/aquaint+wiki.txt.gz.ndim=50.cache',
+                              app.config['Flask']['index'])
+        idf_json = pyserini.get_term_idf_json()
+        answers_list = model.rerank_candidate_answers(question, candidate_passages_sm, idf_json)
+        sorted_answers = sorted(answers_list, key=lambda x: x[0], reverse=True)
+    else:
+        # the re-ranking model chosen is idf
+        sorted_answers = list(candidate_sent_scores)
+
+    print("in idf:{}".format(sorted_answers))
     answers = []
-
-    for score, sent in answers_list:
+    for score, sent in sorted_answers:
+        print(sent)
         answers.append({'passage': sent, 'score': score})
+
     return answers
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Start the Flask API at the specified host, port')
     parser.add_argument('--config', help='config to use', required=False, type=str, default='config.cfg')
     parser.add_argument("--debug", help="print debug info", action="store_true")
+    parser.add_argument("--model", help="[idf|sm]", default="idf")
     args = parser.parse_args()
 
     if not os.path.isfile(args.config):
@@ -78,10 +91,13 @@ if __name__ == "__main__":
         for key, value in config.items(name):
             app.config[name][key] = value
 
+    app.config['Flask']['model'] = args.model
+
     print("Config: {}".format(args.config))
     print("Index: {}".format(app.config['Flask']['index']))
     print("Host: {}".format(app.config['Flask']['host']))
     print("Port: {}".format(app.config['Flask']['port']))
+    print("Re-ranking Model: {}".format(app.config['Flask']['model']))
     print("Debug info: {}".format(args.debug))
 
     app.run(debug=args.debug, host=app.config['Flask']['host'], port=int(app.config['Flask']['port']))
