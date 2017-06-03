02fedc7f4dc1207448be28a9f9b5f4d012773784
Salman Mohammed <smohammed1993@gmail.com>
2017-06-03 18:54:50 -0400

    Freebase Topic index (#193)
    + added code to index topics
    + added an example lookup code (still needs to be fixed)
    + added docs

---
+++ src/main/java/io/anserini/kg/freebase/LookupTopic.java
@@ -0,0 +1,170 @@
+package io.anserini.kg.freebase;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.search.similarities.BM25Similarity;
+import org.apache.lucene.search.similarities.Similarity;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.analysis.core.SimpleAnalyzer;
+import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TopDocs;
+import io.anserini.rerank.ScoredDocuments;
+import org.kohsuke.args4j.*;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Lookups a Freebase mid and returns all properties associated with it.
+ */
+public class LookupTopic implements Closeable {
+  private static final Logger LOG = LogManager.getLogger(LookupTopic.class);
+
+  private final IndexReader reader;
+
+  static final class Args {
+    // Required arguments
+
+    @Option(name = "-index", metaVar = "[Path]", required = true, usage = "index path")
+    public String index;
+
+    @Option(name = "-query", metaVar = "[query]", required =  true, usage = "topic name to query for")
+    public String query;
+  }
+
+  private LookupTopic(String indexDir) throws IOException {
+    // Initialize index reader
+    LOG.info("Reading index from " + indexDir);
+
+    Path indexPath = Paths.get(indexDir);
+
+    if (!Files.exists(indexPath) || !Files.isDirectory(indexPath) || !Files.isReadable(indexPath)) {
+      throw new IllegalArgumentException(indexDir + " does not exist or is not a directory.");
+    }
+
+    this.reader = DirectoryReader.open(FSDirectory.open(indexPath));
+  }
+
+  @Override
+  public void close() throws IOException {
+    reader.close();
+  }
+
+  /**
+   * Prints query results to the standard output stream.
+   *
+   * @param queryName the entity name to search
+   * @throws Exception on error
+   */
+  public void search(String queryName) throws Exception {
+    LOG.info("Querying started...");
+
+    // Initialize index searcher
+    IndexSearcher searcher = new IndexSearcher(reader);
+    SimpleAnalyzer analyzer = new SimpleAnalyzer();
+    int numHits = 20;
+
+    // find exact title
+    QueryParser titleParser = new QueryParser(TopicLuceneDocumentGenerator.FIELD_TITLE, analyzer);
+    Query titleQuery = titleParser.parse(queryName);
+    TopDocs rs = searcher.search(titleQuery, numHits);
+    ScoredDocuments docs = ScoredDocuments.fromTopDocs(rs, searcher);
+
+    for (int i = 0; i < docs.documents.length; i++) {
+      String resultDoc = String.format("%d - SCORE: %f\nTOPIC_MID: %s\nWIKI_TITLE: %s\nW3_LABEL: %s\n\n",
+              (i + 1),
+              docs.scores[i],
+              docs.documents[i].getField(TopicLuceneDocumentGenerator.FIELD_TOPIC_MID).stringValue(),
+              docs.documents[i].getField(TopicLuceneDocumentGenerator.FIELD_TITLE).stringValue(),
+              docs.documents[i].getField(TopicLuceneDocumentGenerator.FIELD_LABEL).stringValue());
+      System.out.println(resultDoc);
+    }
+
+    if (docs.documents.length != 0) {
+      System.out.println("Exact WIKI_TITLE found! Ending search.");
+      return;
+    } else {
+      System.out.println("Exact WIKI_TITLE not found. Searching for the label...");
+    }
+    System.out.println();
+
+    // find exact label
+    QueryParser labelParser = new QueryParser(TopicLuceneDocumentGenerator.FIELD_LABEL, analyzer);
+    Query labelQuery = labelParser.parse(queryName);
+    rs = searcher.search(labelQuery, numHits);
+    docs = ScoredDocuments.fromTopDocs(rs, searcher);
+
+    for (int i = 0; i < docs.documents.length; i++) {
+      String resultDoc = String.format("%d - SCORE: %f\nTOPIC_MID: %s\nWIKI_TITLE: %s\nW3_LABEL: %s\n\n",
+              (i + 1),
+              docs.scores[i],
+              docs.documents[i].getField(TopicLuceneDocumentGenerator.FIELD_TOPIC_MID).stringValue(),
+              docs.documents[i].getField(TopicLuceneDocumentGenerator.FIELD_TITLE).stringValue(),
+              docs.documents[i].getField(TopicLuceneDocumentGenerator.FIELD_LABEL).stringValue());
+      System.out.println(resultDoc);
+    }
+
+    if (docs.documents.length != 0) {
+      System.out.println("Exact W3_LABEL found! Ending search.");
+      return;
+    } else {
+      System.out.println("Exact W3_LABEL not found. Ranking the topics using BM25 according the text/title/label...");
+    }
+    System.out.println();
+
+    float k1 = 1.5f;
+    float b = 0.75f;
+    Similarity similarity = new BM25Similarity(k1, b);
+    searcher.setSimilarity(similarity);
+    MultiFieldQueryParser queryParser = new MultiFieldQueryParser(
+            new String[]{ TopicLuceneDocumentGenerator.FIELD_TITLE,
+                    TopicLuceneDocumentGenerator.FIELD_LABEL,
+                    TopicLuceneDocumentGenerator.FIELD_TEXT },
+            analyzer);
+    queryParser.setDefaultOperator(QueryParser.Operator.OR);
+    Query query = queryParser.parse(queryName);
+
+    rs = searcher.search(query, numHits);
+    docs = ScoredDocuments.fromTopDocs(rs, searcher);
+
+    for (int i = 0; i < docs.documents.length; i++) {
+      String resultDoc = String.format("%d - SCORE: %f\nTOPIC_MID: %s\nWIKI_TITLE: %s\nW3_LABEL: %s\n",
+              (i + 1),
+              docs.scores[i],
+              docs.documents[i].getField(TopicLuceneDocumentGenerator.FIELD_TOPIC_MID).stringValue(),
+              docs.documents[i].getField(TopicLuceneDocumentGenerator.FIELD_TITLE).stringValue(),
+              docs.documents[i].getField(TopicLuceneDocumentGenerator.FIELD_LABEL).stringValue() );
+      System.out.println(resultDoc);
+    }
+
+    LOG.info("Querying completed.");
+  }
+
+  public static void main(String[] args) throws Exception {
+    Args searchArgs = new Args();
+
+    // Parse args
+    CmdLineParser parser = new CmdLineParser(searchArgs,
+            ParserProperties.defaults().withUsageWidth(90));
+
+    try {
+      parser.parseArgument(args);
+    } catch (CmdLineException e) {
+      System.err.println(e.getMessage());
+      parser.printUsage(System.err);
+      System.err.println("Example command: "+ LookupTopic.class.getSimpleName() +
+              parser.printExample(OptionHandlerFilter.REQUIRED));
+      return;
+    }
+
+    new LookupTopic(searchArgs.index).search(searchArgs.query);
+  }
+}
---
+++ src/main/java/io/anserini/kg/freebase/TopicLuceneDocumentGenerator.java
@@ -0,0 +1,123 @@
+package io.anserini.kg.freebase;
+
+import org.apache.lucene.document.*;
+import org.openrdf.model.Literal;
+import org.openrdf.model.ValueFactory;
+import org.openrdf.model.impl.SimpleValueFactory;
+import org.openrdf.rio.ntriples.NTriplesUtil;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Class that converts an {@link ObjectTriples} object into a Lucene document for indexing.
+ */
+public class TopicLuceneDocumentGenerator {
+  /**
+   * FreebaseTopicDocument has four fields:
+   * topicMid - the MID of the topic
+   * title - the object value of the (topicMid, http://rdf.freebase.com/key/wikipedia.en_title)
+   * label - title - the object value of the (topicMid, http://www.w3.org/2000/01/rdf-schema#label)
+   * text - all the values separated by space of the (topicMid, http://rdf.freebase.com/key/wikipedia.en)
+   */
+  public static final String FIELD_TOPIC_MID = "topicMid";
+  public static final String FIELD_TITLE = "title";
+  public static final String FIELD_LABEL = "label";
+  public static final String FIELD_TEXT = "text";
+
+  /**
+   * Predicates for which the literals should be stored
+   */
+  public static final String WIKI_EN_URI = "http://rdf.freebase.com/key/wikipedia.en";
+  public static final String WIKI_EN_TILE_URI = WIKI_EN_URI + "_title";
+  public static final String W3_LABEL_URI = "http://www.w3.org/2000/01/rdf-schema#label";
+
+  /**
+   * Simple value factory to parse literals using Sesame library.
+   */
+  private ValueFactory valueFactory = SimpleValueFactory.getInstance();
+
+  protected IndexTopics.Counters counters;
+  protected IndexTopics.Args args;
+
+  public void config(IndexTopics.Args args) {
+    this.args = args;
+  }
+
+  public void setCounters(IndexTopics.Counters counters) {
+    this.counters = counters;
+  }
+
+  public Document createDocument(ObjectTriples src) {
+    // make a Topic from the ObjectTriples
+    String topicMid = src.getSubject();
+    String title = "";
+    String label = "";
+    String text = "";
+    Map<String, List<String>> predicateValues = src.getPredicateValues();
+    for(Map.Entry<String, List<String>> entry: predicateValues.entrySet()) {
+      String predicate = entry.getKey();
+      List<String> objects = entry.getValue();
+      for (String object : objects) {
+        predicate = cleanUri(predicate);
+        if (predicate.startsWith(WIKI_EN_URI)) {
+          if (predicate.startsWith(WIKI_EN_TILE_URI)) {
+            title = removeQuotes(object);
+          } else {
+            // concatenate other variants with a space
+            text += removeQuotes(object) + " ";
+          }
+        } else if (predicate.startsWith(W3_LABEL_URI)) {
+          Literal parsedLiteral = NTriplesUtil.parseLiteral(object, valueFactory);
+          if (parsedLiteral.getLanguage().toString().equals("Optional[en]")) {
+            label = parsedLiteral.stringValue();
+          }
+        }
+      }
+    }
+
+    // Convert the triple doc to lucene doc
+    Document doc = new Document();
+
+    // Index subject as a StringField to allow searching
+    Field topicMidField = new StringField(FIELD_TOPIC_MID, cleanUri(topicMid), Field.Store.YES);
+    doc.add(topicMidField);
+
+    Field titleField = new TextField(FIELD_TITLE, title, Field.Store.YES);
+    doc.add(titleField);
+
+    Field labelField = new TextField(FIELD_LABEL, label, Field.Store.YES);
+    doc.add(labelField);
+
+    Field textField = new TextField(FIELD_TEXT, text, Field.Store.YES);
+    doc.add(textField);
+
+    src.clear();
+    return doc;
+  }
+
+  /**
+   * Removes '<', '>' if they exist, lower case
+   * <p>
+   * TODO - replace ':' with '_' because query parser doesn't like it
+   *
+   * @param uri
+   * @return
+   */
+  public static String cleanUri(String uri) {
+    if (uri.charAt(0) == '<')
+      return uri.substring(1, uri.length() - 1).toLowerCase();
+    else
+      return uri;
+  }
+
+  /**
+   * Removes quotes from the literal value in object field
+   */
+  private String removeQuotes(String literal) {
+    if (literal.charAt(0) == '\"' && literal.charAt(literal.length()-1) == '\"') {
+      return literal.substring(1, literal.length() - 1);
+    }
+    return literal;
+  }
+}
