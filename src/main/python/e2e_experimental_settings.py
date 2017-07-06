bba3f5c0daa22919d5c7d8e62a4bca262ebefc2d
rosequ <rdsequie@uwaterloo.ca>
2017-07-05 21:13:32 -0400

    consistent spacing

--- src/main/python/e2e_experimental_settings.py
+++ src/main/python/e2e_experimental_settings.py
@@ -5,11 +5,12 @@ import itertools
 import shlex
 import subprocess
 
+
 class Setting(object):
     def __init__(self, label, value_flag_map):
         self.label = label
         self.choice_flags = value_flag_map
-    
+
     def get_settings(self):
         return self.choice_flags.keys()
 
@@ -22,6 +23,7 @@ class Setting(object):
             options.append("{}:{}".format(self.label, key))
         return options
 
+
 class Experiments(object):
     def __init__(self, qa_data_xml, qrel, word_embeddings_file, index_path, model_file):
         self.settings = {}
@@ -31,7 +33,7 @@ class Experiments(object):
         self.w2v_cache = word_embeddings_file
         self.index_path = index_path
         self.model_file = model_file
-        self.cmd_root = "python src/main/python/run_trecqa.py -input {} -output e2e.run -qrel {} -index {} -w2v-cache {} -qa-model-file {} -model idf".\
+        self.cmd_root = "python src/main/python/run_trecqa.py -input {} -output e2e.run -qrel {} -index {} -w2v-cache {} -qa-model-file {} -model idf". \
             format(self.qa_data_xml, self.qrel, self.index_path, self.w2v_cache, self.model_file)
         self.eval_cmd_root = "./eval/trec_eval.9.0/trec_eval -m map -m recip_rank -m bpref {}".format(self.qrel)
         self.rbp_cmd_root = "rbp_eval/rbp_eval {}".format(self.qrel)
@@ -40,7 +42,6 @@ class Experiments(object):
         self.settings[setting.label] = setting
         self._setup_combinations()
 
-
     def _setup_combinations(self):
         all_settings = []
         for setting in self.settings.values():
@@ -48,17 +49,17 @@ class Experiments(object):
 
         self.combinations = []
         for c in itertools.product(*all_settings):
-             self.combinations.append(c)
+            self.combinations.append(c)
 
     def _run_cmd(self, cmd):
         pargs = shlex.split(cmd)
         p = subprocess.Popen(pargs, stdout=subprocess.PIPE, stderr=subprocess.PIPE, \
-                                bufsize=1, universal_newlines=True)
+                             bufsize=1, universal_newlines=True)
         pout, perr = p.communicate()
         return pout, perr
-    
+
     def _run_eval(self, params):
-    
+
         cmd = '{} e2e.run'.format(self.eval_cmd_root)
         out, err = self._run_cmd(cmd)
         # trec_eval scores
@@ -69,8 +70,8 @@ class Experiments(object):
             fields = line.strip().split()
             metrics.append(fields[0])
             scores.append(fields[-1])
-        
-        # rbp_eval scores 
+
+        # rbp_eval scores
         cmd = '{} e2e.run'.format(self.rbp_cmd_root)
         out, err = self._run_cmd(cmd)
         for line in str(out).split('\n'):
@@ -80,7 +81,6 @@ class Experiments(object):
         param, setting = zip(*params)
         print('{}\t{}'.format('\t'.join(param), '\t'.join(metrics)))
         print('{}\t{}'.format('\t'.join(setting), '\t'.join(scores)))
-
 
     def run(self, indices):
         """
@@ -95,7 +95,7 @@ class Experiments(object):
                 setting, choice = setting_choice.split(':')
                 cmd_args.append(self.settings[setting].choice_flags[choice])
                 params.append((setting, choice))
-            
+
             cmd = '{} {}'.format(self.cmd_root, ' '.join(cmd_args))
             print(cmd)
             out, err = self._run_cmd(cmd)
@@ -105,86 +105,83 @@ class Experiments(object):
                 print(out, file=lf)
                 print('---------- ERR ------------', file=lf)
                 print(err, file=lf)
-            self._run_eval(params)
+                self._run_eval(params)
 
-      
-    def run_all(self):
-        """
-        runs all experiments
-        """
-        self.run([ci for ci in range(len(self.combinations))])
-            
-    
-    def list_settings(self):
-        """
-        lists all settings
-        """
-        for c in enumerate(self.combinations):
-            print(c)
-        print("--run X Y Z to run combinations number X Y Z")
+        def run_all(self):
+            """
+            runs all experiments
+            """
+            self.run([ci for ci in range(len(self.combinations))])
 
+        def list_settings(self):
+            """
+            lists all settings
+            """
+            for c in enumerate(self.combinations):
+                print(c)
+            print("--run X Y Z to run combinations number X Y Z")
 
-if __name__ == "__main__":
-    ap = argparse.ArgumentParser(description="Lists exerimental settings and runs experiments")
-    ap.add_argument("--list", help="lists all available experimental settings combinations",
-                    action="store_true")
-    ap.add_argument("--run", help='runs experimenal setting combination NUMBER(s)',
-                    nargs="+", type=int)
-    ap.add_argument("--run-all", action="store_true")
-    ap.add_argument('qa_data_xml', help="path to the QA dataset XML file")
-    ap.add_argument('qrel_file', help="the qrel file e.g. ../data/TrecQA/raw-dev.qrel")
-    ap.add_argument("index_path", help="required for some combination of experiments")
-    ap.add_argument('word_embeddings_file', help="the word embeddings file")
-    ap.add_argument('model_file', help="the model to be used for QA similarity")
-    #ap.add_argument("--idf-source", choices=["qa-data", "corpus-index"], default="corpus-index")
-    #ap.add_argument("--punctuation", choices=["keep", "remove"], default="keep")
-    #ap.add_argument("--dash-words", choices=["keep", "split"], default="keep")
-    #ap.add_argument("models_root", help="path to folder containing models")
+    if __name__ == "__main__":
+        ap = argparse.ArgumentParser(description="Lists exerimental settings and runs experiments")
+        ap.add_argument("--list", help="lists all available experimental settings combinations",
+                        action="store_true")
+        ap.add_argument("--run", help='runs experimenal setting combination NUMBER(s)',
+                        nargs="+", type=int)
+        ap.add_argument("--run-all", action="store_true")
+        ap.add_argument('qa_data_xml', help="path to the QA dataset XML file")
+        ap.add_argument('qrel_file', help="the qrel file e.g. ../data/TrecQA/raw-dev.qrel")
+        ap.add_argument("index_path", help="required for some combination of experiments")
+        ap.add_argument('word_embeddings_file', help="the word embeddings file")
+        ap.add_argument('model_file', help="the model to be used for QA similarity")
+        # ap.add_argument("--idf-source", choices=["qa-data", "corpus-index"], default="corpus-index")
+        # ap.add_argument("--punctuation", choices=["keep", "remove"], default="keep")
+        # ap.add_argument("--dash-words", choices=["keep", "split"], default="keep")
+        # ap.add_argument("models_root", help="path to folder containing models")
 
-    args = ap.parse_args()
+        args = ap.parse_args()
 
-    experiments = Experiments(args.qa_data_xml, args.qrel_file, args.word_embeddings_file,
-                              args.index_path, args.model_file)
+        experiments = Experiments(args.qa_data_xml, args.qrel_file, args.word_embeddings_file,
+                                  args.index_path, args.model_file)
 
-    # experiments.add_setting(Setting('idf_source', {
-    #     'qa-data': '--idf-source qa-data',
-    #     'corpus-index': '--idf-source corpus-index'
-    # }))
+        # experiments.add_setting(Setting('idf_source', {
+        #     'qa-data': '--idf-source qa-data',
+        #     'corpus-index': '--idf-source corpus-index'
+        # }))
 
-    # experiments.add_setting(Setting('punctuation', {
-    #     'keep': '--punctuation keep',
-    #     'remove': '--punctuation remove'
-    # }))
+        # experiments.add_setting(Setting('punctuation', {
+        #     'keep': '--punctuation keep',
+        #     'remove': '--punctuation remove'
+        # }))
 
-    # experiments.add_setting(Setting('dash_words', {
-    #     'keep': '--dash-words keep',
-    #     'split': '--dash-words split'
-    # }))
+        # experiments.add_setting(Setting('dash_words', {
+        #     'keep': '--dash-words keep',
+        #     'split': '--dash-words split'
+        # }))
 
-    experiments.add_setting(Setting('num_hits', {
-        '100': '-hits 100',
-	'200': '-hits 200',
-	'300': '-hits 300',
-	'400': '-hits 400',
-	'500': '-hits 500',
-	'600': '-hits 600',
-	'700': '-hits 700',
-	'800': '-hits 800',
-	'900': '-hits 900',
-	'1000': '-hits 1000'
-    }))
+        experiments.add_setting(Setting('num_hits', {
+            '100': '-hits 100',
+            '200': '-hits 200',
+            '300': '-hits 300',
+            '400': '-hits 400',
+            '500': '-hits 500',
+            '600': '-hits 600',
+            '700': '-hits 700',
+            '800': '-hits 800',
+            '900': '-hits 900',
+            '1000': '-hits 1000'
+        }))
 
-    experiments.add_setting(Setting('top_k', {
-       	'5': '-k 5',
-	'10': '-k 10',
-    }))
+        experiments.add_setting(Setting('top_k', {
+            '5': '-k 5',
+            '10': '-k 10',
+        }))
 
-    experiments.list_settings()
+        experiments.list_settings()
 
-    if args.run:
-        experiments.run(args.run)
+        if args.run:
+            experiments.run(args.run)
 
-    if args.run_all:
-        experiments.run_all()
+        if args.run_all:
+            experiments.run_all()
 
 
--- src/main/python/run_trecqa.py
+++ src/main/python/run_trecqa.py
@@ -8,139 +8,134 @@ from sm_cnn.bridge import SMModelBridge
 import hashlib
 
 class QAModel:
-  instance = None
-  def __init__(self, model_choice, index_path, w2v_cache, qa_model_file):
-    if not QAModel.instance:
-       path = os.getcwd() + '/..'
-       if model_choice == "sm":         
-         QAModel.instance = SMModelBridge(qa_model_file,
-                             w2v_cache,
-                             index_path)
-
+    instance = None
+    def __init__(self, model_choice, index_path, w2v_cache, qa_model_file):
+        if model_choice == "sm":
+            QAModel.instance = SMModelBridge(qa_model_file, w2v_cache, index_path)
 
 def get_answers(pyserini, question, h0, h1, model_choice, index_path, w2v_cache="", qa_model_file=""):
-  candidate_passages_scores = pyserini.ranked_passages(question, h0, h1)
-  idf_json = pyserini.get_term_idf_json()
-  candidate_passages = []
-  tokeninzed_answer = []
+    candidate_passages_scores = pyserini.ranked_passages(question, h0, h1)
+    idf_json = pyserini.get_term_idf_json()
+    candidate_passages = []
+    tokeninzed_answer = []
 
-  if model_choice == 'sm':
+    if model_choice == 'sm':
 
-    qa_DL_model = QAModel(model_choice, index_path, w2v_cache, qa_model_file).instance
+        qa_DL_model = QAModel(model_choice, index_path, w2v_cache, qa_model_file).instance
 
-    for ps in candidate_passages_scores:
-      ps_split = ps.split('\t')
-      candidate_passages.append(ps_split[0])
+        for ps in candidate_passages_scores:
+            ps_split = ps.split('\t')
+            candidate_passages.append(ps_split[0])
 
-    # TODO: for processing input data. Ideally these settings need to come in as program arguments
-    # NOTE: the best model for TrecQA keeps punctuation and keeps dash-words
-    flags = {
-      "punctuation": "", # ignoring for now  you can {keep|remove} punctuation
-      "dash_words": "" # ignoring for now. you can {keep|split} words-with-hyphens
-    }
-    answers_list = qa_DL_model.rerank_candidate_answers(question, candidate_passages, idf_json, flags)
-    sorted_answers = sorted(answers_list, key=lambda x: x[0], reverse=True)
+        # TODO: for processing input data. Ideally these settings need to come in as program arguments
+        # NOTE: the best model for TrecQA keeps punctuation and keeps dash-words
+        flags = {
+          "punctuation": "", # ignoring for now  you can {keep|remove} punctuation
+          "dash_words": "" # ignoring for now. you can {keep|split} words-with-hyphens
+        }
+        answers_list = qa_DL_model.rerank_candidate_answers(question, candidate_passages, idf_json, flags)
+        sorted_answers = sorted(answers_list, key=lambda x: x[0], reverse=True)
 
-    for score, candidate in sorted_answers:
-      tokens = TreebankWordTokenizer().tokenize(candidate.lower().split("\t")[0])
-      tokeninzed_answer.append((tokens, score))
-    return tokeninzed_answer
+        for score, candidate in sorted_answers:
+            tokens = TreebankWordTokenizer().tokenize(candidate.lower().split("\t")[0])
+            tokeninzed_answer.append((tokens, score))
+        return tokeninzed_answer
 
-  elif model_choice == 'idf':
-    for candidate in candidate_passages_scores:
-      candidate_sent, score = candidate.lower().split("\t")
-      tokens = TreebankWordTokenizer().tokenize(candidate_sent.lower())
-      tokeninzed_answer.append((tokens, score))
-    return tokeninzed_answer
+    elif model_choice == 'idf':
+        for candidate in candidate_passages_scores:
+            candidate_sent, score = candidate.lower().split("\t")
+            tokens = TreebankWordTokenizer().tokenize(candidate_sent.lower())
+            tokeninzed_answer.append((tokens, score))
+        return tokeninzed_answer
 
 def jaccard(set1, set2):
-  intersection = set1.intersection(set2)
-  union = set1.union(set2)
-  return len(intersection) / float(len(union))
+    intersection = set1.intersection(set2)
+    union = set1.union(set2)
+    return len(intersection) / float(len(union))
 
 def score_candidates(candidates, answers):
-  scored_candidates = {}
+    scored_candidates = {}
 
-  for candidate in candidates:
-    this_candidate = " ".join(candidate[0])
+    for candidate in candidates:
+        this_candidate = " ".join(candidate[0])
 
-    # xml file doesn't contain answers
-    if not answers:
-      scored_candidates[this_candidate] = ("empty answer", 0.0, candidate[1])
+        # xml file doesn't contain answers
+        if not answers:
+            scored_candidates[this_candidate] = ("empty answer", 0.0, candidate[1])
 
-    for answer in answers:
-      similarity = jaccard(set(candidate[0]), set(answer[1]))
+        for answer in answers:
+            similarity = jaccard(set(candidate[0]), set(answer[1]))
 
-      if this_candidate not in scored_candidates:
-        scored_candidates[this_candidate] = (answer, similarity, candidate[1])
-      elif similarity > scored_candidates[this_candidate][1]:
-        scored_candidates[this_candidate] = (answer, similarity, candidate[1])
+            if this_candidate not in scored_candidates:
+                scored_candidates[this_candidate] = (answer, similarity, candidate[1])
+            elif similarity > scored_candidates[this_candidate][1]:
+                scored_candidates[this_candidate] = (answer, similarity, candidate[1])
 
-  return scored_candidates
+    return scored_candidates
 
 
 def load_data(fname):
-  questions = []
-  answers = {}
-  labels = {}
-  prev = ''
-  answer_count = 0
+    questions = []
+    answers = {}
+    labels = {}
+    prev = ''
+    answer_count = 0
 
-  with open(fname, 'r') as f:
-    for line in f:
-      line = line.strip()
-      qid_match = re.match('<QApairs id=\'(.*)\'>', line)
+    with open(fname, 'r') as f:
+        for line in f:
+            line = line.strip()
+            qid_match = re.match('<QApairs id=\'(.*)\'>', line)
 
-      if qid_match:
-        answer_count = 0
-        qid = qid_match.group(1)
+            if qid_match:
+                answer_count = 0
+                qid = qid_match.group(1)
 
-        if qid not in answers:
-          answers[qid] = []
+                if qid not in answers:
+                    answers[qid] = []
 
-        if qid not in labels:
-          labels[qid] = {}
+                if qid not in labels:
+                    labels[qid] = {}
 
-      if prev and prev.startswith('<question>'):
-        questions.append(line)
+            if prev and prev.startswith('<question>'):
+                questions.append(line)
 
-      label = re.match('^<(positive|negative)>', prev)
+            label = re.match('^<(positive|negative)>', prev)
 
-      if label:
-        label = label.group(1)
-        label = 1 if label == 'positive' else 0
-        answer = line.lower().split('\t')
-        answer_count += 1
+            if label:
+                label = label.group(1)
+                label = 1 if label == 'positive' else 0
+                answer = line.lower().split('\t')
+                answer_count += 1
 
-        answer_id = 'Q{}-A{}'.format(qid, answer_count)
-        answers[qid].append((answer_id, answer, label))
-        labels[qid][answer_id] = label
-      prev = line
+                answer_id = 'Q{}-A{}'.format(qid, answer_count)
+                answers[qid].append((answer_id, answer, label))
+                labels[qid][answer_id] = label
+            prev = line
 
-  return questions, answers, labels
+    return questions, answers, labels
 
 
 def create_qrel_jaccard(fname, answers):
-  with open(fname, 'w') as f:
-    for qid in answers.keys():
-      for answer in answers[qid]:
-        f.write('{} 0 {} {}\n'.format(qid, answer[0], answer[2]))
+    with open(fname, 'w') as f:
+        for qid in answers.keys():
+            for answer in answers[qid]:
+                f.write('{} 0 {} {}\n'.format(qid, answer[0], answer[2]))
 
 # evaluate by TREC QA patterns
 def eval_by_pattern(qid, candidates, pattern, out):
-  seen = set([])
+    seen = set([])
 
-  for i, cand in enumerate(candidates):
-    this_candidate = " ".join(cand[0])
+    for i, cand in enumerate(candidates):
+        this_candidate = " ".join(cand[0])
 
-    hash_value = hashlib.md5(this_candidate.encode()).hexdigest()
-    answer_id = "QA{}.{}".format(qid, hash_value)
+        hash_value = hashlib.md5(this_candidate.encode()).hexdigest()
+        answer_id = "QA{}.{}".format(qid, hash_value)
 
-    if answer_id in seen:
-        continue
-    seen.add(answer_id)
+        if answer_id in seen:
+            continue
+        seen.add(answer_id)
 
-    out.write('{} Q0 {} {} {} TrecQA-pattern\n'.format(qid, answer_id, i+1, 1.0/(i + 1)))
+        out.write('{} Q0 {} {} {} TrecQA-pattern\n'.format(qid, answer_id, i+1, 1.0/(i + 1)))
 
 # evaluate the run file at different depths
 def evaluate_at_k(fname, eval_depth):
@@ -197,107 +192,107 @@ def get_stopwords():
     return stopwords
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser(description='Evaluate the QA system')
-  parser.add_argument('-input', help='path of a TrecQA file', required=True)
-  parser.add_argument("-output", help="path of output file", default=".")
-  parser.add_argument("-qrel", help="path of qrel file")
-  parser.add_argument("-h0", help="number of hits", default=1000)
-  parser.add_argument("-h1", help="h1 passages to be reranked", default=100)
-  parser.add_argument("-k", help="evaluate at depth k", default="5")
-  parser.add_argument("-model", help="[idf|sm]", default='idf')
-  parser.add_argument('-index', help="path of the index", required=True)
-  parser.add_argument('-w2v-cache', help="word embeddings cache file")
-  parser.add_argument('-qa-model-file', help="the path to the model file")
-  parser.add_argument('-eval', help="[pattern|jaccard]", default='pattern')
-  parser.add_argument('-pattern', help='path to the pattern file')
+    parser = argparse.ArgumentParser(description='Evaluate the QA system')
+    parser.add_argument('-input', help='path of a TrecQA file', required=True)
+    parser.add_argument("-output", help="path of output file", default=".")
+    parser.add_argument("-qrel", help="path of qrel file")
+    parser.add_argument("-h0", help="number of hits", default=1000)
+    parser.add_argument("-h1", help="h1 passages to be reranked", default=100)
+    parser.add_argument("-k", help="evaluate at depth k", default="5")
+    parser.add_argument("-model", help="[idf|sm]", default='idf')
+    parser.add_argument('-index', help="path of the index", required=True)
+    parser.add_argument('-w2v-cache', help="word embeddings cache file")
+    parser.add_argument('-qa-model-file', help="the path to the model file")
+    parser.add_argument('-eval', help="[pattern|jaccard]", default='pattern')
+    parser.add_argument('-pattern', help='path to the pattern file')
 
-  args = parser.parse_args()
+    args = parser.parse_args()
 
-  if (args.model == "sm" and not args.w2v_cache and not args.qa_model_file):
-    print("Pass the word embeddings cache file and the model file")
-    parser.print_help()
-    exit()
+    if (args.model == "sm" and not args.w2v_cache and not args.qa_model_file):
+        print("Pass the word embeddings cache file and the model file")
+        parser.print_help()
+        exit()
 
-  pattern_dict = {}
-  if args.eval == 'pattern':
-      if not args.pattern:
-          print("Path to the pattern file should be included if the method of evaluation is pattern based")
-          parser.print_help()
-          exit()
-      else:
-          with open(args.pattern, 'r') as pattern_file:
-            for line in pattern_file:
-              det = line.strip().split(' ', 1)
-
-              if det[0] not in pattern_dict:
-                pattern_dict[det[0]] = det[1]
-
-  pyserini = Pyserini(args.index)
-  questions, answers, labels_actual = load_data(args.input)
-  threshold = 0.7
-  # TODO: what is this ^^ magic number
-
-  qrel_file = ""
-
-  if args.qrel:
-    qrel_file = args.qrel
-  elif args.eval == 'jaccard':
-    qrel_file = '{}/qrel.jaccard.txt'.format(args.output)
-    create_qrel_jaccard(qrel_file, answers)
-  elif args.eval == 'pattern':
-    qrel_file = '{}/qrel.pattern.txt'.format(args.output)
-    stopwords = get_stopwords()
-
-    with open(qrel_file, 'w') as qrel:
-        for qid, question in zip(answers.keys(), questions):
-            # retrieve all sentences corresponding to h0 hits
-            all_sentences = pyserini.get_all_sentences(question, args.h0)
-            cleaned_sentences = clean_sentences(question, all_sentences, stopwords)
-            try:
-                create_qrel_pattern(cleaned_sentences, pattern_dict[qid], qrel, qid)
-            except KeyError as e:
-                print(e)
-                print("pattern not found")
-
-  output_file = '{}/run.qa.{}.{}.h0_{}.h1_{}'.format(args.output, args.model,  args.eval, args.h0, args.h1)
-  seen_docs = []
-
-  with open(output_file, 'w') as out:
-    for qid, question in zip(answers.keys(), questions):
-
-      # sentence re-ranking after ad-hoc retrieval
-      candidates = get_answers(pyserini, question, int(args.h0), int(args.h1), args.model, args.index, args.w2v_cache,
-                               args.qa_model_file)
-
-      if args.eval == 'jaccard':
-        scored_candidates = score_candidates(candidates, answers[qid])
-        i, unjudged_count = 0, 0
-
-        for key, value in scored_candidates.items():
-          jaccard_similarity = value[1]
-          i += 1
-
-          # check if the answer already exists in the TrecQA test set
-          if jaccard_similarity >= threshold:
-            doc_id = value[0][0]
-            if doc_id not in seen_docs:
-              out.write('{} Q0 {} {} {} TRECQA\n'.format(qid, doc_id, i, value[2]))
-              seen_docs.append(doc_id)
-          else:
-            unjudged_count += 1
-            doc_id = 'unjudged{}'.format(unjudged_count)
-            out.write('{} Q0 {} {} {} TRECQA\n'.format(qid, doc_id, i, value[2]))
-
-      elif args.eval == 'pattern':
+    pattern_dict = {}
+    if args.eval == 'pattern':
         if not args.pattern:
-          print("Path to the pattern file should be included if the method of evaluation is pattern based")
-          args.print_help()
-          exit()
-        try:
-            eval_by_pattern(qid, candidates, pattern_dict[qid], out)
-        except KeyError as e:
-            print("Pattern not found for question: {}".format(e))
+            print("Path to the pattern file should be included if the method of evaluation is pattern based")
+            parser.print_help()
+            exit()
+        else:
+            with open(args.pattern, 'r') as pattern_file:
+                for line in pattern_file:
+                    det = line.strip().split(' ', 1)
 
-    eval_depth = [int(i) for i in args.k.strip().split()]
-    evaluate_at_k(output_file, eval_depth)
+                    if det[0] not in pattern_dict:
+                        pattern_dict[det[0]] = det[1]
+
+    pyserini = Pyserini(args.index)
+    questions, answers, labels_actual = load_data(args.input)
+    threshold = 0.7
+    # TODO: what is this ^^ magic number
+
+    qrel_file = ""
+
+    if args.qrel:
+        qrel_file = args.qrel
+    elif args.eval == 'jaccard':
+        qrel_file = '{}/qrel.jaccard.txt'.format(args.output)
+        create_qrel_jaccard(qrel_file, answers)
+    elif args.eval == 'pattern':
+        qrel_file = '{}/qrel.pattern.txt'.format(args.output)
+
+        stopwords = get_stopwords()
+
+        with open(qrel_file, 'w') as qrel:
+            for qid, question in zip(answers.keys(), questions):
+                # retrieve all sentences corresponding to h0 hits
+                all_sentences = pyserini.get_all_sentences(question, args.h0)
+                cleaned_sentences = clean_sentences(question, all_sentences, stopwords)
+                try:
+                    create_qrel_pattern(cleaned_sentences, pattern_dict[qid], qrel, qid)
+                except KeyError as e:
+                    print(e)
+                    print("pattern not found")
+
+    output_file = '{}/run.qa.{}.{}.h0_{}.h1_{}'.format(args.output, args.model,  args.eval, args.h0, args.h1)
+    seen_docs = []
+
+    with open(output_file, 'w') as out:
+        for qid, question in zip(answers.keys(), questions):
+            # sentence re-ranking after ad-hoc retrieval
+            candidates = get_answers(pyserini, question, int(args.h0), int(args.h1), args.model, args.index, args.w2v_cache,
+                                   args.qa_model_file)
+
+            if args.eval == 'jaccard':
+                scored_candidates = score_candidates(candidates, answers[qid])
+                i, unjudged_count = 0, 0
+
+                for key, value in scored_candidates.items():
+                  jaccard_similarity = value[1]
+                  i += 1
+
+                # check if the answer already exists in the TrecQA test set
+                if jaccard_similarity >= threshold:
+                    doc_id = value[0][0]
+                    if doc_id not in seen_docs:
+                        out.write('{} Q0 {} {} {} TRECQA\n'.format(qid, doc_id, i, value[2]))
+                        seen_docs.append(doc_id)
+                    else:
+                        unjudged_count += 1
+                        doc_id = 'unjudged{}'.format(unjudged_count)
+                        out.write('{} Q0 {} {} {} TRECQA\n'.format(qid, doc_id, i, value[2]))
+
+            elif args.eval == 'pattern':
+                if not args.pattern:
+                    print("Path to the pattern file should be included if the method of evaluation is pattern based")
+                    args.print_help()
+                    exit()
+                try:
+                    eval_by_pattern(qid, candidates, pattern_dict[qid], out)
+                except KeyError as e:
+                    print("Pattern not found for question: {}".format(e))
+
+        eval_depth = [int(i) for i in args.k.strip().split()]
+        evaluate_at_k(output_file, eval_depth)
 
