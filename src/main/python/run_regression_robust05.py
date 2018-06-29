da09c5b11dc34aed03a5882031dcc40070de1867
Jimmy Lin <jimmylin@uwaterloo.ca>
2018-06-29 19:01:39 -0400

    Add --index option to regression scripts to explicitly build index (#281)
    By default, run_regression_*.py doesn't rebuild index,
    instead using indexes in /tuna1/scratch/
    To explicitly rebuild index, use --index option.

--- src/main/python/run_regression_robust05.py
+++ src/main/python/run_regression_robust05.py
@@ -1,5 +1,7 @@
 import sys
 import os
+import argparse
+
 from subprocess import call
 
 index_cmd = """
@@ -9,10 +11,10 @@ nohup sh target/appassembler/bin/IndexCollection -collection TrecCollection \
  -storePositions -storeDocvectors -storeRawDocs -optimize"""
 
 run_cmds = [ \
-    "nohup sh target/appassembler/bin/SearchCollection -topicreader Trec -index lucene-index.aquaint.pos+docvectors+rawdocs -topics src/main/resources/topics-and-qrels/topics.robust05.txt -output run.aquaint.robust05.bm25.txt -bm25",
-    "nohup sh target/appassembler/bin/SearchCollection -topicreader Trec -index lucene-index.aquaint.pos+docvectors+rawdocs -topics src/main/resources/topics-and-qrels/topics.robust05.txt -output run.aquaint.robust05.bm25+rm3.txt -bm25 -rm3",
-    "nohup sh target/appassembler/bin/SearchCollection -topicreader Trec -index lucene-index.aquaint.pos+docvectors+rawdocs -topics src/main/resources/topics-and-qrels/topics.robust05.txt -output run.aquaint.robust05.ql.txt -ql",
-    "nohup sh target/appassembler/bin/SearchCollection -topicreader Trec -index lucene-index.aquaint.pos+docvectors+rawdocs -topics src/main/resources/topics-and-qrels/topics.robust05.txt -output run.aquaint.robust05.ql+rm3.txt -ql -rm3"]
+    "nohup sh target/appassembler/bin/SearchCollection -topicreader Trec -index {} -topics src/main/resources/topics-and-qrels/topics.robust05.txt -output run.aquaint.robust05.bm25.txt -bm25",
+    "nohup sh target/appassembler/bin/SearchCollection -topicreader Trec -index {} -topics src/main/resources/topics-and-qrels/topics.robust05.txt -output run.aquaint.robust05.bm25+rm3.txt -bm25 -rm3",
+    "nohup sh target/appassembler/bin/SearchCollection -topicreader Trec -index {} -topics src/main/resources/topics-and-qrels/topics.robust05.txt -output run.aquaint.robust05.ql.txt -ql",
+    "nohup sh target/appassembler/bin/SearchCollection -topicreader Trec -index {} -topics src/main/resources/topics-and-qrels/topics.robust05.txt -output run.aquaint.robust05.ql+rm3.txt -ql -rm3"]
 
 qrels = "src/main/resources/topics-and-qrels/qrels.robust2005.txt"
 
@@ -23,9 +25,22 @@ def trec_eval_metric(metric, qrels, run):
     return float(os.popen("eval/trec_eval.9.0/trec_eval -m {} {} {}".format(metric, qrels, run)).read().split("\t")[2].strip())
 
 if __name__ == "__main__":
-    call(index_cmd, shell=True)
+    parser = argparse.ArgumentParser(description='Run regression tests on Robust05.')
+    parser.add_argument('--index', dest='index', action='store_true', help='rebuild index from scratch')
+
+    args = parser.parse_args()
+
+    # Decide if we're going to index from scratch. If not, use pre-stored index at known location.
+    if args.index == True:
+        call(index_cmd, shell=True)
+        index_path = 'lucene-index.aquaint.pos+docvectors+rawdocs'
+        print(args.index)
+    else:
+        index_path = '/tuna1/indexes/lucene-index.aquaint.pos+docvectors+rawdocs'
+
+    # Use the correct index path.
     for cmd in run_cmds:
-        call(cmd, shell=True)
+        call(cmd.format(index_path), shell=True)
 
     expected_map = extract_value_from_doc("TREC 2005 Robust Track Topics", 1, 1)
     actual_map = trec_eval_metric("map", qrels, "run.aquaint.robust05.bm25.txt")
