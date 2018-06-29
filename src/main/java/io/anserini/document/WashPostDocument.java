b3b212e2d92d76a25da360c43b84be2de4d018b8
Yuhao Xie (Kyle) <Kytabyte@users.noreply.github.com>
2018-06-29 06:55:46 -0400

    Patch for #250 "Change WashPost collection source to two-space indent" (#284)

--- src/main/java/io/anserini/document/WashPostDocument.java
+++ src/main/java/io/anserini/document/WashPostDocument.java
@@ -32,84 +32,84 @@ import org.json.JSONObject;
  * A Washington Post document.
  */
 public class WashPostDocument implements SourceDocument {
-    private static final Logger LOG = LogManager.getLogger(WashPostDocument.class);
-    private static final String PATTERN = "<\\/?\\w+>";
+  private static final Logger LOG = LogManager.getLogger(WashPostDocument.class);
+  private static final String PATTERN = "<\\/?\\w+>";
 
-    private final String ID_TAG = "id";
-    private final String DATE_TAG = "published_date";
-    private final String CONTENT_TAG = "contents";
-    private final String PARAGRAPH_TAG = "content";
-    private final String TYPE_TAG = "type";
-    private final List<String> CONTENT_TYPE_TAG = Arrays.asList("sanitized_html", "tweet");
+  private final String ID_TAG = "id";
+  private final String DATE_TAG = "published_date";
+  private final String CONTENT_TAG = "contents";
+  private final String PARAGRAPH_TAG = "content";
+  private final String TYPE_TAG = "type";
+  private final List<String> CONTENT_TYPE_TAG = Arrays.asList("sanitized_html", "tweet");
 
-    protected String id;
-    protected long date;
-    protected String content;
+  protected String id;
+  protected long date;
+  protected String content;
 
 
-    @Override
-    public SourceDocument readNextRecord(BufferedReader bufferedReader) throws IOException {
-        String nextRecord = bufferedReader.readLine();
-        if (nextRecord == null) {
-            return null;
+  @Override
+  public SourceDocument readNextRecord(BufferedReader bufferedReader) throws IOException {
+    String nextRecord = bufferedReader.readLine();
+    if (nextRecord == null) {
+      return null;
+    }
+    return parseRecord(nextRecord);
+
+  }
+
+  private SourceDocument parseRecord(String record) {
+    StringBuilder builder = new StringBuilder();
+
+    JSONObject contentObj;
+    JSONObject recordObj = new JSONObject(record);
+
+    if (!recordObj.has(ID_TAG) || !recordObj.has(DATE_TAG)) {
+      // For current dataset, we can make sure all record has unique id and
+      //  published date. So we just simply log a warning and return null
+      //  here in case future data may bring up this issue
+      LOG.warn("No unique ID or published date for this record, ignored...");
+      return null;
+    }
+    id = recordObj.getString(ID_TAG);
+    date = recordObj.getLong(DATE_TAG);
+
+    JSONArray contentArray = recordObj.getJSONArray(CONTENT_TAG);
+    for (Object obj : contentArray) {
+      contentObj = (JSONObject) obj;
+      if (contentObj.has(TYPE_TAG) && contentObj.has(PARAGRAPH_TAG)) {
+        if (CONTENT_TYPE_TAG.contains(contentObj.getString(TYPE_TAG))) {
+          try {
+            builder.append(removeTags(contentObj.getString(PARAGRAPH_TAG).trim())).append("\n");
+          } catch (JSONException e) {
+            LOG.error("Error caught while retrieving JSON string.");
+            e.printStackTrace();
+          }
         }
-        return parseRecord(nextRecord);
-
+      } else {
+        LOG.warn("No type or content tag defined in Article " + id + ", ignored this file.");
+      }
     }
+    content = builder.toString();
+    return this;
+  }
 
-    private SourceDocument parseRecord(String record) {
-        StringBuilder builder = new StringBuilder();
-
-        JSONObject contentObj;
-        JSONObject recordObj = new JSONObject(record);
-
-        if (!recordObj.has(ID_TAG) || !recordObj.has(DATE_TAG)) {
-            // For current dataset, we can make sure all record has unique id and
-            //  published date. So we just simply log a warning and return null
-            //  here in case future data may bring up this issue
-            LOG.warn("No unique ID or published date for this record, ignored...");
-            return null;
-        }
-        id = recordObj.getString(ID_TAG);
-        date = recordObj.getLong(DATE_TAG);
-
-        JSONArray contentArray = recordObj.getJSONArray(CONTENT_TAG);
-        for (Object obj : contentArray) {
-            contentObj = (JSONObject) obj;
-            if (contentObj.has(TYPE_TAG) && contentObj.has(PARAGRAPH_TAG)) {
-                if (CONTENT_TYPE_TAG.contains(contentObj.getString(TYPE_TAG))) {
-                    try {
-                        builder.append(removeTags(contentObj.getString(PARAGRAPH_TAG).trim())).append("\n");
-                    } catch (JSONException e) {
-                        LOG.error("Error caught while retrieving JSON string.");
-                        e.printStackTrace();
-                    }
-                }
-            } else {
-                LOG.warn("No type or content tag defined in Article " + id + ", ignored this file.");
-            }
-        }
-        content = builder.toString();
-        return this;
-    }
-
-    private String removeTags(String content) {
-        return content.replaceAll(PATTERN, " ");
-    }
+  private String removeTags(String content) {
+    return content.replaceAll(PATTERN, " ");
+  }
 
 
-    @Override
-    public String id() {
-        return id;
-    }
+  @Override
+  public String id() {
+    return id;
+  }
 
-    @Override
-    public String content() {
-        return content;
-    }
+  @Override
+  public String content() {
+    return content;
+  }
 
-    @Override
-    public boolean indexable() {
-        return true;
-    }
+  @Override
+  public boolean indexable() {
+    return true;
+  }
 }
