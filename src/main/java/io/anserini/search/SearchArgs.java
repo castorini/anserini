5fbe09c3e9a87cbc97c8dbf80817b134f1e2296c
Peilin Yang <yangpeilyn@gmail.com>
2018-07-07 06:37:12 -0700

    renaming parameters in axiom reranking model so that they are naming after whatever are in the paper (#306)

--- src/main/java/io/anserini/search/SearchArgs.java
+++ src/main/java/io/anserini/search/SearchArgs.java
@@ -104,11 +104,11 @@ public class SearchArgs {
   @Option(name = "-axiom.seed", metaVar = "[number]", usage = "seed for the random generator in axiomatic reranking")
   public long axiom_seed = 42L;
 
-  @Option(name = "-axiom.m", usage = "parameter M in axiomatic reranking")
-  public int axiom_m = 20;
-
   @Option(name = "-axiom.r", usage = "parameter R in axiomatic reranking")
-  public int axiom_r = 30;
+  public int axiom_r = 20;
+
+  @Option(name = "-axiom.n", usage = "parameter N in axiomatic reranking")
+  public int axiom_n = 30;
 
   @Option(name = "-axiom.beta", usage = "parameter beta for Axiomatic query expansion model")
   public float axiom_beta = 0.4f;
