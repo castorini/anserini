fc712f352724374ed59b20f951475b0945fdc79b
Boris Lin <boruilin224@gmail.com>
2018-07-12 15:44:46 -0400

    Refactor variable names

--- src/main/java/io/anserini/collection/TweetCollection.java
+++ src/main/java/io/anserini/collection/TweetCollection.java
@@ -95,8 +95,8 @@ public class TweetCollection extends Collection {
 
     // Optional fields
     protected Optional<String> name;
-    protected Optional<String> profile_image_url;
-    protected OptionalLong timestamp_ms;
+    protected Optional<String> profileImageUrl;
+    protected OptionalLong timestampMs;
     protected OptionalLong epoch;
     protected Optional<String> lang;
     protected OptionalLong inReplyToStatusId;
@@ -143,81 +143,81 @@ public class TweetCollection extends Collection {
         return false;
       }
 
-      if (JsonParser.isAvailable(tweetObj.delete())) {
+      if (JsonParser.isFieldAvailable(tweetObj.getDelete())) {
         return false;
       }
 
-      id = tweetObj.id_str();
-      idLong = Long.parseLong(tweetObj.id_str());
-      text = tweetObj.text();
-      createdAt = tweetObj.created_at();
+      id = tweetObj.getIdStr();
+      idLong = Long.parseLong(id);
+      text = tweetObj.getText();
+      createdAt = tweetObj.getCreatedAt();
 
       try {
-        timestamp_ms = OptionalLong.of((new SimpleDateFormat(DATE_FORMAT, Locale.ENGLISH)).parse(createdAt).getTime());
-        epoch = timestamp_ms.isPresent() ? OptionalLong.of(timestamp_ms.getAsLong() / 1000) : OptionalLong.empty();
+        timestampMs = OptionalLong.of((new SimpleDateFormat(DATE_FORMAT, Locale.ENGLISH)).parse(createdAt).getTime());
+        epoch = timestampMs.isPresent() ? OptionalLong.of(timestampMs.getAsLong() / 1000) : OptionalLong.empty();
       } catch (ParseException e) {
-        timestamp_ms = OptionalLong.of(-1L);
+        timestampMs = OptionalLong.of(-1L);
         epoch = OptionalLong.of(-1L);
         return false;
       }
 
-      if (JsonParser.isAvailable(tweetObj.in_reply_to_status_id())) {
-        inReplyToStatusId = tweetObj.in_reply_to_status_id();
+      if (JsonParser.isFieldAvailable(tweetObj.getInReplyToStatusId())) {
+        inReplyToStatusId = tweetObj.getInReplyToStatusId();
       } else {
         inReplyToStatusId = OptionalLong.empty();
       }
 
-      if (JsonParser.isAvailable(tweetObj.in_reply_to_user_id())) {
-        inReplyToUserId = tweetObj.in_reply_to_user_id();
+      if (JsonParser.isFieldAvailable(tweetObj.getInReplyToUserId())) {
+        inReplyToUserId = tweetObj.getInReplyToUserId();
       } else {
         inReplyToUserId = OptionalLong.empty();
       }
 
-      if (JsonParser.isAvailable(tweetObj.retweeted_status())) {
-        retweetStatusId = tweetObj.retweeted_status().get().id();
-        if (JsonParser.isAvailable(tweetObj.retweeted_status().get().user())) {
-          retweetUserId = tweetObj.retweeted_status().get().user().get().id();
+      if (JsonParser.isFieldAvailable(tweetObj.getRetweetedStatus())) {
+        retweetStatusId = tweetObj.getRetweetedStatus().get().getId();
+        if (JsonParser.isFieldAvailable(tweetObj.getRetweetedStatus().get().getUser())) {
+          retweetUserId = tweetObj.getRetweetedStatus().get().getUser().get().getId();
         } else {
           retweetUserId = OptionalLong.empty();
         }
-        retweetCount = tweetObj.retweet_count();
+        retweetCount = tweetObj.getRetweetCount();
       } else {
         retweetStatusId = OptionalLong.empty();
         retweetUserId = OptionalLong.empty();
         retweetCount = OptionalLong.empty();
       }
 
-      if (JsonParser.isAvailable(tweetObj.coordinates()) &&
-          JsonParser.isAvailable(tweetObj.coordinates().get().coordinates()) &&
-          tweetObj.coordinates().get().coordinates().get().size() >= 2) {
-        longitude = tweetObj.coordinates().get().coordinates().get().get(0);
-        latitude = tweetObj.coordinates().get().coordinates().get().get(1);
+      if (JsonParser.isFieldAvailable(tweetObj.getCoordinates()) &&
+          JsonParser.isFieldAvailable(tweetObj.getCoordinates().get().getCoordinates()) &&
+          tweetObj.getCoordinates().get().getCoordinates().get().size() >= 2) {
+        longitude = tweetObj.getCoordinates().get().getCoordinates().get().get(0);
+        latitude = tweetObj.getCoordinates().get().getCoordinates().get().get(1);
       } else {
         latitude = OptionalDouble.empty();
         longitude = OptionalDouble.empty();
       }
 
-      if (JsonParser.isAvailable(tweetObj.lang())) {
-        lang = tweetObj.lang();
+      if (JsonParser.isFieldAvailable(tweetObj.getLang())) {
+        lang = tweetObj.getLang();
       } else {
         lang = Optional.empty();
       }
 
-      followersCount = tweetObj.user().followers_count();
-      friendsCount = tweetObj.user().friends_count();
-      statusesCount = tweetObj.user().statuses_count();
-      screenname = tweetObj.user().screen_name();
+      followersCount = tweetObj.getUser().getFollowersCount();
+      friendsCount = tweetObj.getUser().getFriendsCount();
+      statusesCount = tweetObj.getUser().getStatusesCount();
+      screenname = tweetObj.getUser().getScreenName();
 
-      if (JsonParser.isAvailable(tweetObj.user().name())) {
-        name = tweetObj.user().name();
+      if (JsonParser.isFieldAvailable(tweetObj.getUser().getName())) {
+        name = tweetObj.getUser().getName();
       } else {
         name = Optional.empty();
       }
 
-      if (JsonParser.isAvailable(tweetObj.user().profile_image_url())) {
-        profile_image_url = tweetObj.user().profile_image_url();
+      if (JsonParser.isFieldAvailable(tweetObj.getUser().getProfileImageUrl())) {
+        profileImageUrl = tweetObj.getUser().getProfileImageUrl();
       } else {
-        profile_image_url = Optional.empty();
+        profileImageUrl = Optional.empty();
       }
 
       jsonString = json;
@@ -263,26 +263,38 @@ public class TweetCollection extends Collection {
       return true;
     }
 
-    public long getIdLong() { return idLong; }
+    public long getIdLong() {
+      return idLong;
+    }
+
     public String getScreenname() {
       return screenname;
     }
+
     public String getCreatedAt() {
       return createdAt;
     }
+
     public String getText() {
       return text;
     }
-    public TweetObject getJsonObject() { return jsonObject; }
+
+    public TweetObject getJsonObject() {
+      return jsonObject;
+    }
+
     public String getJsonString() {
       return jsonString;
     }
+
     public int getFollowersCount() {
       return followersCount;
     }
+
     public int getFriendsCount() {
       return friendsCount;
     }
+
     public int getStatusesCount() {
       return statusesCount;
     }
@@ -290,32 +302,47 @@ public class TweetCollection extends Collection {
     public Optional<String> getName() {
       return name;
     }
+
     public Optional<String> getProfileImageURL() {
-      return profile_image_url;
+      return profileImageUrl;
     }
-    public OptionalLong getTimestampMs() { return timestamp_ms; }
-    public OptionalLong getEpoch() { return epoch; }
+
+    public OptionalLong getTimestampMs() {
+      return timestampMs;
+    }
+
+    public OptionalLong getEpoch() {
+      return epoch;
+    }
+
     public Optional<String> getLang() {
       return lang;
     }
+
     public OptionalLong getInReplyToStatusId() {
       return inReplyToStatusId;
     }
+
     public OptionalLong getInReplyToUserId() {
       return inReplyToUserId;
     }
+
     public OptionalDouble getlatitude() {
       return latitude;
     }
+
     public OptionalDouble getLongitude() {
       return longitude;
     }
+
     public OptionalLong getRetweetedStatusId() {
       return retweetStatusId;
     }
+
     public OptionalLong getRetweetedUserId() {
       return retweetUserId;
     }
+
     public OptionalLong getRetweetCount() {
       return retweetCount;
     }
@@ -326,16 +353,16 @@ public class TweetCollection extends Collection {
     public static class TweetObject {
 
       // Required fields
-      protected String created_at;
-      protected String id_str;
+      protected String createdAt;
+      protected String idStr;
       protected String text;
       protected TweetObject.User user;
 
       // Optional fields
-      protected OptionalLong retweet_count;
-      protected OptionalLong in_reply_to_status_id;
-      protected OptionalLong in_reply_to_user_id;
-      protected Optional<TweetObject.RetweetedStatus> retweeted_status;
+      protected OptionalLong retweetCount;
+      protected OptionalLong inReplyToStatusId;
+      protected OptionalLong inReplyToUserId;
+      protected Optional<TweetObject.RetweetedStatus> retweetedStatus;
       protected Optional<String> lang;
       protected Optional<TweetObject.Delete> delete;
       protected Optional<TweetObject.Coordinates> coordinates;
@@ -343,11 +370,11 @@ public class TweetCollection extends Collection {
       // Must make inner classes static for deserialization in Jackson
       // http://www.cowtowncoder.com/blog/archives/2010/08/entry_411.html
       public static class Delete {
-        protected Optional<String> timestamp_ms;
+        protected Optional<String> timestampMs;
 
         @JsonGetter("timestamp_ms")
-        public Optional<String> timestamp_ms() {
-          return timestamp_ms;
+        public Optional<String> getTimestampMs() {
+          return timestampMs;
         }
       }
 
@@ -355,7 +382,7 @@ public class TweetCollection extends Collection {
         protected Optional<List<OptionalDouble>> coordinates;
 
         @JsonGetter("coordinates")
-        public Optional<List<OptionalDouble>> coordinates() { return coordinates; }
+        public Optional<List<OptionalDouble>> getCoordinates() { return coordinates; }
       }
 
       public static class RetweetedStatus {
@@ -363,157 +390,153 @@ public class TweetCollection extends Collection {
         protected Optional<TweetObject.User> user;
 
         @JsonGetter("id")
-        public OptionalLong id() {
+        public OptionalLong getId() {
           return id;
         }
 
         @JsonGetter("user")
-        public Optional<TweetObject.User> user() {
+        public Optional<TweetObject.User> getUser() {
           return user;
         }
       }
 
       public static class User {
         // Required fields
-        protected String screen_name;
-        protected int followers_count;
-        protected int friends_count;
-        protected int statuses_count;
+        protected String screenName;
+        protected int followersCount;
+        protected int friendsCount;
+        protected int statusesCount;
 
         // Opional fields
         protected Optional<String> name;
-        protected Optional<String> profile_image_url;
+        protected Optional<String> profileImageUrl;
         protected OptionalLong id;
 
         @JsonCreator
         public User(
-                @JsonProperty(value = "followers_count", required = true) int followers_count,
-                @JsonProperty(value = "friends_count", required = true) int friends_count,
-                @JsonProperty(value = "statuses_count", required = true) int statuses_count,
-                @JsonProperty(value = "screen_name", required = true) String screen_name) {
-          this.followers_count = followers_count;
-          this.friends_count = friends_count;
-          this.statuses_count = statuses_count;
-          this.screen_name = screen_name;
+                @JsonProperty(value = "followers_count", required = true) int followersCount,
+                @JsonProperty(value = "friends_count", required = true) int friendsCount,
+                @JsonProperty(value = "statuses_count", required = true) int statusesCount,
+                @JsonProperty(value = "screen_name", required = true) String screenName) {
+          this.followersCount = followersCount;
+          this.friendsCount = friendsCount;
+          this.statusesCount = statusesCount;
+          this.screenName = screenName;
         }
 
         @JsonGetter("screen_name")
-        public String screen_name() {
-          return screen_name;
+        public String getScreenName() {
+          return screenName;
         }
 
         @JsonGetter("followers_count")
-        public int followers_count() {
-          return followers_count;
+        public int getFollowersCount() {
+          return followersCount;
         }
 
         @JsonGetter("friends_count")
-        public int friends_count() {
-          return friends_count;
+        public int getFriendsCount() {
+          return friendsCount;
         }
 
         @JsonGetter("statuses_count")
-        public int statuses_count() {
-          return statuses_count;
+        public int getStatusesCount() {
+          return statusesCount;
         }
 
         @JsonGetter("name")
-        public Optional<String> name() {
+        public Optional<String> getName() {
           return name;
         }
 
         @JsonGetter("profile_image_url")
-        public Optional<String> profile_image_url() {
-          return profile_image_url;
+        public Optional<String> getProfileImageUrl() {
+          return profileImageUrl;
         }
 
         @JsonGetter("id")
-        public OptionalLong id() {
+        public OptionalLong getId() {
           return id;
         }
       }
 
       @JsonCreator
       public TweetObject(
-              @JsonProperty(value = "created_at", required = true) String created_at,
-              @JsonProperty(value = "id_str", required = true) String id_str,
+              @JsonProperty(value = "created_at", required = true) String createdAt,
+              @JsonProperty(value = "id_str", required = true) String idStr,
               @JsonProperty(value = "text", required = true) String text,
               @JsonProperty(value = "user", required = true) TweetObject.User user) {
-        this.created_at = created_at;
-        this.id_str = id_str;
+        this.createdAt = createdAt;
+        this.idStr = idStr;
         this.text = text;
         this.user = user;
       }
 
       @JsonGetter("id_str")
-      public String id_str() {
-        return id_str;
+      public String getIdStr() {
+        return idStr;
       }
 
       @JsonGetter("text")
-      public String text() {
+      public String getText() {
         return text;
       }
 
       @JsonGetter("user")
-      public TweetObject.User user() {
+      public TweetObject.User getUser() {
         return user;
       }
 
       @JsonGetter("created_at")
-      public String created_at() {
-        return created_at;
+      public String getCreatedAt() {
+        return createdAt;
       }
 
       @JsonGetter("retweet_count")
-      public OptionalLong retweet_count() {
-        return retweet_count;
+      public OptionalLong getRetweetCount() {
+        return retweetCount;
       }
 
       @JsonSetter("retweet_count")
-      public void set_retweet_count_internal(JsonNode retweet_count_internal) {
-        if (retweet_count_internal != null) {
-          if (retweet_count_internal.isTextual()) {
+      public void setRetweetCountInternal(JsonNode retweetCountInternal) {
+        if (retweetCountInternal != null) {
+          if (retweetCountInternal.isTextual()) {
             // retweet_count might say "100+"
             // TODO: This is ugly, come back and fix later.
-            retweet_count = OptionalLong.of(Long.parseLong(retweet_count_internal.asText().replace("+", "")));
-          } else if (retweet_count_internal.isNumber()) {
-            retweet_count = OptionalLong.of(retweet_count_internal.asLong());
+            retweetCount = OptionalLong.of(Long.parseLong(retweetCountInternal.asText().replace("+", "")));
+          } else if (retweetCountInternal.isNumber()) {
+            retweetCount = OptionalLong.of(retweetCountInternal.asLong());
           }
         }
       }
 
       @JsonGetter("in_reply_to_status_id")
-      public OptionalLong in_reply_to_status_id() {
-        return in_reply_to_status_id;
+      public OptionalLong getInReplyToStatusId() {
+        return inReplyToStatusId;
       }
 
       @JsonGetter("in_reply_to_user_id")
-      public OptionalLong in_reply_to_user_id() {
-        return in_reply_to_user_id;
+      public OptionalLong getInReplyToUserId() {
+        return inReplyToUserId;
       }
 
       @JsonGetter("retweeted_status")
-      public Optional<TweetObject.RetweetedStatus> retweeted_status() {
-        return retweeted_status;
+      public Optional<TweetObject.RetweetedStatus> getRetweetedStatus() {
+        return retweetedStatus;
       }
 
       @JsonGetter("lang")
-      public Optional<String> lang() {
+      public Optional<String> getLang() {
         return lang;
       }
 
       @JsonGetter("delete")
-      public Optional<TweetObject.Delete> delete() {
+      public Optional<TweetObject.Delete> getDelete() {
         return delete;
       }
 
       @JsonGetter("coordinates")
-      public Optional<TweetObject.Coordinates> coordinates() { return coordinates; }
+      public Optional<TweetObject.Coordinates> getCoordinates() { return coordinates; }
     }
-
-
   }
-
-
 }
