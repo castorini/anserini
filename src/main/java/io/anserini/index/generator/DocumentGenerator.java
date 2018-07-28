30846e8c74877c01df871dd382bb2b0d80e425be
Boris Lin <boruilin224@gmail.com>
2018-07-27 23:06:33 -0400

    Add MapCollection

---
+++ src/main/java/io/anserini/index/generator/DocumentGenerator.java
@@ -0,0 +1,153 @@
+/**
+ * Anserini: An information retrieval toolkit built on Lucene
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.anserini.index.generator;
+
+import io.anserini.collection.SourceDocument;
+import io.anserini.index.transform.StringTransform;
+import io.anserini.util.MapCollection;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.FieldType;
+import org.apache.lucene.document.SortedDocValuesField;
+import org.apache.lucene.document.StoredField;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.index.IndexOptions;
+import org.apache.lucene.util.BytesRef;
+
+/**
+ * Converts a {@link SourceDocument} into a Lucene {@link Document}, ready to be indexed.
+ * Prior to the creation of the Lucene document, this class will apply an optional
+ * {@link StringTransform} to, for example, clean HTML document.
+ *
+ * @param <T> type of the source document
+ */
+public class DocumentGenerator<T extends SourceDocument> {
+  private static final Logger LOG = LogManager.getLogger(DocumentGenerator.class);
+
+  public static final String FIELD_RAW = "raw";
+  public static final String FIELD_BODY = "contents";
+  public static final String FIELD_ID = "id";
+
+  private final StringTransform transform;
+
+  protected MapCollection.Counters counters;
+  protected MapCollection.Args args;
+
+  /**
+   * Default constructor.
+   */
+  public DocumentGenerator() {
+    this.transform = null;
+  }
+
+  /**
+   * Constructor to specify optional {@link StringTransform}.
+   *
+   * @param transform string transform to apply
+   */
+  public DocumentGenerator(StringTransform transform) {
+    this.transform = transform;
+  }
+
+  /**
+   * Constructor with config and counters
+   *
+   * @param args configuration arguments
+   * @param counters counters
+   */
+  public DocumentGenerator(MapCollection.Args args, MapCollection.Counters counters) {
+    this.transform = null;
+    config(args);
+    setCounters(counters);
+  }
+
+  /**
+   * Constructor with config and counters
+   *
+   * @param transform string transform to apply
+   * @param args configuration arguments
+   * @param counters counters
+   */
+  public DocumentGenerator(StringTransform transform, MapCollection.Args args,
+      MapCollection.Counters counters) {
+    this.transform = transform;
+    config(args);
+    setCounters(counters);
+  }
+
+  public void config(MapCollection.Args args) {
+    this.args = args;
+  }
+
+  public void setCounters(MapCollection.Counters counters) {
+    this.counters = counters;
+  }
+
+  public Document createDocument(T src) {
+    String id = src.id();
+    String contents;
+
+    try {
+      // If there's a transform, use it.
+      contents = transform != null ? transform.apply(src.content()) : src.content();
+    } catch (Exception e) {
+      LOG.error("Error extracting document text, skipping document: " + id, e);
+      counters.errors.incrementAndGet();
+      return null;
+    }
+
+    if (contents.trim().length() == 0) {
+      // Log and move on, otherwise output can be very noisy.
+      counters.empty.incrementAndGet();
+      return null;
+    }
+
+    // Make a new, empty document.
+    Document document = new Document();
+
+    // Store the collection docid.
+    document.add(new StringField(FIELD_ID, id, Field.Store.YES));
+    // This is needed to break score ties by docid.
+    document.add(new SortedDocValuesField(FIELD_ID, new BytesRef(id)));
+
+    if (args.storeRawDocs) {
+      document.add(new StoredField(FIELD_RAW, src.content()));
+    }
+
+    FieldType fieldType = new FieldType();
+    fieldType.setStored(args.storeTransformedDocs);
+
+    // Are we storing document vectors?
+    if (args.storeDocvectors) {
+      fieldType.setStoreTermVectors(true);
+      fieldType.setStoreTermVectorPositions(true);
+    }
+
+    // Are we building a "positional" or "count" index?
+    if (args.storePositions) {
+      fieldType.setIndexOptions(IndexOptions.DOCS_AND_FREQS_AND_POSITIONS);
+    } else {
+      fieldType.setIndexOptions(IndexOptions.DOCS_AND_FREQS);
+    }
+
+    document.add(new Field(FIELD_BODY, contents, fieldType));
+
+    return document;
+  }
+}
---
+++ src/main/java/io/anserini/util/MapCollection.java
@@ -0,0 +1,386 @@
+/**
+ * Anserini: An information retrieval toolkit built on Lucene
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.anserini.util;
+
+
+import io.anserini.analysis.TweetAnalyzer;
+import io.anserini.collection.AbstractFileSegment;
+import io.anserini.collection.DocumentCollection;
+import io.anserini.collection.FileSegmentProvider;
+import io.anserini.collection.SourceDocument;
+import io.anserini.index.generator.DocumentGenerator;
+import io.anserini.index.generator.LuceneDocumentGenerator;
+
+import io.anserini.index.transform.JsoupStringTransform;
+import io.anserini.index.transform.StringTransform;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.time.DurationFormatUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.lucene.analysis.CharArraySet;
+import org.apache.lucene.analysis.en.EnglishAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.ConcurrentMergeScheduler;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.search.similarities.BM25Similarity;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.OptionHandlerFilter;
+import org.kohsuke.args4j.ParserProperties;
+import org.kohsuke.args4j.Option;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Set;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+public final class MapCollection {
+  private static final Logger LOG = LogManager.getLogger(MapCollection.class);
+
+  public static final class Args {
+
+    // required arguments
+
+    @Option(name = "-input", metaVar = "[Directory]", required = true, usage = "collection directory")
+    public String input;
+
+//    @Option(name = "-index", metaVar = "[Path]", required = true, usage = "index path")
+//    public String index;
+
+    @Option(name = "-threads", metaVar = "[Number]", required = true, usage = "Number of Threads")
+    public int threads;
+
+    @Option(name = "-collection", required = true, usage = "collection class in io.anserini.collection")
+    public String collectionClass;
+
+    @Option(name = "-generator", required = true, usage = "document generator in io.anserini.index.generator")
+    public String generatorClass;
+
+    // optional arguments
+    @Option(name = "-storePositions", usage = "boolean switch to index storePositions")
+    public boolean storePositions = false;
+
+    @Option(name = "-storeDocvectors", usage = "boolean switch to store document vectors")
+    public boolean storeDocvectors = false;
+
+    @Option(name = "-storeTransformedDocs", usage = "boolean switch to store transformed document text")
+    public boolean storeTransformedDocs = false;
+
+    @Option(name = "-storeRawDocs", usage = "boolean switch to store raw document text")
+    public boolean storeRawDocs = false;
+
+    @Option(name = "-optimize", usage = "boolean switch to optimize index (force merge)")
+    public boolean optimize = false;
+
+    @Option(name = "-keepStopwords", usage = "boolean switch to keep stopwords")
+    public boolean keepStopwords = false;
+
+    @Option(name = "-uniqueDocid", usage = "remove duplicated documents with the same doc id when indexing. " +
+            "please note that this option may slow the indexing a lot and if you are sure there is no " +
+            "duplicated document ids in the corpus you shouldn't use this option.")
+    public boolean uniqueDocid = false;
+
+    @Option(name = "-memorybuffer", usage = "memory buffer size")
+    public int memorybufferSize = 2048;
+
+    @Option(name = "-whitelist", usage = "file containing docids, one per line; only specified docids will be indexed.")
+    public String whitelist = null;
+
+    @Option(name = "-tweet.keepRetweets", usage = "boolean switch to keep retweets while indexing")
+    public boolean tweetKeepRetweets = false;
+    @Option(name = "-tweet.keepUrls", usage = "boolean switch to keep URLs while indexing tweets")
+    public boolean tweetKeepUrls = false;
+    @Option(name = "-tweet.stemming", usage = "boolean switch to apply Porter stemming while indexing tweets")
+    public boolean tweetStemming = false;
+    @Option(name = "-tweet.maxId", usage = "the max tweet Id for indexing. Tweet Ids that are larger " +
+            " (when being parsed to Long type) than this value will NOT be indexed")
+    public long tweetMaxId = Long.MAX_VALUE;
+    @Option(name = "-tweet.deletedIdsFile", metaVar = "[Path]",
+            usage = "a file that contains deleted tweetIds, one per line. these tweeets won't be indexed")
+    public String tweetDeletedIdsFile = "";
+  }
+
+  public final class Counters {
+    /**
+     * Counter for successfully indexed documents.
+     */
+    public AtomicLong indexed = new AtomicLong();
+
+    /**
+     * Counter for empty documents that are not indexed. Empty documents are not necessary errors;
+     * it could be the case, for example, that a document is comprised solely of stopwords.
+     */
+    public AtomicLong empty = new AtomicLong();
+
+    /**
+     * Counter for unindexed documents. These are cases where the {@link SourceDocument} returned
+     * by {@link AbstractFileSegment} is {@code null} or the {@link LuceneDocumentGenerator}
+     * returned {@code null}. These are not necessarily errors.
+     */
+    public AtomicLong unindexed = new AtomicLong();
+
+    /**
+     * Counter for unindexable documents. These are cases where {@link SourceDocument#indexable()}
+     * returns false.
+     */
+    public AtomicLong unindexable = new AtomicLong();
+
+    /**
+     * Counter for skipped documents. These are cases documents are skipped as part of normal
+     * processing logic, e.g., using a whitelist, not indexing retweets or deleted tweets.
+     */
+    public AtomicLong skipped = new AtomicLong();
+
+    /**
+     * Counter for unexpected errors.
+     */
+    public AtomicLong errors = new AtomicLong();
+  }
+
+  private final class MapperThread extends Thread {
+    final private Path inputFile;
+    //    final private IndexWriter writer;
+    final private DocumentCollection collection;
+
+    private MapperThread(DocumentCollection collection, Path inputFile) throws IOException {
+//      this.writer = writer;
+      this.collection = collection;
+      this.inputFile = inputFile;
+      setName(inputFile.getFileName().toString());
+    }
+
+    @Override
+    public void run() {
+      try {
+        @SuppressWarnings("unchecked")
+        DocumentGenerator generator =
+                (DocumentGenerator) generatorClass
+                        .getDeclaredConstructor(StringTransform.class, Args.class, Counters.class)
+                        .newInstance(new JsoupStringTransform(), args, counters);
+
+        int cnt = 0;
+        AbstractFileSegment iter = ((FileSegmentProvider) collection).createFileSegment(inputFile);
+        while (true) {
+          boolean hasNext = false;
+          try {
+            hasNext = iter.hasNext();
+          } catch (NoSuchElementException e1) {
+            break;
+          } catch (Exception e2) {
+            LOG.warn("Exception when parsing document: ", e2);
+            counters.errors.incrementAndGet();
+            continue;
+          }
+
+          if (!hasNext) {
+            break;
+          }
+
+          SourceDocument d = iter.next();
+
+          if (!d.indexable()) {
+            counters.unindexable.incrementAndGet();
+            continue;
+          }
+
+          @SuppressWarnings("unchecked") // Yes, we know what we're doing here.
+                  Document doc = generator.createDocument(d);
+          if (doc == null) {
+            counters.unindexed.incrementAndGet();
+            continue;
+          }
+
+          if (whitelistDocids != null && !whitelistDocids.contains(d.id())) {
+            counters.skipped.incrementAndGet();
+            continue;
+          }
+
+//          if (args.uniqueDocid) {
+//            writer.updateDocument(new Term("id", d.id()), doc);
+//          } else {
+//            writer.addDocument(doc);
+//          }
+          cnt++;
+        }
+
+        iter.close();
+        LOG.info(inputFile.getParent().getFileName().toString() + File.separator +
+                inputFile.getFileName().toString() + ": " + cnt + " docs added.");
+        counters.indexed.addAndGet(cnt);
+      } catch (Exception e) {
+        LOG.error(Thread.currentThread().getName() + ": Unexpected Exception:", e);
+      }
+    }
+  }
+
+  private final Args args;
+  private final Path collectionPath;
+  private final Set whitelistDocids;
+  private final Class collectionClass;
+  private final Class generatorClass;
+  private final DocumentCollection collection;
+  private final Counters counters;
+
+  public MapCollection(Args args) throws Exception {
+    this.args = args;
+
+    LOG.info("DocumentCollection path: " + args.input);
+    LOG.info("CollectionClass: " + args.collectionClass);
+    LOG.info("Generator: " + args.generatorClass);
+    LOG.info("Threads: " + args.threads);
+    LOG.info("Keep stopwords? " + args.keepStopwords);
+    LOG.info("Store positions? " + args.storePositions);
+    LOG.info("Store docvectors? " + args.storeDocvectors);
+    LOG.info("Store transformed docs? " + args.storeTransformedDocs);
+    LOG.info("Store raw docs? " + args.storeRawDocs);
+    LOG.info("Optimize (merge segments)? " + args.optimize);
+    LOG.info("Whitelist: " + args.whitelist);
+
+    collectionPath = Paths.get(args.input);
+    if (!Files.exists(collectionPath) || !Files.isReadable(collectionPath) || !Files.isDirectory(collectionPath)) {
+      throw new RuntimeException("Document directory " + collectionPath.toString() +
+              " does not exist or is not readable, please check the path");
+    }
+
+    this.generatorClass = Class.forName("io.anserini.index.generator." + args.generatorClass);
+    this.collectionClass = Class.forName("io.anserini.collection." + args.collectionClass);
+
+    collection = (DocumentCollection) this.collectionClass.newInstance();
+    collection.setCollectionPath(collectionPath);
+
+    if (args.whitelist != null) {
+      List<String> lines = FileUtils.readLines(new File(args.whitelist), "utf-8");
+      this.whitelistDocids = new HashSet<>(lines);
+    } else {
+      this.whitelistDocids = null;
+    }
+
+    this.counters = new Counters();
+  }
+
+  public void run() throws IOException, InterruptedException {
+    final long start = System.nanoTime();
+    LOG.info("Starting mapper...");
+
+    int numThreads = args.threads;
+
+//    final EnglishAnalyzer englishAnalyzer= args.keepStopwords ?
+//            new EnglishAnalyzer(CharArraySet.EMPTY_SET) : new EnglishAnalyzer();
+//    final TweetAnalyzer tweetAnalyzer = new TweetAnalyzer(args.tweetStemming);
+//    final IndexWriterConfig config = args.collectionClass.equals("TweetCollection") ?
+//            new IndexWriterConfig(tweetAnalyzer) : new IndexWriterConfig(englishAnalyzer);
+//    config.setSimilarity(new BM25Similarity());
+//    config.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
+//    config.setRAMBufferSizeMB(args.memorybufferSize);
+//    config.setUseCompoundFile(false);
+//    config.setMergeScheduler(new ConcurrentMergeScheduler());
+
+//    final IndexWriter writer = new IndexWriter(dir, config);
+
+    final ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(numThreads);
+    final List segmentPaths = ((FileSegmentProvider) collection).getFileSegmentPaths();
+
+    final int segmentCnt = segmentPaths.size();
+    LOG.info(segmentCnt + " files found in " + collectionPath.toString());
+    for (int i = 0; i < segmentCnt; i++) {
+      executor.execute(new MapCollection.MapperThread(collection, (Path) segmentPaths.get(i)));
+    }
+
+    executor.shutdown();
+
+    try {
+      // Wait for existing tasks to terminate
+      while (!executor.awaitTermination(1, TimeUnit.MINUTES)) {
+        LOG.info(String.format("%.2f percent completed",
+                (double) executor.getCompletedTaskCount() / segmentCnt * 100.0d));
+      }
+    } catch (InterruptedException ie) {
+      // (Re-)Cancel if current thread also interrupted
+      executor.shutdownNow();
+      // Preserve interrupt status
+      Thread.currentThread().interrupt();
+    }
+
+    if (segmentCnt != executor.getCompletedTaskCount()) {
+      throw new RuntimeException("totalFiles = " + segmentCnt +
+              " is not equal to completedTaskCount =  " + executor.getCompletedTaskCount());
+    }
+
+//    int numIndexed = writer.maxDoc();
+//
+//    try {
+//      writer.commit();
+//      if (args.optimize)
+//        writer.forceMerge(1);
+//    } finally {
+//      try {
+//        writer.close();
+//      } catch (IOException e) {
+//        // It is possible that this happens... but nothing much we can do at this point,
+//        // so just log the error and move on.
+//        LOG.error(e);
+//      }
+//    }
+
+//    if (numIndexed != counters.indexed.get()) {
+//      LOG.warn("Unexpected difference between number of indexed documents and index maxDoc.");
+//    }
+
+    LOG.info("# Final Counter Values");
+    LOG.info(String.format("indexed:     %,12d", counters.indexed.get()));
+    LOG.info(String.format("empty:       %,12d", counters.empty.get()));
+    LOG.info(String.format("unindexed:   %,12d", counters.unindexed.get()));
+    LOG.info(String.format("unindexable: %,12d", counters.unindexable.get()));
+    LOG.info(String.format("skipped:     %,12d", counters.skipped.get()));
+    LOG.info(String.format("errors:      %,12d", counters.errors.get()));
+
+    final long durationMillis = TimeUnit.MILLISECONDS.convert(System.nanoTime() - start, TimeUnit.NANOSECONDS);
+    LOG.info(String.format("Total %,d documents indexed in %s", counters.indexed.get(),
+            DurationFormatUtils.formatDuration(durationMillis, "HH:mm:ss")));
+  }
+
+  public static void main(String[] args) throws Exception {
+    Args mapCollectionArgs = new Args();
+    CmdLineParser parser = new CmdLineParser(mapCollectionArgs, ParserProperties.defaults().withUsageWidth(90));
+
+    try {
+      parser.parseArgument(args);
+    } catch (CmdLineException e) {
+      System.err.println(e.getMessage());
+      parser.printUsage(System.err);
+      System.err.println("Example: "+ MapCollection.class.getSimpleName() +
+              parser.printExample(OptionHandlerFilter.REQUIRED));
+      return;
+    }
+
+    new MapCollection(mapCollectionArgs).run();
+  }
+}
+
