ee4285ea1e3542d3f472f511aba79259dedf353f
Boris Lin <boruilin224@gmail.com>
2018-08-05 03:08:16 -0400

    Fix document unit tests

--- src/test/java/io/anserini/collection/TrecDocumentTest.java
+++ src/test/java/io/anserini/collection/TrecDocumentTest.java
@@ -66,13 +66,20 @@ public class TrecDocumentTest extends DocumentTest {
     for (int i = 0; i < rawFiles.size(); i++) {
       BaseFileSegment<TrecCollection.Document> iter = collection.createFileSegment(rawFiles.get(i));
       while (true) {
+        boolean hasNext;
         try {
-          TrecCollection.Document parsed = iter.next();
-          assertEquals(parsed.id(), expected.get(i).get("id"));
-          assertEquals(parsed.content(), expected.get(i).get("content"));
+          hasNext = iter.hasNext();
         } catch (NoSuchElementException e) {
           break;
         }
+
+        if (!hasNext) {
+          break;
+        }
+
+        TrecCollection.Document parsed = iter.next();
+        assertEquals(parsed.id(), expected.get(i).get("id"));
+        assertEquals(parsed.content(), expected.get(i).get("content"));
       }
     }
   }
