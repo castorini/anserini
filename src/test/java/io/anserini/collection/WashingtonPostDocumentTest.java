7c77f799d61908f404f271f0ed6eba56584a6945
Yuhao Xie (Kyle) <Kytabyte@users.noreply.github.com>
2018-08-07 09:53:29 -0400

    More on #375: added title and fullcaption (#390)

--- src/test/java/io/anserini/collection/WashingtonPostDocumentTest.java
+++ src/test/java/io/anserini/collection/WashingtonPostDocumentTest.java
@@ -61,7 +61,8 @@ public class WashingtonPostDocumentTest extends DocumentTest {
     HashMap<String, String> doc1 = new HashMap<>();
     doc1.put("id", "5f992bbc-4b9f-11e2-a6a6-aabac85e8036");
     // Only "sanitized_html" and "tweet" of <type> subtag in <content> tag will be included
-    doc1.put("content", "Using light to help reset your body clock\n" +
+    doc1.put("content", "Controlled exposure to light can ease jet lag’s effects before and after a trip\n" +
+             "Using light to help reset your body clock\n" +
              "When traveling east:\n" +
              "A few days before you leave, start exposing yourself to bright light in the morning.\n" +
              "When traveling west:\n" +
