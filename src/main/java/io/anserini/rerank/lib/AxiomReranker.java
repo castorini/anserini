f0d22c4b1aa520ea5d8be19af3f2511d374a6f79
Jimmy Lin <jimmylin@uwaterloo.ca>
2018-09-23 10:06:57 -0400

    Tweaked tagline; made sure all files have license headers (#439)
    Addresses #400

--- src/main/java/io/anserini/rerank/lib/AxiomReranker.java
+++ src/main/java/io/anserini/rerank/lib/AxiomReranker.java
@@ -1,5 +1,5 @@
 /**
- * Anserini: An information retrieval toolkit built on Lucene
+ * Anserini: A toolkit for reproducible information retrieval research built on Lucene
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
