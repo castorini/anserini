1d20367af3fb202d41bc71ee16062b5ce6e6f291
Jimmy Lin <jimmylin@uwaterloo.ca>
2018-09-23 10:06:57 -0400

    Tweaked tagline; made sure all files have license headers (#439)
    Addresses #400

--- src/main/java/io/anserini/eval/Eval.java
+++ src/main/java/io/anserini/eval/Eval.java
@@ -1,14 +1,11 @@
-package io.anserini.eval;
-
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+/**
+ * Anserini: A toolkit for reproducible information retrieval research built on Lucene
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,6 +13,8 @@ package io.anserini.eval;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package io.anserini.eval;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
