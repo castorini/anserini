7922b4eb704a5e016d5552b2637e32526da6fb24
Peilin Yang <peiliny@twitter.com>
2018-10-22 10:42:46 -0700

    update unit test

--- src/test/java/io/anserini/util/FeatureVectorTest.java
+++ src/test/java/io/anserini/util/FeatureVectorTest.java
@@ -8,17 +8,23 @@ import java.util.Arrays;
 import java.util.HashSet;
 
 public class FeatureVectorTest extends LuceneTestCase {
-  @Test
-  public void pruneToSizeTest() {
+  private final FeatureVector createAndAddFeatureWeights() {
     FeatureVector fv = new FeatureVector();
     fv.addFeatureWeight("a", 0.2f);
     fv.addFeatureWeight("a", 0.3f);
     fv.addFeatureWeight("b", 0.3f);
     fv.addFeatureWeight("b", 0.5f);
     fv.addFeatureWeight("c", 0.4f);
+    return fv;
+  }
   
-    assertEquals(fv.pruneToSize(2).getFeatures().size(), 2);
-    assertEquals(fv.pruneToSize(1).getFeatures().size(), 1);
-    assertEquals(fv.pruneToSize(2).getFeatures(), new HashSet<>(Arrays.asList(new String[]{"a", "b"})));
+  @Test
+  public void pruneToSizeTest() {
+    FeatureVector fv1 = createAndAddFeatureWeights();
+    assertEquals(fv1.pruneToSize(2).getFeatures().size(), 2);
+    FeatureVector fv2 = createAndAddFeatureWeights();
+    assertEquals(fv2.pruneToSize(1).getFeatures().size(), 1);
+    FeatureVector fv3 = createAndAddFeatureWeights();
+    assertEquals(fv3.pruneToSize(2).getFeatures(), new HashSet<>(Arrays.asList(new String[]{"a", "b"})));
   }
 }
