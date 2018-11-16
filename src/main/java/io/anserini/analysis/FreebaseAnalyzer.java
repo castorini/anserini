f426d71ea7ca04ce222ad296bdd1add58fb6a8f7
Shi Peng <spone.2014@gmail.com>
2018-11-16 16:17:53 -0500

    Add license header and comments

--- src/main/java/io/anserini/analysis/FreebaseAnalyzer.java
+++ src/main/java/io/anserini/analysis/FreebaseAnalyzer.java
@@ -1,3 +1,19 @@
+/**
+ * Anserini: A toolkit for reproducible information retrieval research built on Lucene
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.anserini.analysis;
 
 import org.apache.lucene.analysis.CharArraySet;
@@ -40,7 +56,9 @@ public final class FreebaseAnalyzer extends StopwordAnalyzerBase {
     EnglishPossessiveFilter result2 = new EnglishPossessiveFilter(result);
     LowerCaseFilter result3 = new LowerCaseFilter(result2);
     Object result4 = new StopFilter(result3, this.stopwords);
-    result4 = new ASCIIFoldingFilter((TokenStream) result4);;
+    /* ASCIIFoldingFilter is used for accent folding. This will norma. ize the characters
+    * with accent such as latin characters, which enhances the search */
+    result4 = new ASCIIFoldingFilter((TokenStream) result4);
     if(!this.stemExclusionSet.isEmpty()) {
       result4 = new SetKeywordMarkerFilter((TokenStream)result4, this.stemExclusionSet);
     }
