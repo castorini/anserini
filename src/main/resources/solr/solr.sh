f52ecbcf93a8a443dfc04392541959e7f740c08a
Ryan Clancy <ryan.clancy@uwaterloo.ca>
2019-04-23 11:59:50 -0400

    Archive lang dirs and update solr.sh script.

--- src/main/resources/solr/solr.sh
+++ src/main/resources/solr/solr.sh
@@ -7,7 +7,7 @@ SOLR_VERSION=7.6.0
 SOLR_DIR=solr-${SOLR_VERSION}
 
 # Download Solr
-wget "http://mirror.csclub.uwaterloo.ca/apache/lucene/solr/$SOLR_VERSION/solr-$SOLR_VERSION.tgz" -O solr.tgz
+wget "https://archive.apache.org/dist/lucene/solr/$SOLR_VERSION/solr-$SOLR_VERSION.tgz" -O solr.tgz
 
 # Extract archive
 tar -zxvf solr.tgz
@@ -15,10 +15,14 @@ tar -zxvf solr.tgz
 # Copy anserini into lib dir
 mkdir ${SOLR_DIR}/lib && cp ././../../../../target/anserini-*-fatjar.jar ${SOLR_DIR}/lib
 
-# Copy configsets
-cp -r anserini ${SOLR_DIR}/server/solr/configsets
-cp -r anserini-twitter ${SOLR_DIR}/server/solr/configsets
+# Extract lang.zip in each configset
+unzip anserini/conf/lang.zip -d anserini/conf
+unzip anserini-twitter/conf/lang.zip -d anserini-twitter/conf
 
 echo "###"
-echo "# To start Solr, run: $SOLR_DIR/bin/solr start -c -m 8G"
+echo "# Start Solr: $SOLR_DIR/bin/solr start -c -m 8G"
+echo "# Import configsets:"
+echo "#  - $SOLR_DIR/bin/solr zk -z localhost:9983 upconfig -n anserini -d anserini"
+echo "#  - $SOLR_DIR/bin/solr zk -z localhost:9983 upconfig -n anserini-twitter -d anserini-twitter"
+echo "# Browse Solr: http://localhost:8983"
 echo "###"
