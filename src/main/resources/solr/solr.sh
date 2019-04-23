b6b3c32afe1df9b061c592aabe750cfba963eb63
Ryan Clancy <ryan.clancy@uwaterloo.ca>
2019-04-23 13:02:41 -0400

    Update script to assume Solr is already present

--- src/main/resources/solr/solr.sh
+++ src/main/resources/solr/solr.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
 
-# Solr version to download (i.e., 7.6.0)
-SOLR_VERSION=7.6.0
+###
+# This script assumes a single-node SolrCloud instance is running locally.
+###
 
-# Solr directory name
-SOLR_DIR=solr-${SOLR_VERSION}
+# Solr install directory
+SOLR_DIR=$1
 
-# Download Solr
-wget "https://archive.apache.org/dist/lucene/solr/$SOLR_VERSION/solr-$SOLR_VERSION.tgz" -O solr.tgz
-
-# Extract archive
-tar -zxvf solr.tgz
+# Solr's ZooKeeper URL
+ZOOKEEPER_URL=$2
 
 # Copy anserini into lib dir
 mkdir ${SOLR_DIR}/lib && cp ././../../../../target/anserini-*-fatjar.jar ${SOLR_DIR}/lib
@@ -19,10 +17,6 @@ mkdir ${SOLR_DIR}/lib && cp ././../../../../target/anserini-*-fatjar.jar ${SOLR_
 unzip anserini/conf/lang.zip -d anserini/conf
 unzip anserini-twitter/conf/lang.zip -d anserini-twitter/conf
 
-echo "###"
-echo "# 1) Start Solr: $SOLR_DIR/bin/solr start -c -m 8G"
-echo "# 2) Import configsets:"
-echo "#     - $SOLR_DIR/bin/solr zk -z localhost:9983 upconfig -n anserini -d anserini"
-echo "#     - $SOLR_DIR/bin/solr zk -z localhost:9983 upconfig -n anserini-twitter -d anserini-twitter"
-echo "# 3) Browse Solr: http://localhost:8983"
-echo "###"
+# Upload configset to Solr
+$SOLR_DIR/bin/solr zk -z ${ZOOKEEPER_URL:-localhost:9983} upconfig -n anserini -d anserini
+$SOLR_DIR/bin/solr zk -z ${ZOOKEEPER_URL:-localhost:9983} upconfig -n anserini-twitter -d anserini-twitter
