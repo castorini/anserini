fc1d4ae3b44c506bab83b3df80842141c232a41f
lintool <jimmylin@uwaterloo.ca>
2019-04-26 20:55:00 -0400

    Initial WebTopicReader commit.

---
+++ src/main/java/io/anserini/search/topicreader/WebTopicReader.java
@@ -0,0 +1,48 @@
+/**
+ * Anserini: A toolkit for reproducible information retrieval research built on Lucene
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.anserini.search.topicreader;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class WebTopicReader extends TopicReader {
+  public WebTopicReader(Path topicFile) {
+    super(topicFile);
+  }
+
+  @Override
+  public SortedMap<Integer, Map<String, String>> read(BufferedReader bRdr) throws IOException {
+    SortedMap<Integer, Map<String, String>> map = new TreeMap<>();
+
+    String line;
+    while ((line = bRdr.readLine()) != null) {
+      line = line.trim();
+      String[] arr = line.split(":");
+
+      Map<String,String> fields = new HashMap<>();
+      fields.put("title", arr[1]);
+      map.put(Integer.valueOf(arr[0]), fields);
+    }
+
+    return map;
+  }
+}
