b0e815d5c1bf8fb5c7e0387bd08f2e66db249ff1
lintool <jimmylin@uwaterloo.ca>
2019-04-28 13:48:53 -0400

    Refactoring BM25 tuning script; parameterized various options.

--- src/main/python/msmarco/tune_bm25.py
+++ src/main/python/msmarco/tune_bm25.py
@@ -22,31 +22,54 @@ limitations under the License.
 #
 # Compared to default Anserini parameters of b1=0.9, k=0.4, MRR@10 = 0.18388092964024202
 
+import argparse
 import os
-import subprocess
 import re
+import subprocess
 
-base_directory = 'msmarco_data/bm25_tuning'
-index = 'msmarco_data/lucene-index-msmarco'
+parser = argparse.ArgumentParser(description='Retrieve MS MARCO Passages')
+parser.add_argument('--base_directory', required=True, help='base directory for storing runs')
+parser.add_argument('--index', required=True, help='index to use')
+parser.add_argument('--queries', required=True, help='queries for evaluation')
+parser.add_argument('--qrels', required=True, help='qrels for evaluation')
 
-for b1 in [0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2]:
-    for k in [0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
+args = parser.parse_args()
+
+base_directory = args.base_directory
+index = args.index
+qrels = args.qrels
+queries = args.queries
+
+print('# Settings')
+print('base directory: {}'.format(base_directory))
+print('index: {}'.format(index))
+print('queries: {}'.format(queries))
+print('qrels: {}'.format(qrels))
+print('\n')
+
+# For an initial parameter search, the following ranges are a good start:
+#   b1 in [0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2]
+#   k  in [0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+#
+# However, based on results from above, we know the good settings are somewhere here:
+for b1 in [0.50, 0.55, 0.60, 0.65, 0.70]:
+    for k in [0.70, 0.75, 0.80, 0.85, 0.90]:
         print('Trying... b1 = {}, k = {}'.format(b1, k))
         filename = 'run.bm25.b1_{}.k_{}.txt'.format(b1, k)
         if os.path.isfile('{}/{}'.format(base_directory, filename)):
            print('Run already exists, skipping!')
         else:
            subprocess.call('python src/main/python/msmarco/retrieve.py \
-               --index {} --qid_queries msmarco_data/queries.dev.small.tsv \
-               --output {}/{} --b1 {} --k {} --hits 1000'.format(index, base_directory, filename, b1, k), shell=True)
+               --index {} --qid_queries {} --output {}/{} \
+               --b1 {} --k {} --hits 1000'.format(index, queries, base_directory, filename, b1, k), shell=True)
 
 print('\n\nStarting evaluation...')
 
 max_score = 0
 max_file = ''
-for filename in os.listdir(base_directory):
+for filename in sorted(os.listdir(base_directory)):
    results = subprocess.check_output(['python', 'src/main/python/msmarco/msmarco_eval.py', \
-       'msmarco_data/qrels.dev.small.tsv', '{}/{}'.format(base_directory, filename)])
+       '{}'.format(qrels), '{}/{}'.format(base_directory, filename)])
    match = re.search('MRR @10: ([\d.]+)', results.decode('utf-8'))
    score = float(match.group(1))
    print('{}: MRR@10 = {}'.format(filename, score))
