d7c6ac1c00629b21999bf4238c3e50cfd7d0f3b7
Jimmy Lin <jimmylin@uwaterloo.ca>
2019-04-30 14:33:43 -0400

    MS MARCO scripts reformatting (#616)
    + converted scripts to PEP8 formatting
    + change use to argparse

--- src/main/python/msmarco/filter_queries.py
+++ src/main/python/msmarco/filter_queries.py
@@ -1,34 +1,41 @@
-'''Keeps only queries that are in the qrels file.'''
-from absl import app
-from absl import flags
+# -*- coding: utf-8 -*-
+"""
+Anserini: A Lucene toolkit for replicable information retrieval research
 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-FLAGS = flags.FLAGS
+http://www.apache.org/licenses/LICENSE-2.0
 
-flags.DEFINE_string('qrels', None, 'MS MARCO .tsv qrels file.')
-flags.DEFINE_string('queries', None, 'queries file.')
-flags.DEFINE_string('output_queries', None, 'Path to write the queries file.')
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 
-
-def main(_):
-  qrels = set()
-  with open(FLAGS.qrels) as f:
-    for line in f:
-      query_id, _, _, _ = line.rstrip().split('\t')
-      qrels.add(query_id)
-
-  with open(FLAGS.output_queries, 'w') as fout:
-    with open(FLAGS.queries) as f:
-      for line in f:
-        query_id, _ = line.rstrip().split('\t')
-        if query_id in qrels:
-          fout.write(line)
-
-  print('Done!')
-
+import argparse
 
 if __name__ == '__main__':
-  flags.mark_flag_as_required('qrels')
-  flags.mark_flag_as_required('queries')
-  flags.mark_flag_as_required('output_queries')
-  app.run(main)
+    parser = argparse.ArgumentParser(description='Keeps only queries that are in the qrels file.')
+    parser.add_argument('--qrels', required=True, help='MS MARCO .tsv qrels file')
+    parser.add_argument('--queries', required=True, help='queries file')
+    parser.add_argument('--output_queries', required=True, help='path to write the queries file')
+
+    args = parser.parse_args()
+
+    qrels = set()
+    with open(args.qrels) as f:
+        for line in f:
+            query_id, _, _, _ = line.rstrip().split('\t')
+            qrels.add(query_id)
+
+    with open(args.output_queries, 'w') as fout:
+        with open(args.queries) as f:
+            for line in f:
+                query_id, _ = line.rstrip().split('\t')
+                if query_id in qrels:
+                    fout.write(line)
+
+    print('Done!')
