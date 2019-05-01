3eef6be6b876bad4a3805ff0b67603cd145b98c4
Jimmy Lin <jimmylin@uwaterloo.ca>
2019-05-01 17:07:58 -0400

    Refactor script to augment original collection with query predictions (#622)
    + changed to PEP 8 formatting
    + added --original_copies - this allows us to script weighted expansion experiments

--- src/main/python/msmarco/augment_collection_with_predictions.py
+++ src/main/python/msmarco/augment_collection_with_predictions.py
@@ -1,72 +1,72 @@
-'''Converts MSMARCO's tsv collection to Anserini jsonl files.'''
+# -*- coding: utf-8 -*-
+"""
+Anserini: A Lucene toolkit for replicable information retrieval research
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 import json
 import os
-from absl import app
-from absl import flags
+import argparse
 
+def convert_collection(args):
+    print('Converting collection...')
 
-FLAGS = flags.FLAGS
+    predictions_file = open(args.predictions)
+    file_index = 0
+    with open(args.collection_path) as f:
+        for i, line in enumerate(f):
+            # Start writting to a new file whent the current one reached its maximum capacity. 
+            if i % args.max_docs_per_file == 0:
+                if i > 0:
+                    output_jsonl_file.close()
+                output_path = os.path.join(args.output_folder, 'docs{:02d}.json'.format(file_index))
+                output_jsonl_file = open(output_path, 'w')
+                file_index += 1
 
-flags.DEFINE_string('collection_path', None, 
-                    'MS MARCO .tsv collection file.')
-flags.DEFINE_string('predictions', None, 
-                    'Query Predictions that will be attached to documents.')
-flags.DEFINE_string('output_folder', None, 'Folder to write the jsonl files.')
-flags.DEFINE_integer('beam_size', None, 
-                    'Number of predictions made per document in the predictions file.')
-flags.DEFINE_integer('max_docs_per_file', 1000000, 
-                     'Maximum number of documents in each jsonl file.')
+            doc_id, doc_text = line.rstrip().split('\t')
 
-
-def convert_collection():
-  print('Converting collection...')
-
-  predictions_file = open(FLAGS.predictions)
-  file_index = 0
-  with open(FLAGS.collection_path) as f:
-    for i, line in enumerate(f):
-
-      # Start writting to a new file whent the current one reached its maximum 
-      # capacity. 
-      if i % FLAGS.max_docs_per_file == 0:
-        if i > 0:
-          output_jsonl_file.close()
-        output_path = os.path.join(
-            FLAGS.output_folder, 'docs{:02d}.json'.format(file_index))
-        output_jsonl_file = open(output_path, 'w')
-        file_index += 1
-
-      doc_id, doc_text = line.rstrip().split('\t')
-
-      # Reads from predictions and merge then to the original doc text. 
-      pred_text = []
-      for _ in range(FLAGS.beam_size):
-        pred_text.append(predictions_file.readline().strip())
-      pred_text = ' '.join(pred_text)
-      pred_text = pred_text.replace(' / ', ' ')
-      text = doc_text + ' ' + pred_text
+            # Reads from predictions and merge then to the original doc text. 
+            pred_text = []
+            for _ in range(args.stride):
+                pred_text.append(predictions_file.readline().strip())
+            pred_text = ' '.join(pred_text)
+            pred_text = pred_text.replace(' / ', ' ')
+            text = (doc_text + ' ') * args.original_copies + pred_text
       
-      output_dict = {'id': doc_id, 'contents': text}
-      output_jsonl_file.write(json.dumps(output_dict) + '\n')
+            output_dict = {'id': doc_id, 'contents': text}
+            output_jsonl_file.write(json.dumps(output_dict) + '\n')
 
-      if i % 100000 == 0:
-        print('Converted {} docs in {} files'.format(i, file_index))
+            if i % 100000 == 0:
+                print('Converted {} docs in {} files'.format(i, file_index))
 
-  output_jsonl_file.close()
-  predictions_file.close()
-
-
-def main(_):
-  if not os.path.exists(FLAGS.output_folder):
-    os.makedirs(FLAGS.output_folder)
-
-  convert_collection()
-  print('Done!')
+    output_jsonl_file.close()
+    predictions_file.close()
 
 
 if __name__ == '__main__':
-  flags.mark_flag_as_required('collection_path')
-  flags.mark_flag_as_required('predictions')
-  flags.mark_flag_as_required('output_folder')
-  flags.mark_flag_as_required('beam_size')
-  app.run(main)
+    parser = argparse.ArgumentParser(description='Augments MS MARCO tsv collection with predicted queries to create Anserini jsonl collection')
+    parser.add_argument('--collection_path', required=True, help='MS MARCO tsv collection')
+    parser.add_argument('--predictions', required=True, help='query predictions file')
+    parser.add_argument('--output_folder', required=True, help='output folder for jsonl collection')
+    parser.add_argument('--stride', required=True, type=int, help='even [s] lines in predictions file is associated with each document')
+    parser.add_argument('--max_docs_per_file', default=1000000, type=int, help='maximum number of documents in each jsonl file')
+    parser.add_argument('--original_copies', default=1, type=int, help='number of copies of the original document to duplicate')
+
+    args = parser.parse_args()
+
+    if not os.path.exists(args.output_folder):
+        os.makedirs(args.output_folder)
+
+    convert_collection(args)
+    print('Done!')
