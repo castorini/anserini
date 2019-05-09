f0ccad1bc58dbbf71d66040e2b25e6c03851d235
lintool <jimmylin@uwaterloo.ca>
2019-05-09 09:52:33 -0400

    bug fix: transposed bm25 parameters.

--- src/main/python/msmarco/retrieve.py
+++ src/main/python/msmarco/retrieve.py
@@ -31,8 +31,8 @@ if __name__ == '__main__':
     parser.add_argument('--output', required=True, default='', help='output filee')
     parser.add_argument('--index', required=True, default='', help='index path')
     parser.add_argument('--hits', default=10, help='number of hits to retrieve')
-    parser.add_argument('--b1', default=0.82, help='BM25 b1 parameter')
-    parser.add_argument('--k', default=0.72, help='BM25 k parameter')
+    parser.add_argument('--k1', default=0.82, help='BM25 k1 parameter')
+    parser.add_argument('--b', default=0.72, help='BM25 b parameter')
     # See our MS MARCO documentation to understand how these parameter values were tuned.
     parser.add_argument('--rm3', action='store_true', default=False, help='use RM3')
     parser.add_argument('--fbTerms', default=10, type=int, help='RM3 parameter: number of expansion terms')
@@ -42,8 +42,8 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     searcher = JSearcher(JString(args.index))
-    searcher.setBM25Similarity(float(args.b1), float(args.k))
-    print('Initializing BM25, setting b1={} and k={}'.format(args.b1, args.k))
+    searcher.setBM25Similarity(float(args.k1), float(args.b))
+    print('Initializing BM25, setting k1={} and b={}'.format(args.k1, args.b))
     if args.rm3:
         searcher.setRM3Reranker(args.fbTerms, args.fbDocs, args.originalQueryWeight)
         print('Initializing RM3, setting fbTerms={}, fbDocs={} and originalQueryWeight={}'.format(args.fbTerms, args.fbDocs, args.originalQueryWeight))
