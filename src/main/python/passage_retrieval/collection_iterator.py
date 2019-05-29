6cf3ddf1c2ecee9d4d8703958cf6ae00567834c2
Emily Wang <sj8wang@edu.uwaterloo.ca>
2019-05-29 15:20:26 -0400

    Updated Python collections wrapper and passage retrieval setup (#656)
    * Updated Python collections wrapper and passage retrieval setup (#4)
    
    * Fixed pyjnius type issues and file segment naming
    * Updated tokenizer and score aggregation code
    * Updated documentation and results
    
    * PR changes and added docstrings
    
    * minor tweaks

--- src/main/python/passage_retrieval/collection_iterator.py
+++ src/main/python/passage_retrieval/collection_iterator.py
@@ -13,8 +13,30 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def IterSegment(fs, generator, output_path, tokenizer, raw):
+def IterSegment(fs, generator, output_path, tokenizer, tokenmin, raw):
+    """
+    Parameters
+    ----------
+    fs : pycollection.FileSegment
+        File segment to iterate over for documents.
+        
+    generator : pygenerator.Generator
+        Generator to apply transforms and create Lucene documents.
+        
+    output_path : str
+        Path to output json file containing passages from the file segment.
     
+    tokenizer : DocumentTokenizer
+        Tokenizer to call on document contents for passage splitting.
+        
+    tokenmin : int
+        Minimum limit argument (e.g. minword) passed to tokenizer
+        
+    raw : bool
+        True for using raw (FIELD_RAW) document contents,
+        False for using parsed (FIELD_BODY) document contents.
+    
+    """
     results = []
     doc_count = 0
     
@@ -41,7 +63,7 @@ def IterSegment(fs, generator, output_path, tokenizer, raw):
         else:
             # split document into segments
             try:
-                array = tokenizer(id, contents)
+                array = tokenizer.run_tokenizer(id, contents, tokenmin)
                 results += array # merge lists
             except:
                 fs.collection.counters.skipped.increment()
@@ -56,8 +78,10 @@ def IterSegment(fs, generator, output_path, tokenizer, raw):
     
     count = len(results)
     if (count > 0):
-        # write json array to outputdir (either as array of docs, or many arrays of doc tokens)
-        with open(os.path.join(output_path, '{}.json'.format(fs.segment_name)), 'w') as f:
+        # write json array to outputdir 
+        # (either as array of docs, or many arrays of doc tokens)
+        with open(os.path.join(output_path, 
+                               '{}.json'.format(fs.segment_name)), 'w') as f:
             jsonstr = json.dumps(results, separators=(',', ':'), indent=2)
             f.write(jsonstr)
             
@@ -70,8 +94,37 @@ def IterSegment(fs, generator, output_path, tokenizer, raw):
     
 def IterCollection(input_path, collection_class, 
                    generator_class, output_path, 
-                   threads=1, tokenize=None, raw=False):
-    
+                   threads=1, tokenize=None, 
+                   tokenmin=0, raw=False):
+    """
+    Parameters
+    ----------
+    input_path : str
+        Path to input collection.
+        
+    collection_class : str
+        Anserini collection class to use.
+        
+    generator_class : str
+        Anserini generator class to use.
+        
+    output_path : str
+        Path to output resulting json collection.
+        
+    threads : int
+        Maximum number of threads.
+        
+    tokenize : str
+        Name of tokenizer method to use for document-to-passage splitting.
+        
+    tokenmin : int
+        Minimum limit argument (e.g. minword) passed to tokenizer
+        
+    raw : bool
+        True for using raw (FIELD_RAW) document contents,
+        False for using parsed (FIELD_BODY) document contents.
+        
+    """
     start = time.time()
     logger.info("Begin reading collection.")
     
@@ -79,7 +132,7 @@ def IterCollection(input_path, collection_class,
     tokenizer = None
     if tokenize is not None:
         try:
-            tokenizer = DocumentTokenizer(tokenize).tokenizer
+            tokenizer = DocumentTokenizer(tokenize)
         except:
             raise ValueError(tokenize)
 
@@ -92,13 +145,14 @@ def IterCollection(input_path, collection_class,
     
     with ThreadPoolExecutor(max_workers=threads) as executor:    
         for (seg_num, fs) in enumerate(collection.segments):
-            executor.submit(IterSegment, fs, generator, output_path, tokenizer, raw)
+            executor.submit(IterSegment, fs, generator, output_path, 
+                            tokenizer, tokenmin, raw)
     
     end = time.time()
     elapsed = end - start
 
     print("all threads complete")
-    logger.info("# Final Counter Values");
+    logger.info("# Final Counter Values")
     logger.info("indexable:     {:12d}".format(collection.counters.indexable.value))
     logger.info("unindexable: {:12d}".format(collection.counters.unindexable.value))
     logger.info("skipped:     {:12d}".format(collection.counters.skipped.value))
