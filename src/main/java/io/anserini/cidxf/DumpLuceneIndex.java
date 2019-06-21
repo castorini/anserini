94363727e8e469060630a92b128cdd861d6caae1
lintool <jimmylin@uwaterloo.ca>
2019-06-21 16:22:42 -0400

    Code cleanup, generated first version of dump.

--- src/main/java/io/anserini/cidxf/DumpLuceneIndex.java
+++ src/main/java/io/anserini/cidxf/DumpLuceneIndex.java
@@ -1,10 +1,20 @@
 package io.anserini.cidxf;
 
-import com.google.protobuf.CodedOutputStream;
-import org.apache.lucene.index.*;
+import io.anserini.index.generator.LuceneDocumentGenerator;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.LeafReader;
+import org.apache.lucene.index.PostingsEnum;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.index.TermsEnum;
 import org.apache.lucene.search.DocIdSetIterator;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.BytesRef;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.OptionHandlerFilter;
+import org.kohsuke.args4j.ParserProperties;
 
 import java.io.FileOutputStream;
 import java.nio.file.Paths;
@@ -14,12 +24,35 @@ import static io.anserini.cidxf.CommonIndexFormat.PostingsList;
 import static io.anserini.cidxf.CommonIndexFormat.PostingsListOrBuilder;
 
 public class DumpLuceneIndex {
-  public static void main(String[] argv) throws Exception {
-    IndexReader reader = DirectoryReader.open(
-        FSDirectory.open(Paths.get("lucene-index.robust04.pos+docvectors+rawdocs")));
+  public static class Args {
+    @Option(name = "-postingsOutput", metaVar = "[file]", required = true, usage = "postings output")
+    public String postingsOutput = "";
 
-    FileOutputStream fileOut = new FileOutputStream("postings.dat");
-    //CodedOutputStream output = CodedOutputStream.newInstance(fileOut);
+    @Option(name = "-docidOutput", metaVar = "[file]", required = true, usage = "docid output")
+    public String docidOutput = "";
+
+    @Option(name = "-index", metaVar = "[path]", required = true, usage = "index path")
+    public String index = "";
+
+    @Option(name = "-max", metaVar = "[int]", usage = "maximum number of postings to write")
+    public int max = Integer.MAX_VALUE;
+  }
+
+  public static void main(String[] argv) throws Exception {
+    Args args = new Args();
+    CmdLineParser parser = new CmdLineParser(args, ParserProperties.defaults().withUsageWidth(90));
+
+    try {
+      parser.parseArgument(argv);
+    } catch (CmdLineException e) {
+      System.err.println(e.getMessage());
+      parser.printUsage(System.err);
+      System.err.println("Example: Eval " + parser.printExample(OptionHandlerFilter.REQUIRED));
+      return;
+    }
+
+    IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get(args.index)));
+    FileOutputStream fileOut = new FileOutputStream(args.postingsOutput);
 
     int cnt = 0;
     // This is how you iterate through terms in the postings list.
@@ -40,6 +73,7 @@ public class DumpLuceneIndex {
 
       int curDocid;
       int prevDocid = -1;
+      int postingsWritten = 0;
       PostingsEnum postingsEnum = leafReader.postings(term);
       while (postingsEnum.nextDoc() != DocIdSetIterator.NO_MORE_DOCS) {
         curDocid = postingsEnum.docID();
@@ -49,25 +83,38 @@ public class DumpLuceneIndex {
         //System.out.print(String.format(" (%s, %s, %s)", postingsEnum.docID(), postingsEnum.freq(), code));
         ((PostingsList.Builder) plBuilder).addPosting(
             Posting.newBuilder().setDocid(code).setTf(postingsEnum.freq()).build());
+        postingsWritten++;
         prevDocid = curDocid;
       }
-      //System.out.println("");
+      // The number of postings written should be the same as the df.
+      if (postingsWritten != df) {
+        throw new RuntimeException(String.format("Unexpected number of postings! expected %d got %d", df, postingsWritten));
+      }
+
       CommonIndexFormat.PostingsList pl = ((CommonIndexFormat.PostingsList.Builder) plBuilder).build();
-      //System.out.println(pl.toString());
       pl.writeDelimitedTo(fileOut);
 
       bytesRef = termsEnum.next();
 
       cnt++;
-      if ( cnt > Integer.MAX_VALUE) {
+      if ( cnt > args.max) {
         break;
       }
-      if (cnt % 1000 == 0) {
-        System.out.println("Dumped " + cnt + " postings");
+      if (cnt % 10000 == 0) {
+        System.out.println("Wrote " + cnt + " postings...");
       }
     }
-
+    System.out.println("Total of " + cnt + " postings written.");
     fileOut.close();
 
+    System.out.println("Writing docids...");
+    FileOutputStream docidOut = new FileOutputStream(args.docidOutput);
+    for (int i=0; i<reader.maxDoc(); i++) {
+      docidOut.write((reader.document(i).getField(LuceneDocumentGenerator.FIELD_ID).stringValue() +  "\n").getBytes());
+    }
+    docidOut.close();
+    System.out.println("Done!");
+
+    reader.close();
   }
 }
--- src/main/java/io/anserini/cidxf/ReadCommonIndexFormatDump.java
+++ src/main/java/io/anserini/cidxf/ReadCommonIndexFormatDump.java
@@ -1,23 +1,49 @@
 package io.anserini.cidxf;
 
-import com.google.protobuf.CodedInputStream;
-import com.google.protobuf.CodedOutputStream;
-import org.apache.lucene.index.DirectoryReader;
-import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.store.FSDirectory;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.OptionHandlerFilter;
+import org.kohsuke.args4j.ParserProperties;
 
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.nio.file.Paths;
 
 public class ReadCommonIndexFormatDump {
-  public static void main(String[] argv) throws Exception {
-    FileInputStream fileIn = new FileInputStream("postings.dat");
-    CodedInputStream input = CodedInputStream.newInstance(fileIn);
+  public static class Args {
+    @Option(name = "-postings", metaVar = "[file]", required = true, usage = "postings file")
+    public String postings = "";
 
-    for (int i=0; i<10; i++ ) {
+    @Option(name = "-max", metaVar = "[int]", usage = "maximum number of postings to write")
+    public int max = 10;
+  }
+
+  public static void main(String[] argv) throws Exception {
+    Args args = new Args();
+    CmdLineParser parser = new CmdLineParser(args, ParserProperties.defaults().withUsageWidth(90));
+
+    try {
+      parser.parseArgument(argv);
+    } catch (CmdLineException e) {
+      System.err.println(e.getMessage());
+      parser.printUsage(System.err);
+      System.err.println("Example: Eval " + parser.printExample(OptionHandlerFilter.REQUIRED));
+      return;
+    }
+
+    FileInputStream fileIn = new FileInputStream(args.postings);
+    for (int i=0; i<args.max; i++ ) {
       CommonIndexFormat.PostingsList pl = CommonIndexFormat.PostingsList.parseDelimitedFrom(fileIn);
-      System.out.println(pl.getTerm());
+      System.out.print(String.format("term: '%s', df=%d, cf=%d", pl.getTerm(), pl.getDf(), pl.getCf()));
+
+      if (pl.getDf() != pl.getPostingCount()) {
+        throw new RuntimeException(String.format(
+            "Unexpected number of postings! expected %d got %d", pl.getDf(), pl.getPostingCount()));
+      }
+
+      for (int j=0; j< (pl.getDf() > 10 ? 10 : pl.getDf()); j++) {
+        System.out.print(String.format(" (%d, %d)", pl.getPosting(j).getDocid(), pl.getPosting(j).getTf()));
+      }
+      System.out.println("");
     }
     fileIn.close();
   }
