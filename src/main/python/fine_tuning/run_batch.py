64bae9c8b87ad56bc8cf6ea0c5405eb2a82b3682
Jimmy Lin <jimmylin@uwaterloo.ca>
2019-07-03 15:04:22 -0400

    Fixed "neural hype" tuning experiments after Lucene 8.0 upgrade (#736)
    + retuned retrieval models for Lucene 8.0
    
    Refactored tuning script:
    + made command-line parameters more consistent
    + broke fold settings into external config files for greater generality
    + removed unintuitive distinction between "model" and "basemodel": there's just "model" now.

--- src/main/python/fine_tuning/run_batch.py
+++ src/main/python/fine_tuning/run_batch.py
@@ -32,12 +32,12 @@ from xfold import XFoldValidate
 logger = logging.getLogger('fine_tuning')
 logger.setLevel(logging.INFO)
 # create console handler with a higher log level
-ch = logging.StreamHandler()
-ch.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-ch.setFormatter(formatter)
+#ch = logging.StreamHandler()
+#ch.setLevel(logging.INFO)
+#formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+#ch.setFormatter(formatter)
 # add the handlers to the logger
-logger.addHandler(ch)
+#logger.addHandler(ch)
 
 parallelism=1
 def batch_everything(all_params, func):
@@ -46,8 +46,11 @@ def batch_everything(all_params, func):
     p = Pool(min(parallelism, len(all_params)))
     p.map(func, all_params)
 
-def isclose(a, b, rel_tol=1e-09, abs_tol=0.0):
-    return abs(a-b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol)
+
+# As long as two numbers match to the four decimal place, we're good
+def is_close(a, b):
+    return abs(round(a, 4) - round(b, 4)) <= 1e-05
+
 
 def get_index_path(yaml_data):
     """
@@ -58,24 +61,6 @@ def get_index_path(yaml_data):
             index_path = os.path.join(index_root, yaml_data['index_path'])
             break
     return index_path
-
-def load_drr_fold_mapping(fold_dir):
-    # load the qid: fold_id mapping of the 5-folds used 
-    # in the deep relevance ranking paper
-    # (https://github.com/nlpaueb/deep-relevance-ranking)
-    fold_mapping = {}
-    for fold_id in xrange(5):
-        fold_fn = os.path.join(fold_dir,'rob04.test.s{}.json'.format(fold_id+1))
-        try:
-            fold_info = json.load(open(fold_fn))
-        except IOError:
-            logger.error("Error when parsing fold file: {}\n".format(fold_fn))
-            return None
-        else:
-            for q_info in fold_info['questions']:
-                qid = q_info['id']
-                fold_mapping[qid] = fold_id
-    return fold_mapping
 
 
 def batch_retrieval(collection_yaml, models_yaml, output_root):
@@ -92,7 +77,6 @@ def batch_retrieval(collection_yaml, models_yaml, output_root):
             '-topicreader', collection_yaml['topic_reader'],
             '-index', index_path,
             '-topics', os.path.join(collection_yaml['anserini_root'], collection_yaml['topic_root'], collection_yaml['topic']),
-            '-{}'.format(models_yaml['basemodel']),
             para[0],
             '-output', para[1]
         )
@@ -100,8 +84,10 @@ def batch_retrieval(collection_yaml, models_yaml, output_root):
     logger.info('='*10+'Starting Batch Retrieval'+'='*10)
     batch_everything(all_params, atom_retrieval)
 
+
 def atom_retrieval(para):
     subprocess.call(' '.join(para), shell=True)
+
 
 def batch_eval(collection_yaml, models_yaml, output_root):
     all_params = []
@@ -121,8 +107,10 @@ def batch_eval(collection_yaml, models_yaml, output_root):
     logger.info('='*10+'Starting Batch Evaluation'+'='*10)
     batch_everything(all_params, atom_eval)
 
+
 def atom_eval(params):
     Evaluation.output_all_evaluations(*params)
+
 
 def batch_output_effectiveness(collection_yaml, models_yaml, output_root):
     all_params = []
@@ -132,58 +120,78 @@ def batch_output_effectiveness(collection_yaml, models_yaml, output_root):
     logger.info('='*10+'Starting Output Effectiveness'+'='*10)
     batch_everything(all_params, atom_output_effectiveness)
 
+
 def atom_output_effectiveness(para):
     index_path = para[0]
     output_fn = para[1]
     input_fns = para[2:]
     Effectiveness(index_path).output_effectiveness(output_fn, input_fns)
 
-def verify_effectiveness(collection_yaml, models_yaml, output_root, use_drr_fold):
+
+# How to print colored text in terminal in Python?
+# https://stackoverflow.com/questions/287871/how-to-print-colored-text-in-terminal-in-python
+
+def verify_effectiveness(collection_yaml, models_yaml, output_root, fold_settings, verbose):
     index_path = get_index_path(collection_yaml)
     this_output_root = os.path.join(output_root, collection_yaml['name'])
     effectiveness, per_topic_oracle = Effectiveness(index_path).load_optimal_effectiveness(this_output_root)
     success_optimal = True
+
     for e in effectiveness:
-        if e['basemodel'] != models_yaml['basemodel'] or e['model'] != models_yaml['name'] or e['metric'] not in models_yaml['expected'][collection_yaml['name']]:
+        if e['metric'] not in models_yaml['expected'][collection_yaml['name']]:
             continue
         expected = models_yaml['expected'][collection_yaml['name']][e['metric']]
-        if isclose(expected['best_avg'], e['best_avg']['value']):
-            logger.info(json.dumps(e, sort_keys=True))
+        if is_close(expected['best_avg'], e['best_avg']['value']):
+            logger.info(' best_avg          --- model: %s, metric: %6s, expected: %.4f, actual: %.4f \x1b[6;30;42m[OK]\x1b[0m' % (e['model'], e['metric'], expected['best_avg'], e['best_avg']['value']))
         else:
             success_optimal = False
-            logger.error('!'*5+'base model: %s model: %s metric: %s expected best: %f actual: %s ' % (e['basemodel'], e['model'], e['metric'], expected['best_avg'], e['best_avg']['value'])+'!'*5)
-        if isclose(expected['oracles_per_topic'], e['oracles_per_topic']):
-            logger.info(json.dumps(e, sort_keys=True))
+            logger.error('best_avg          --- model: %s, metric: %6s, expected: %.4f, actual: %.4f \x1b[6;30;41m[ERROR]\x1b[0m' % (e['model'], e['metric'], expected['best_avg'], e['best_avg']['value']))
+        if is_close(expected['oracles_per_topic'], e['oracles_per_topic']):
+            logger.info(' oracles_per_topic --- model: %s, metric: %6s, expected: %.4f, actual: %.4f \x1b[6;30;42m[OK]\x1b[0m' % (e['model'], e['metric'], expected['oracles_per_topic'], e['oracles_per_topic']))
         else:
             success_optimal = False
-            logger.error('!'*5+'base model: %s model: %s metric: %s oracles_per_topic: %f actual: %s ' % (e['basemodel'], e['model'], e['metric'], expected['oracles_per_topic'], e['oracles_per_topic'])+'!'*5)
+            logger.error('oracles_per_topic --- model: %s, metric: %6s, expected: %.4f, actual: %.4f \x1b[6;30;41m[ERROR]\x1b[0m' % (e['model'], e['metric'], expected['oracles_per_topic'], e['oracles_per_topic']))
+
+    if fold_settings == '':
+        return
 
     success_xfold = True
-    for fold in [2, 5]:
-        if collection_yaml['name'] == 'robust04' and fold == 5 and use_drr_fold:
-            fold_dir = os.path.join(collection_yaml['anserini_root'], 'src/main/resources/fine_tuning/drr_folds')
-            fold_mapping = load_drr_fold_mapping(fold_dir)
-            x_fold_effectiveness = XFoldValidate(output_root, collection_yaml['name'], fold, fold_mapping).tune(False)
-        else:
-            x_fold_effectiveness = XFoldValidate(output_root, collection_yaml['name'], fold).tune(False)
-        for basemodel in x_fold_effectiveness:
-            if models_yaml['basemodel'] != basemodel:
+    fold = -1
+
+    logger.info('Checking fold settings: ' + fold_settings)
+
+    fold_mapping = {}
+    num_folds = 0
+    with open(fold_settings) as json_file:
+        raw_json_folds = json.load(json_file)
+        for fold in raw_json_folds:
+            for t in fold:
+                fold_mapping[t] = num_folds
+            num_folds = num_folds + 1
+
+    logger.info('Number of folds: %d' % (num_folds))
+    fold = num_folds
+
+    x_fold_effectiveness = XFoldValidate(output_root, collection_yaml['name'], fold, fold_mapping).tune(verbose)
+
+    for model in x_fold_effectiveness:
+        if models_yaml['name'] != model:
+            continue
+        for metric in x_fold_effectiveness[model]:
+            if metric not in models_yaml['expected'][collection_yaml['name']]:
                 continue
-            for model in x_fold_effectiveness[basemodel]:
-                if models_yaml['name'] != model:
-                    continue
-                for metric in x_fold_effectiveness[basemodel][model]:
-                    if metric not in models_yaml['expected'][collection_yaml['name']]:
-                        continue
-                    expected = models_yaml['expected'][collection_yaml['name']][metric]
-                    if isclose(expected['%d-fold' % fold], x_fold_effectiveness[basemodel][model][metric]):
-                        logger.info(json.dumps(x_fold_effectiveness[basemodel][model], sort_keys=True))
-                    else:
-                        success_optimal = False
-                        logger.error('!'*5+'base model: %s model: %s fold: %d metric: %s expected: %f actual: %s ' % (basemodel, model, fold, metric, expected['%d-fold' % fold], x_fold_effectiveness[basemodel][model][metric])+'!'*5)
+            expected = models_yaml['expected'][collection_yaml['name']][metric]
+            if is_close(expected['%d-fold' % fold], x_fold_effectiveness[model][metric]):
+                logger.info(' xvalidation --- model: %s, metric: %6s, expected: %.4f, actual: %.4f \x1b[6;30;42m[OK]\x1b[0m' % (model, metric, expected['%d-fold' % fold], x_fold_effectiveness[model][metric]))
+            else:
+                success_optimal = False
+                logger.error('xvalidation --- model: %s, metric: %6s, expected: %.4f, actual: %.4f \x1b[6;30;41m[ERROR]\x1b[0m' % (model, metric, expected['%d-fold' % fold], x_fold_effectiveness[model][metric]))
 
     if success_optimal and success_xfold:
-        logger.info('[Regression Tests Passed] All Passed^^^')
+        logger.info('\x1b[6;30;42m[All Tests Passed!]\x1b[0m')
+    else:
+        logger.info('\x1b[6;30;41m[Tests Failures!]\x1b[0m')
+
 
 def del_method_related_files(method_name):
     folders = ['split_results', 'merged_results', 'evals', 'effectiveness']
@@ -207,11 +215,11 @@ if __name__ == '__main__':
     parser.add_argument('--anserini_root', default='', help='Anserini path')
     parser.add_argument('--run', action='store_true', help='Generate the runs files and evaluate them. Otherwise we only output the evaluation results (based on the existing eval files)')
     parser.add_argument('--collection', required=True, help='the collection key in yaml')
-    parser.add_argument('--basemodel', default='bm25', choices=['bm25', 'ql'], help='the ranking model')
-    parser.add_argument('--model', default='axiom', choices=['bm25', 'ql', 'axiom', 'rm3', 'bm25+axiom', 'bm25+rm3'], help='the higher level model')
-    parser.add_argument('--n', dest='parallelism', type=int, default=16, help='number of parallel threads for retrieval/eval')
+    parser.add_argument('--model', required=True, help='model')
+    parser.add_argument('--threads', dest='parallelism', type=int, default=16, help='number of parallel threads for retrieval and evaluation')
     parser.add_argument('--output_root', default='fine_tuning_results', help='output directory of all results')
-    parser.add_argument('--use_drr_fold', action='store_true', help='if specified, use the 5-folds from the deep-relevance-ranking paper')
+    parser.add_argument('--fold_settings', default='', help='JSON file holding fold definitions, see src/main/resources/fine_tuning/robust04-paper1-folds.json for an example')
+    parser.add_argument('--verbose', action='store_true', help='if specified print out model parameters and per fold scores')
 
     # runtime
     parser.add_argument(
@@ -243,11 +251,9 @@ if __name__ == '__main__':
         collection_yaml['anserini_root'] = args.anserini_root
         if not os.path.exists(os.path.join(args.output_root, collection_yaml['name'])):
             os.makedirs(os.path.join(args.output_root, collection_yaml['name']))
-        models_yaml['basemodel'] = args.basemodel
 
         if args.run:
             batch_retrieval(collection_yaml, models_yaml, args.output_root)
             batch_eval(collection_yaml, models_yaml, args.output_root)
             batch_output_effectiveness(collection_yaml, models_yaml, args.output_root)
-        verify_effectiveness(collection_yaml, models_yaml, args.output_root, args.use_drr_fold)
-
+        verify_effectiveness(collection_yaml, models_yaml, args.output_root, args.fold_settings, args.verbose)
