de73e5b7de1e68afe0164cd13d4bb5e851f755e9
Tommaso Teofili <teofili@adobe.com>
2019-10-21 21:19:38 +0200

    Approximate nearest vector search with Lucene (#827)
    Introduces a couple of approaches to index and search for nearest vectors in Anserini with plain Lucene:
    + "Large Scale Indexing and Searching Deep Convolutional Neural Network Features" by Amato et al., aka "fake words"
    + "Lexical LSH", based on LSH'ed feature ngrams

---
+++ src/main/java/io/anserini/analysis/vectors/lexlsh/LexicalLshFeaturePositionTokenFilter.java
@@ -0,0 +1,58 @@
+package io.anserini.analysis.vectors.lexlsh;
+
+import org.apache.lucene.analysis.TokenFilter;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+
+import java.io.IOException;
+
+/**
+ * {@link TokenFilter} which prepends the token / feature position plus underscore to the token itself
+ * (possibly with some offset)
+ */
+final class LexicalLshFeaturePositionTokenFilter extends TokenFilter {
+
+  private final CharTermAttribute termAttribute = addAttribute(CharTermAttribute.class);
+  private final int start;
+  private int tokenCount = 0;
+
+  LexicalLshFeaturePositionTokenFilter(TokenStream stream) {
+    this(stream, 0);
+  }
+
+  LexicalLshFeaturePositionTokenFilter(TokenStream stream, int start) {
+    super(stream);
+    this.start = start;
+  }
+
+  @Override
+  public boolean incrementToken() throws IOException {
+    if (input.incrementToken()) {
+      tokenCount++;
+      String token = new String(termAttribute.buffer(), 0, termAttribute.length());
+      termAttribute.setEmpty();
+      termAttribute.append(String.valueOf(tokenCount));
+      termAttribute.append("_");
+      if (start > 0 && start < token.length()) {
+        if (token.startsWith("-")) {
+          termAttribute.append("-");
+          termAttribute.append(token.substring(start + 1));
+        } else {
+          termAttribute.append(token.substring(start));
+        }
+      } else {
+        termAttribute.append(token);
+      }
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  @Override
+  public void reset() throws IOException {
+    super.reset();
+    tokenCount = 0;
+  }
+
+}
---
+++ src/main/java/io/anserini/analysis/vectors/lexlsh/LexicalLshTruncateTokenFilter.java
@@ -0,0 +1,42 @@
+package io.anserini.analysis.vectors.lexlsh;
+
+import org.apache.lucene.analysis.TokenFilter;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.tokenattributes.KeywordAttribute;
+
+import java.io.IOException;
+
+/**
+ * {@link TokenFilter} which truncates a number beyond {#length} decimals.
+ */
+class LexicalLshTruncateTokenFilter extends TokenFilter {
+
+  private final CharTermAttribute termAttribute = addAttribute(CharTermAttribute.class);
+  private final KeywordAttribute keywordAttr = addAttribute(KeywordAttribute.class);
+
+  private final int decimals;
+
+  LexicalLshTruncateTokenFilter(TokenStream input, int decimals) {
+    super(input);
+    if (decimals < 1) {
+      throw new IllegalArgumentException("'decimals' parameter must be a positive number: " + decimals);
+    }
+    this.decimals = decimals;
+  }
+
+  @Override
+  public final boolean incrementToken() throws IOException {
+    if (input.incrementToken()) {
+      String s = termAttribute.toString();
+      int decimalPlaceSeparatorIndex = s.indexOf(".");
+      int threshold = decimalPlaceSeparatorIndex + 1 + decimals;
+      if (!keywordAttr.isKeyword() && termAttribute.length() > threshold) {
+        termAttribute.setLength(threshold);
+      }
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
