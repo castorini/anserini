aa1e13401ab49eddfe7e6027dc59ef88c7a01c30
Jimmy Lin <jimmylin@uwaterloo.ca>
2020-05-04 13:59:54 -0400

    Added TREC-COVID round 2 topics (#1140)

--- src/test/java/io/anserini/search/topicreader/CovidTopicReaderTest.java
+++ src/test/java/io/anserini/search/topicreader/CovidTopicReaderTest.java
@@ -28,7 +28,7 @@ import static org.junit.Assert.assertEquals;
 public class CovidTopicReaderTest {
 
   @Test
-  public void test() throws IOException {
+  public void test1() throws IOException {
     TopicReader<Integer> reader = new CovidTopicReader(
         Paths.get("src/main/resources/topics-and-qrels/topics.covid-round1.xml"));
 
@@ -49,6 +49,32 @@ public class CovidTopicReaderTest {
         topics.get(topics.lastKey()).get("question"));
     assertEquals(
         "seeking specific information on clinical outcomes in COVID-19 patients treated with remdesivir",
+        topics.get(topics.lastKey()).get("narrative"));
+  }
+
+  @Test
+  public void test2() throws IOException {
+    TopicReader<Integer> reader = new CovidTopicReader(
+        Paths.get("src/main/resources/topics-and-qrels/topics.covid-round2.xml"));
+
+    SortedMap<Integer, Map<String, String>> topics = reader.read();
+
+    assertEquals(35, topics.keySet().size());
+
+    assertEquals(1, (int) topics.firstKey());
+    assertEquals("coronavirus origin", topics.get(topics.firstKey()).get("query"));
+    assertEquals("what is the origin of COVID-19", topics.get(topics.firstKey()).get("question"));
+    assertEquals("seeking range of information about the SARS-CoV-2 virus's origin, " +
+            "including its evolution, animal source, and first transmission into humans",
+        topics.get(topics.firstKey()).get("narrative"));
+
+    assertEquals(35, (int) topics.lastKey());
+    assertEquals("coronavirus public datasets", topics.get(topics.lastKey()).get("query"));
+    assertEquals("What new public datasets are available related to COVID-19?",
+        topics.get(topics.lastKey()).get("question"));
+    assertEquals("Seeking articles that specifically release new data related to SARS-CoV-2 or COVID-19, " +
+            "including genomic data, patient data, public health data, etc. Articles that reference previously " +
+            "existing datasets are not relevant.",
         topics.get(topics.lastKey()).get("narrative"));
   }
 }
