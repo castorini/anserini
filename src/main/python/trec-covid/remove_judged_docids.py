3e968b4ad6c699e169d16287bc69d312c9d81905
Kuang Lu <lukuang1989@gmail.com>
2020-05-06 20:15:48 -0400

    add the script

---
+++ src/main/python/trec-covid/remove_judged_docids.py
@@ -0,0 +1,51 @@
+"""
+remove judged docids from run file according to a qrel file
+"""
+import argparse
+from collections import defaultdict
+
+
+def read_qrels(qrel_file):
+    qrels = defaultdict(set)
+    with open(qrel_file) as f:
+        for line in f:
+            cols = line.split()
+            qid = cols[0]
+            docid = cols[2]
+            qrels[qid].add(docid)
+    return qrels
+
+
+def main():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('qrel_file')
+    parser.add_argument('src_run_file')
+    parser.add_argument('dest_run_file')
+    parser.add_argument(
+        '--k', type=int, default=1000,
+        help='the number of results to keep per-topic'
+    )
+    args=parser.parse_args()
+
+    qrels = read_qrels(args.qrel_file)
+    counts = defaultdict(int)
+    with open(args.dest_run_file, 'w') as of:
+        with open(args.src_run_file) as f:
+            for line in f:
+                cols = line.split()
+                qid = cols[0]
+
+                if counts[qid] >= args.k:
+                    continue
+                docid = cols[2]
+
+                if (
+                    qid not in qrels
+                    or docid not in qrels[qid]
+                ):
+                    counts[qid] += 1
+                    of.write(line)
+
+
+if __name__=='__main__':
+    main()
