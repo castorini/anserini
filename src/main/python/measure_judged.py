05f24d1b887ba2cfe0c0abc7fb2b77c64ddd4428
Rodrigo Frassetto Nogueira <rodrigonogueira4@gmail.com>
2020-05-07 11:36:45 -0400

    Addresses Jimmy's comments

--- src/main/python/measure_judged.py
+++ src/main/python/measure_judged.py
@@ -1,5 +1,16 @@
-"""Script to measure the percentage of judged documents at various
-cutoffs."""
+# -*- coding: utf-8 -*-
+'''
+Anserini: A Lucene toolkit for replicable information retrieval research
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
 import argparse
 import collections
 
@@ -49,13 +60,17 @@ if __name__ == "__main__":
                     'cutoffs.')
     parser.add_argument('--qrels', type=str, required=True, help='qrels file')
     parser.add_argument('--run', type=str, required=True, help='run file')
+    parser.add_argument('--cutoffs', nargs='+', type=int,
+                        default=[5, 10, 20, 30],
+                        help='Space-separate list of cutoffs. '
+                             'E.g.: --cutoffs 5 10 20')
 
     args = parser.parse_args()
 
     qrels = load_qrels(args.qrels)
     run = load_run(args.run)
 
-    for max_rank in [5, 10, 20, 30]:
+    for max_rank in args.cutoffs:
         percentage_judged = 0
 
         for query_id, doc_ids in run.items():
