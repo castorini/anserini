d395af9a4d2166f98f770e369705cbad359fb752
Pepijn Boers <pepijn96@msn.com>
2020-05-29 13:27:36 +0200

    Bindings for TREC News topics (#1245)

--- src/main/java/io/anserini/search/topicreader/TopicReader.java
+++ src/main/java/io/anserini/search/topicreader/TopicReader.java
@@ -76,7 +76,9 @@ public abstract class TopicReader<K> {
       Map.entry("topics.covid-round2.xml", CovidTopicReader.class),
       Map.entry("topics.covid-round2-udel.xml", CovidTopicReader.class),
       Map.entry("topics.covid-round3.xml", CovidTopicReader.class),
-      Map.entry("topics.covid-round3-udel.xml", CovidTopicReader.class)
+      Map.entry("topics.covid-round3-udel.xml", CovidTopicReader.class),
+      Map.entry("topics.backgroundlinking18.txt", BackgroundLinkingTopicReader.class),
+      Map.entry("topics.backgroundlinking19.txt", BackgroundLinkingTopicReader.class)
   );
 
   /**
