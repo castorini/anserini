39455061ae67c32f71692c3dce49cc15b9064143
Kelvin Jiang <k.jiang1118@gmail.com>
2020-06-14 01:18:59 -0400

    updated generate_queries_and_qrels.py to optionally create labels file

--- src/main/python/fever/generate_queries_and_qrels.py
+++ src/main/python/fever/generate_queries_and_qrels.py
@@ -25,7 +25,12 @@ def generate_queries_and_qrels(args):
         # open qrels file if provided
         if args.output_qrels_file:
             print('Generating qrels...')
-            f_out = open(args.output_qrels_file, 'w', encoding='utf-8')
+            qrels_out = open(args.output_qrels_file, 'w', encoding='utf-8')
+
+        # open labels file if provided
+        if args.output_labels_file:
+            print('Generating labels...')
+            labels_out = open(args.output_labels_file, 'w', encoding='utf-8')
 
         for line in f_in:
             line_json = json.loads(line.strip())
@@ -36,33 +41,40 @@ def generate_queries_and_qrels(args):
 
             # save query to queries dict
             queries[qid] = query
+            if label == 'NOT ENOUGH INFO':
+                continue
 
             # write claims and evidence to qrels file if provided
             if args.output_qrels_file:
-                if label == 'NOT ENOUGH INFO':
-                    f_out.write(f'{qid}\t0\t\t\n')
-                else:
-                    # dedupe evidences for the query
-                    evidences = set()
-                    for annotator in line_json['evidence']:
-                        for evidence in annotator:
-                            if args.granularity == 'sentence':
-                                evidences.add((evidence[2], evidence[3]))
-                            else:  # args.granularity == 'paragraph'
-                                evidences.add(evidence[2])
+                # dedupe evidences for the query
+                evidences = set()
+                for annotator in line_json['evidence']:
+                    for evidence in annotator:
+                        if args.granularity == 'sentence':
+                            evidences.add((evidence[2], evidence[3]))
+                        else:  # args.granularity == 'paragraph'
+                            evidences.add(evidence[2])
 
-                    # write deduped evidences to qrels file
-                    relevance = 1 if label == 'SUPPORTS' else 0
-                    if args.granularity == 'sentence':
-                        for doc_id, sentence_id in evidences:
-                            f_out.write(f'{qid}\t0\t{doc_id}_{sentence_id}\t{relevance}\n')
-                    else:  # args.granularity == 'paragraph'
-                        for doc_id in evidences:
-                            f_out.write(f'{qid}\t0\t{doc_id}\t{relevance}\n')
+                # write deduped evidences to qrels file
+                if args.granularity == 'sentence':
+                    for doc_id, sentence_id in evidences:
+                        qrels_out.write(f'{qid}\t0\t{doc_id}_{sentence_id}\t2\n')
+                else:  # args.granularity == 'paragraph'
+                    for doc_id in evidences:
+                        qrels_out.write(f'{qid}\t0\t{doc_id}\t2\n')
+
+            # write claims and labels to labels file if provided
+            if args.output_labels_file:
+                score = 1 if label == 'SUPPORTS' else 0
+                labels_out.write(f'{qid}\t{score}\n')
 
         # close qrels file if provided
         if args.output_qrels_file:
-            f_out.close()
+            qrels_out.close()
+
+        # close labels file if provided
+        if args.output_labels_file:
+            labels_out.close()
 
     # write queries to queries file if provided
     if args.output_queries_file:
@@ -76,6 +88,7 @@ if __name__ == '__main__':
     parser.add_argument('--dataset_file', required=True, help='Path to FEVER dataset file.')
     parser.add_argument('--output_queries_file', help='Output queries file.')
     parser.add_argument('--output_qrels_file', help='Output qrels file.')
+    parser.add_argument('--output_labels_file', help='Output labels file.')
     parser.add_argument('--granularity',
                         required=True,
                         choices=['paragraph', 'sentence'],
