82303a8c00cd89c7c1e068e6175deaeeed4bcd06
lintool <jimmylin@uwaterloo.ca>
2020-06-16 13:13:54 -0400

    Fixed CI, addressed CR.

--- src/main/java/io/anserini/search/SearchMsmarco.java
+++ src/main/java/io/anserini/search/SearchMsmarco.java
@@ -43,40 +43,40 @@ public class SearchMsmarco {
     @Option(name = "-queries", metaVar = "[file]", required = true, usage="Queries file.")
     public String qid_queries = "";
 
-    @Option(name = "-output", metaVar = "[file]", required = true, usage = "output file")
+    @Option(name = "-output", metaVar = "[file]", required = true, usage = "Output run file.")
     public String output = "";
 
-    @Option(name = "-index", metaVar = "[path]", required = true, usage = "index path")
+    @Option(name = "-index", metaVar = "[path]", required = true, usage = "Index path.")
     public String index = "";
 
     // optional arguments
-    @Option(name = "-threads", metaVar = "[number]", usage = "maximum number of threads")
+    @Option(name = "-threads", metaVar = "[number]", usage = "Maximum number of threads.")
     public int threads = 1;
 
-    @Option(name = "-hits", metaVar = "[number]", usage = "number of hits to retrieve")
+    @Option(name = "-hits", metaVar = "[number]", usage = "Number of hits to retrieve.")
     public int hits = 10;
 
-    @Option(name = "-k1", metaVar = "[value]", usage = "BM25 k1 parameter")
+    @Option(name = "-k1", metaVar = "[value]", usage = "BM25 k1 parameter.")
     public float k1 = 0.82f;
 
-    @Option(name = "-b", metaVar = "[value]", usage = "BM25 b parameter")
+    @Option(name = "-b", metaVar = "[value]", usage = "BM25 b parameter.")
     public float b = 0.68f;
 
     // See our MS MARCO documentation to understand how these parameter values were tuned.
-    @Option(name = "-rm3", usage = "use RM3 query expansion model")
+    @Option(name = "-rm3", usage = "Use RM3.")
     public boolean rm3 = false;
 
-    @Option(name = "-fbTerms", metaVar = "[number]", usage = "RM3 parameter: number of expansion terms")
+    @Option(name = "-fbTerms", metaVar = "[number]", usage = "RM3: number of expansion terms.")
     public int fbTerms = 10;
 
-    @Option(name = "-fbDocs", metaVar = "[number]", usage = "RM3 parameter: number of documents")
+    @Option(name = "-fbDocs", metaVar = "[number]", usage = "RM3: number of documents.")
     public int fbDocs = 10;
 
-    @Option(name = "-originalQueryWeight", metaVar = "[value]", usage = "RM3 parameter: weight to assign to the original query")
+    @Option(name = "-originalQueryWeight", metaVar = "[value]", usage = "RM3: weight of original query.")
     public float originalQueryWeight = 0.5f;
 
     @Option(name = "-fields", metaVar = "[key=value]", handler = MapOptionHandler.class,
-            usage = "Fields to search with assigned float weight")
+            usage = "Fields to search with assigned float weights.")
     public Map<String, String> fields = new HashMap<>();
   }
 
