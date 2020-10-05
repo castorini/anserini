5b49dcd5a9c166aa29973ddf4b03055ab8f9452e
nsndimt <nsndimt@gmail.com>
2020-10-05 18:00:03 -0400

    add benchmark

--- src/main/java/io/anserini/ltr/FeatureExtractorCli.java
+++ src/main/java/io/anserini/ltr/FeatureExtractorCli.java
@@ -1,7 +1,10 @@
 package io.anserini.ltr;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.anserini.ltr.feature.OrderedSequentialPairsFeatureExtractor;
 import io.anserini.ltr.feature.UnorderedSequentialPairsFeatureExtractor;
+import io.anserini.ltr.feature.base.*;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
@@ -36,7 +39,18 @@ public class FeatureExtractorCli {
 
         FeatureExtractorUtils utils = new FeatureExtractorUtils(cmdArgs.indexDir, cmdArgs.threads);
 
-        utils.add(new OrderedSequentialPairsFeatureExtractor(3));
+        utils.add(new AvgICTFFeatureExtractor());
+        utils.add(new AvgIDFFeatureExtractor());
+        utils.add(new BM25FeatureExtractor());
+        utils.add(new DocSizeFeatureExtractor());
+        utils.add(new MatchingTermCount());
+        utils.add(new QueryLength());
+        utils.add(new SCQFeatureExtractor());
+        utils.add(new SCSFeatureExtractor());
+        utils.add(new SumMatchingTF());
+        utils.add(new TFIDFFeatureExtractor());
+        utils.add(new UniqueTermCount());
+
 
         File file = new File(cmdArgs.jsonFile);
         BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
@@ -44,21 +58,40 @@ public class FeatureExtractorCli {
         ArrayList<String> qids = new ArrayList<>();
         int offset = 0;
         String lastQid = null;
+        ObjectMapper mapper = new ObjectMapper();
+        ArrayList<String> names = utils.list();
+        long[] time = new long[names.size()];
+        for(int i = 0; i < names.size(); i++){
+            time[i] = 0;
+        }
         while((line=reader.readLine())!=null){
             qids.add(utils.lazyExtract(line));
             if(qids.size()>=1000){
                 try{
                     while(qids.size()>0) {
                         lastQid = qids.remove(0);
-                        utils.getResult(lastQid);
+                        String allResult = utils.getResult(lastQid);
+                        TypeReference<ArrayList<output>> typeref = new TypeReference<>() {};
+                        ArrayList<output> outputArray = mapper.readValue(allResult, typeref);
+                        for(output res:outputArray){
+                            for(int i = 0; i < names.size(); i++){
+                                time[i] += res.time.get(i);
+                            }
+                        }
                         offset++;
-                        System.out.println("finish qid:"+lastQid);
                     }
                 } catch (Exception e) {
                     System.out.println("the offset is:"+offset+"at qid:"+lastQid);
                     throw e;
                 }
             }
+        }
+        long sumtime = 0;
+        for(int i = 0; i < names.size(); i++){
+            sumtime += time[i];
+        }
+        for(int i = 0; i < names.size(); i++){
+            System.out.println(names.get(i)+" takes "+time[i]/1000000000.0 + "account for "+ time[i]/sumtime);
         }
     }
 }
