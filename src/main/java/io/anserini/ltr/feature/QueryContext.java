d2c509e3084e9bc11b928bb35a37263a7fd1551f
nsndimt <nsndimt@gmail.com>
2020-10-15 23:45:55 -0400

    rewrite count bigram

--- src/main/java/io/anserini/ltr/feature/QueryContext.java
+++ src/main/java/io/anserini/ltr/feature/QueryContext.java
@@ -1,5 +1,7 @@
 package io.anserini.ltr.feature;
 
+import org.apache.commons.lang3.tuple.Pair;
+
 import java.util.List;
 import java.util.Map;
 
@@ -7,6 +9,8 @@ public class QueryContext {
     public String queryText;
     public List<String> queryTokens;
     public int querySize;
+    public Pair<String, String> queryPair;
+    public Pair<String, String> queryBigram;
     //maybe put pre-retrieval feature here
     public Map<String, Float> cache;
     public QueryContext(List<String> queryTokens){
@@ -14,4 +18,7 @@ public class QueryContext {
         this.queryText = String.join(",", queryTokens);
         this.querySize = queryTokens.size();
     }
+
+
+
 }
