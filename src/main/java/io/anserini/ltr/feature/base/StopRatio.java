34669eb0aa42a37c7e56ae2a3796804db2c9cafc
stephanie <stephaniewhoo@hotmail.com>
2020-11-17 15:17:07 -0500

    add stop ratio

---
+++ src/main/java/io/anserini/ltr/feature/base/StopRatio.java
@@ -0,0 +1,74 @@
+/*
+ * Anserini: A Lucene toolkit for replicable information retrieval research
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.anserini.ltr.feature.base;
+
+import io.anserini.ltr.StopWords;
+import io.anserini.ltr.feature.DocumentContext;
+import io.anserini.ltr.feature.FieldContext;
+import io.anserini.ltr.feature.FeatureExtractor;
+import io.anserini.ltr.feature.QueryContext;
+
+import java.util.List;
+
+/**
+ * StopCover
+ * Compute the ratio of stop/non-stop.
+ */
+public class StopRatio implements FeatureExtractor {
+    private String field;
+
+    public StopRatio() { }
+
+    @Override
+    public float extract(DocumentContext documentContext, QueryContext queryContext) {
+        FieldContext context = documentContext.fieldContexts.get(field);
+        long cov = 0l;
+        float score = 0.0f;
+        List<String> stopWords = StopWords.getStopWordList();
+        for (String stopWord : stopWords) {
+            if (context.termFreqs.containsKey(stopWord)) {
+                cov += context.termFreqs.get(stopWord);
+            }
+        }
+        long nonStop = context.docSize - cov;
+        if(nonStop != 0) {
+            score = cov / nonStop;
+        }
+        return score;
+    }
+
+    @Override
+    public float postEdit(DocumentContext context, QueryContext queryContext) {
+        return queryContext.getSelfLog(context.docId, getName());
+    }
+
+    @Override
+    public String getName() {
+        return "StopRatio";
+    }
+
+    @Override
+    public String getField() {
+        return null;
+    }
+
+    @Override
+    public FeatureExtractor clone() {
+        return new StopRatio();
+    }
+}
+
