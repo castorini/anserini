4650bf19929282a46759d2f9a74b146522961842
stephanie <stephaniewhoo@hotmail.com>
2021-01-03 21:01:04 -0500

    add entityRule implementation + query entities

--- src/main/java/io/anserini/ltr/feature/QueryContext.java
+++ src/main/java/io/anserini/ltr/feature/QueryContext.java
@@ -1,5 +1,6 @@
 package io.anserini.ltr.feature;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.lang3.tuple.Pair;
@@ -12,12 +13,31 @@ public class QueryContext {
     public String qid;
     public Map<String, QueryFieldContext> fieldContexts;
     private Set<String> fieldsToLoad; // analyzed, text, text_unlemm, text_bert_tok
+    public Map<String, List<String>> queryEntities;
 
-    public QueryContext(String qid, Set<String> fieldsToLoad, JsonNode root){
+    public QueryContext(String qid, Set<String> fieldsToLoad, JsonNode root) throws JsonProcessingException {
         this.qid = qid;
         this.fieldsToLoad = fieldsToLoad;
         fieldContexts = new HashMap<>();
+        queryEntities = new HashMap<>();
+        ObjectMapper mapper = new ObjectMapper();
         for(String fieldName: fieldsToLoad)
             fieldContexts.put(fieldName, new QueryFieldContext(fieldName, root));
+        String entityJson = root.get("entity").asText();
+        JsonNode node = mapper.readValue(entityJson, JsonNode.class);
+        Iterator<Map.Entry<String, JsonNode>> ents = node.fields();
+        while(ents.hasNext()) {
+            Map.Entry<String, JsonNode> ent = ents.next();
+            String nameEnt = ent.getKey();
+            String entText = ent.getValue().asText();
+            List<String> temp;
+            if (queryEntities.containsKey(nameEnt)) {
+                temp = queryEntities.get(nameEnt);
+            } else {
+                temp = new ArrayList<>();
+            }
+            temp.add(entText);
+            queryEntities.put(nameEnt, temp);
+        }
     }
 }
--- src/main/java/io/anserini/ltr/feature/base/EntityRule.java
+++ src/main/java/io/anserini/ltr/feature/base/EntityRule.java
@@ -3,35 +3,77 @@ package io.anserini.ltr.feature.base;
 import io.anserini.index.IndexArgs;
 import io.anserini.ltr.feature.*;
 
+import java.util.List;
+
 public class EntityRule implements FeatureExtractor {
     private String field;
     private String qfield;
 
     public EntityRule() {
-        this.field = "entity";
-        this.qfield = "raw";
+        this.field = "contents";
+        this.qfield = "analyzed";
     }
 
-    public EntityRule(String field, String qfield) { // entity
+    public EntityRule(String field, String qfield) {
         this.field = field;
         this.qfield = qfield;
     }
 
+    public float entityCounts(DocumentContext documentContext, String ent) {
+        float ret = 0.0f;
+        if (documentContext.entities.containsKey(ent)) {
+            ret += documentContext.entities.get(ent).size();
+        }
+        return ret;
+    }
+    public float matchCounts(QueryContext queryContext, DocumentContext documentContext, String ent){
+        float ret = 0.0f;
+        if (queryContext.queryEntities.containsKey(ent)) {
+            if (documentContext.entities.containsKey(ent)) {
+                List<String> docEnts = documentContext.entities.get(ent);
+                for (String text : queryContext.queryEntities.get(ent)) {
+                    for (String docText:docEnts) {
+                        if (docText.equals(text)){
+                            ret += 1;
+                        } } } } }
+        return ret;
+    }
+
     @Override
     public float extract(DocumentContext documentContext, QueryContext queryContext) {
-        //call raw
-        String entityJson = documentContext.entityJson;
-        //FieldContext context = documentContext.get("entity");
         QueryFieldContext queryFieldContext = queryContext.fieldContexts.get(qfield);
-        QueryFieldContext entityQuery = queryContext.fieldContexts.get("entity");
         float score = 0.0f;
-        //position check
-        if (queryFieldContext.queryTokens.get(1) == "how"){
-            if (queryFieldContext.queryTokens.get(2) == "long"){
-
-            } else if (queryFieldContext.queryTokens.get(2) == "much"){
-
+        if (queryFieldContext.queryTokens.get(0).equals("how")){
+            // how long, how many, how much
+            if (queryFieldContext.queryTokens.get(1).equals("long")){
+                score += entityCounts(documentContext, "DATE");
+                score += entityCounts(documentContext, "TIME");
+                score += entityCounts(documentContext, "CARDINAL");
+            } else if (queryFieldContext.queryTokens.get(1).equals("much")){
+                score += entityCounts(documentContext, "QUANTITY");
+                score += entityCounts(documentContext, "MONEY");
+            } else if (queryFieldContext.queryTokens.get(1).equals("mani")){
+                if (queryContext.queryEntities.containsKey("DATE")) {
+                    score += entityCounts(documentContext, "DATE");
+                }
+                score += entityCounts(documentContext, "QUANTITY");
+                score += entityCounts(documentContext, "CARDINAL");
             }
+        } else if (queryFieldContext.queryTokens.get(0).equals("where")){
+            // where general counts and specific matching counts
+            score += entityCounts(documentContext, "FAC");
+            score += entityCounts(documentContext, "ORG");
+            score += entityCounts(documentContext, "GPE");
+            score += matchCounts(queryContext,documentContext,"FAC");
+            score += matchCounts(queryContext,documentContext,"ORG");
+            score += matchCounts(queryContext,documentContext,"GPE");
+
+        } else if (queryFieldContext.queryTokens.get(0).equals("when")){
+            score += entityCounts(documentContext, "DATE");
+            score += entityCounts(documentContext, "TIME");
+        } else if (queryFieldContext.queryTokens.get(0).equals("who")){
+            score += entityCounts(documentContext, "PERSON");
+            score += matchCounts(queryContext,documentContext,"PERSON");
         }
 
         return score;
@@ -45,7 +87,7 @@ public class EntityRule implements FeatureExtractor {
 
     @Override
     public String getName() {
-        return String.format("%s_%s_QAHow", field, qfield);
+        return String.format("%s_%s_EntityRule", field, qfield);
     }
 
     @Override
