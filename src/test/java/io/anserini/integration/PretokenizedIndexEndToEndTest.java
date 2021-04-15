a748f5d29b3f4021d72990dd53e7842dd1d107e9
Jimmy Lin <jimmylin@uwaterloo.ca>
2021-04-15 18:57:03 -0400

    Improved e2e testing: implemented getDocumentTokens in IndexReaderUtils (#1521)

--- src/test/java/io/anserini/integration/PretokenizedIndexEndToEndTest.java
+++ src/test/java/io/anserini/integration/PretokenizedIndexEndToEndTest.java
@@ -15,15 +15,11 @@
  */
 package io.anserini.integration;
 
-import io.anserini.collection.DocumentCollection;
 import io.anserini.collection.JsonCollection;
-import io.anserini.collection.TrecCollection;
 import io.anserini.index.IndexArgs;
-import io.anserini.index.IndexCollection;
 import io.anserini.index.generator.DefaultLuceneDocumentGenerator;
 import io.anserini.search.SearchArgs;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -45,34 +41,30 @@ public class PretokenizedIndexEndToEndTest extends EndToEndTest {
   @Override
   protected void setCheckIndexGroundTruth() {
     docCount = 2;
-    documents.put("2000000", Map.of(
+    referenceDocs.put("2000000", Map.of(
       "contents", "this was ##a simple pretokenized test",
       "raw","{\n" +
       "  \"id\" : \"2000000\",\n" +
       "  \"contents\" : \"this was ##a simple pretokenized test\"\n" +
       "}"));
-    documents.put("2000001", Map.of(
+    referenceDocs.put("2000001", Map.of(
       "contents", "some time extra ##vert ##ing and some time intro ##vert ##ing",
       "raw","{\n" +
       "  \"id\" : \"2000001\",\n" +
       "  \"contents\" : \"some time extra ##vert ##ing and some time intro ##vert ##ing\"\n" +
       "}"
     ));
-    tokens.put("2000000", Map.of(
-      "contents", Map.of(
-      "this", 1L, "was", 1L, "##a", 1L, "simple", 1L, "pretokenized", 1L, "test", 1L)));
-    tokens.put("2000001",Map.of(
-      "contents", Map.of(
-      "some", 2L, "time", 2L, "extra", 1L, "##vert", 2L, "##ing", 2L, "and", 1L, "intro", 1L)));
+    referenceDocTokens.put("2000000", Map.of(
+      "contents", List.of("this", "was", "##a", "simple", "pretokenized", "test")));
+    referenceDocTokens.put("2000001", Map.of(
+      "contents", List.of("some", "time", "extra", "##vert", "##ing", "and", "some", "time", "intro", "##vert", "##ing")));
 
     fieldNormStatusTotalFields = 1;
-    // whitespace analyzer keeps everything, includes docid
-    // this is ##a simple pretokenized test some time extra ##vert ##ing and intro 2000000 2000001
     termIndexStatusTermCount = 15;
     termIndexStatusTotFreq = 15;
     storedFieldStatusTotalDocCounts = 2;
     termIndexStatusTotPos = 17 + storedFieldStatusTotalDocCounts;
-    storedFieldStatusTotFields = 6; // 1 docs * (1 id + 1 contents + 1 raw) *2
+    storedFieldStatusTotFields = 6;
   }
 
   @Override
