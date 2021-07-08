dad4b82cba2d879ae20147b2abdd04564331ea6f
Xueguang Ma 马雪光 <x93ma@edu.uwaterloo.ca>
2021-07-08 14:28:51 -0400

    Simple impact searcher (#1592)
    * SimpleImpactSearcher for pyserini

--- src/main/java/io/anserini/search/query/BagOfWordsQueryGenerator.java
+++ src/main/java/io/anserini/search/query/BagOfWordsQueryGenerator.java
@@ -37,10 +37,12 @@ public class BagOfWordsQueryGenerator extends QueryGenerator {
   @Override
   public Query buildQuery(String field, Analyzer analyzer, String queryText) {
     List<String> tokens = AnalyzerUtils.analyze(analyzer, queryText);
-    Map<String, Long> collect = tokens.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    Map<String, Long> collect = tokens.stream()
+        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
     BooleanQuery.Builder builder = new BooleanQuery.Builder();
-    for (String t : collect.keySet()){
-      builder.add(new BoostQuery(new TermQuery(new Term(field, t)), (float)collect.get(t)), BooleanClause.Occur.SHOULD);
+    for (String t : collect.keySet()) {
+      builder.add(new BoostQuery(new TermQuery(new Term(field, t)), (float) collect.get(t)),
+          BooleanClause.Occur.SHOULD);
     }
     return builder.build();
   }
@@ -57,4 +59,26 @@ public class BagOfWordsQueryGenerator extends QueryGenerator {
     }
     return builder.build();
   }
+
+  public Query buildQuery(String field, Map<String, Float> queryTokenWeights) {
+    BooleanQuery.Builder builder = new BooleanQuery.Builder();
+    for (String t : queryTokenWeights.keySet()) {
+      builder.add(new BoostQuery(new TermQuery(new Term(field, t)), queryTokenWeights.get(t)),
+          BooleanClause.Occur.SHOULD);
+    }
+    return builder.build();
+  }
+
+  public Query buildQuery(Map<String, Float> fields, Map<String, Float> queryTokenWeights) {
+    BooleanQuery.Builder builder = new BooleanQuery.Builder();
+    for (Map.Entry<String, Float> entry : fields.entrySet()) {
+      String field = entry.getKey();
+      float boost = entry.getValue();
+
+      Query clause = buildQuery(field, queryTokenWeights);
+      builder.add(new BoostQuery(clause, boost), BooleanClause.Occur.SHOULD);
+    }
+    return builder.build();
+  }
+
 }
