986ac54736294c096270477cc37a2f5f4d68869c
MarcosFP97 <marcosfernandezpichel@gmail.com>
2021-07-09 17:27:17 +0200

    NoCleanC4Generator

---
+++ src/main/java/io/anserini/index/generator/NoCleanC4Generator.java
@@ -0,0 +1,57 @@
+/*
+ * Anserini: A Lucene toolkit for reproducible information retrieval research
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.anserini.index.generator;
+
+import io.anserini.collection.NoCleanC4Collection;
+import io.anserini.index.IndexArgs;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.LongPoint;
+import org.apache.lucene.document.StoredField;
+import org.apache.lucene.document.StringField;
+
+public class NoCleanC4Generator extends DefaultLuceneDocumentGenerator<NoCleanC4Collection.Document>{
+  protected IndexArgs args;
+
+  // constants for storing
+  public enum C4Field {
+    ID("id"),
+    URL("url"),
+    TIMESTAMP("timestamp");
+
+    public final String name;
+
+    C4Field(String s) {
+      name = s;
+    }
+  }
+
+  public NoCleanC4Generator(IndexArgs args) {
+    super(args);
+    this.args = args;
+  }
+
+  public Document createDocument(NoCleanC4Collection.Document doc) throws GeneratorException {
+    Document document = super.createDocument(doc);
+
+    // contents and id stored in superclass method
+    document.add(new StringField(C4Field.URL.name, doc.getUrl(), Field.Store.YES));
+    document.add(new LongPoint(C4Field.TIMESTAMP.name, doc.getTimestamp()));
+    document.add(new StoredField(C4Field.TIMESTAMP.name, doc.getTimestamp()));
+    return document;
+  }
+}
