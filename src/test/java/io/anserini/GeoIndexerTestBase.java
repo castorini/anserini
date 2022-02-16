9ffee2a6b6484716e2ee98edac40d22621f7d3b7
d1shs0ap <42086645+d1shs0ap@users.noreply.github.com>
2022-02-15 23:21:38 -0500

    made changes w.r.t. review

--- src/test/java/io/anserini/GeoIndexerTestBase.java
+++ src/test/java/io/anserini/GeoIndexerTestBase.java
@@ -38,125 +38,125 @@ import java.util.List;
 
 
 public class GeoIndexerTestBase extends LuceneTestCase {
-    protected Path tempDir1;
+  protected Path tempDir1;
 
-    private void buildTestIndex() throws IOException {
-        try {
-            Directory dir = FSDirectory.open(tempDir1);
+  private void buildTestIndex() throws IOException {
+    try {
+      Directory dir = FSDirectory.open(tempDir1);
 
-            IndexWriterConfig config = new IndexWriterConfig();
-            config.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
+      IndexWriterConfig config = new IndexWriterConfig();
+      config.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
 
-            IndexWriter writer = new IndexWriter(dir, config);
+      IndexWriter writer = new IndexWriter(dir, config);
 
+      // Index Lake Ontario (a Polygon) using Polygon.fromGeoJSON
+      Document doc1 = new Document();
+      Path path1 = Paths.get("src/test/resources/sample_docs/geosearch/lake_ontario.geojson");
+      byte[] encoded = Files.readAllBytes(path1);
+      String s = new String(encoded);
+      Polygon lakeOntario = Polygon.fromGeoJSON(s)[0];
 
-            Document doc1 = new Document();
-            Path path1 = Paths.get("src/test/resources/sample_docs/geosearch/lake_ontario.geojson");
-            byte[] encoded = Files.readAllBytes(path1);
-            String s = new String(encoded);
-            Polygon lakeOntario = Polygon.fromGeoJSON(s)[0];
+      Field[] fields = LatLonShape.createIndexableFields("geometry", lakeOntario);
+      for (Field f: fields) {
+        doc1.add(f);
+      }
+      writer.addDocument(doc1);
 
-            Field[] fields = LatLonShape.createIndexableFields("geometry", lakeOntario);
-            for (Field f: fields) {
-                doc1.add(f);
-            }
-            writer.addDocument(doc1);
+      // Index Polygon with a hole in it using SimpleWKTShapeParser
+      Document doc2 = new Document();
+      Path path2 = Paths.get("src/test/resources/sample_docs/geosearch/self_containing_polygon.wkt");
+      List<String> listLines2 = Files.readAllLines(path2);
+      String[] lines2 = listLines2.toArray(new String[0]);
+      Polygon polygonWithHole = (Polygon) SimpleWKTShapeParser.parse(lines2[0]);
 
+      Field[] fields2 = LatLonShape.createIndexableFields("geometry", polygonWithHole);
+      for (Field f: fields2) {
+        doc2.add(f);
+      }
+      writer.addDocument(doc2);
 
-            Document doc2 = new Document();
-            Path path2 = Paths.get("src/test/resources/sample_docs/geosearch/self_containing_polygon.wkt");
-            List<String> listLines2 = Files.readAllLines(path2);
-            String[] lines2 = listLines2.toArray(new String[0]);
-            Polygon polygonWithHole = (Polygon) SimpleWKTShapeParser.parse(lines2[0]);
+      // Index MultiPolygon using SimpleWKTShapeParser
+      Document doc3 = new Document();
+      Path path3 = Paths.get("src/test/resources/sample_docs/geosearch/multipolygon.wkt");
+      List<String> listLines3 = Files.readAllLines(path3);
+      String[] lines3 = listLines3.toArray(new String[0]);
+      Polygon[] multipolygon = (Polygon[]) SimpleWKTShapeParser.parse(lines3[0]);
 
-            Field[] fields2 = LatLonShape.createIndexableFields("geometry", polygonWithHole);
-            for (Field f: fields2) {
-                doc2.add(f);
-            }
-            writer.addDocument(doc2);
-
-
-            Document doc3 = new Document();
-            Path path3 = Paths.get("src/test/resources/sample_docs/geosearch/multipolygon.wkt");
-            List<String> listLines3 = Files.readAllLines(path3);
-            String[] lines3 = listLines3.toArray(new String[0]);
-            Polygon[] multipolygon = (Polygon[]) SimpleWKTShapeParser.parse(lines3[0]);
-
-            for (Polygon p: multipolygon) {
-                Field[] fields3 = LatLonShape.createIndexableFields("geometry", p);
-                for (Field f: fields3) {
-                    doc3.add(f);
-                }
-            }
-            writer.addDocument(doc3);
-
-
-            Document doc4 = new Document();
-            Path path4 = Paths.get("src/test/resources/sample_docs/geosearch/line.wkt");
-            List<String> listLines4 = Files.readAllLines(path4);
-            String[] lines4 = listLines4.toArray(new String[0]);
-            Line lineShape = (Line) SimpleWKTShapeParser.parse(lines4[0]);
-
-            Field[] fields4 = LatLonShape.createIndexableFields("geometry", lineShape);
-            for (Field f: fields4) {
-                doc4.add(f);
-            }
-            writer.addDocument(doc4);
-
-
-            Document doc5 = new Document();
-            Path path5 = Paths.get("src/test/resources/sample_docs/geosearch/multiline.wkt");
-            List<String> listLines5 = Files.readAllLines(path5);
-            String[] lines5 = listLines5.toArray(new String[0]);
-            Line[] lineShapes = (Line[]) SimpleWKTShapeParser.parse(lines5[0]);
-
-            for (Line l: lineShapes) {
-                Field[] fields5 = LatLonShape.createIndexableFields("geometry", l);
-                for (Field f : fields5) {
-                    doc5.add(f);
-                }
-            }
-            writer.addDocument(doc5);
-
-
-            Document doc6 = new Document();
-            Path path6 = Paths.get("src/test/resources/sample_docs/geosearch/grand_river.wkt");
-            List<String> listLines6 = Files.readAllLines(path6);
-            String[] lines6 = listLines6.toArray(new String[0]);
-            Line grand_river = (Line) SimpleWKTShapeParser.parse(lines6[0]);
-
-            Field[] fields6 = LatLonShape.createIndexableFields("geometry", grand_river);
-            for (Field f: fields6) {
-                doc6.add(f);
-            }
-            writer.addDocument(doc6);
-
-
-            writer.commit();
-            writer.forceMerge(1);
-            writer.close();
-
-            dir.close();
-
-        } catch (ParseException e) {
-            e.printStackTrace();
+      for (Polygon p: multipolygon) {
+        Field[] fields3 = LatLonShape.createIndexableFields("geometry", p);
+        for (Field f: fields3) {
+          doc3.add(f);
         }
-    }
+      }
+      writer.addDocument(doc3);
 
-    @Before
-    @Override
-    public void setUp() throws Exception {
-        super.setUp();
+      // Index LineString using SimpleWKTShapeParser
+      Document doc4 = new Document();
+      Path path4 = Paths.get("src/test/resources/sample_docs/geosearch/line.wkt");
+      List<String> listLines4 = Files.readAllLines(path4);
+      String[] lines4 = listLines4.toArray(new String[0]);
+      Line lineShape = (Line) SimpleWKTShapeParser.parse(lines4[0]);
 
-        tempDir1 = createTempDir();
-        buildTestIndex();
-    }
+      Field[] fields4 = LatLonShape.createIndexableFields("geometry", lineShape);
+      for (Field f: fields4) {
+        doc4.add(f);
+      }
+      writer.addDocument(doc4);
 
-    @After
-    @Override
-    public void tearDown() throws Exception {
-        // Call garbage collector for Windows compatibility
-        System.gc();
-        super.tearDown();
+      // Index MultiLineString using SimpleWKTShapeParser
+      Document doc5 = new Document();
+      Path path5 = Paths.get("src/test/resources/sample_docs/geosearch/multiline.wkt");
+      List<String> listLines5 = Files.readAllLines(path5);
+      String[] lines5 = listLines5.toArray(new String[0]);
+      Line[] lineShapes = (Line[]) SimpleWKTShapeParser.parse(lines5[0]);
+
+      for (Line l: lineShapes) {
+        Field[] fields5 = LatLonShape.createIndexableFields("geometry", l);
+        for (Field f : fields5) {
+          doc5.add(f);
+        }
+      }
+      writer.addDocument(doc5);
+
+      // Index Grand River (a LineString) using SimpleWKTShapeParser
+      Document doc6 = new Document();
+      Path path6 = Paths.get("src/test/resources/sample_docs/geosearch/grand_river.wkt");
+      List<String> listLines6 = Files.readAllLines(path6);
+      String[] lines6 = listLines6.toArray(new String[0]);
+      Line grand_river = (Line) SimpleWKTShapeParser.parse(lines6[0]);
+
+      Field[] fields6 = LatLonShape.createIndexableFields("geometry", grand_river);
+      for (Field f: fields6) {
+        doc6.add(f);
+      }
+      writer.addDocument(doc6);
+
+
+      writer.commit();
+      writer.forceMerge(1);
+      writer.close();
+
+      dir.close();
+
+    } catch (ParseException e) {
+      e.printStackTrace();
     }
+  }
+
+  @Before
+  @Override
+  public void setUp() throws Exception {
+    super.setUp();
+
+    tempDir1 = createTempDir();
+    buildTestIndex();
+  }
+
+  @After
+  @Override
+  public void tearDown() throws Exception {
+    // Call garbage collector for Windows compatibility
+    System.gc();
+    super.tearDown();
+  }
 }
