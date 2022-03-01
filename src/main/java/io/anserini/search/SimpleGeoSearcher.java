50eab10f660595a5e96ced837b090323117436d8
d1shs0ap <42086645+d1shs0ap@users.noreply.github.com>
2022-02-28 21:34:26 -0500

    Hydrorivers search (#1781)
    + add a "bridge" searcher class for geo queries to be called by Python backend
    + add string "id" field to existing index geo generator class

---
+++ src/main/java/io/anserini/search/SimpleGeoSearcher.java
@@ -0,0 +1,67 @@
+package io.anserini.search;
+import io.anserini.index.IndexArgs;
+import io.anserini.rerank.ScoredDocuments;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.LatLonShape;
+import org.apache.lucene.document.ShapeField;
+import org.apache.lucene.geo.LatLonGeometry;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexableField;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.FSDirectory;
+import java.io.Closeable;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
+public class SimpleGeoSearcher extends SimpleSearcher implements Closeable {
+  private IndexReader reader;
+  private IndexSearcher searcher = null;
+
+  public Result[] searchGeo(Query query, int k) throws IOException {
+    if (searcher == null) {
+      searcher = new IndexSearcher(reader);
+    }
+
+    TopDocs rs = searcher.search(query, k);
+    ScoredDocuments hits = ScoredDocuments.fromTopDocs(rs, searcher);
+    Result[] results = new Result[hits.ids.length];
+
+    for (int i = 0; i < hits.ids.length; i++) {
+      Document doc = hits.documents[i];
+      String docId = doc.getField(IndexArgs.ID).stringValue();
+
+      IndexableField field;
+      field = doc.getField(IndexArgs.CONTENTS);
+      String contents = field == null ? null : field.stringValue();
+
+      field = doc.getField(IndexArgs.RAW);
+      String raw = field == null ? null : field.stringValue();
+
+      results[i] = new Result(docId, hits.ids[i], hits.scores[i], contents, raw, doc);
+    }
+
+    return results;
+  }
+
+  @Override
+  public void close() throws IOException {
+    reader.close();
+  }
+
+  public SimpleGeoSearcher(String indexDir) throws IOException {
+    Path indexPath = Paths.get(indexDir);
+
+    if (!Files.exists(indexPath) || !Files.isDirectory(indexPath) || !Files.isReadable(indexPath)) {
+      throw new IllegalArgumentException(indexDir + " does not exist or is not a directory.");
+    }
+
+    reader = DirectoryReader.open(FSDirectory.open(indexPath));
+    searcher = new IndexSearcher(reader);
+  }
+}
