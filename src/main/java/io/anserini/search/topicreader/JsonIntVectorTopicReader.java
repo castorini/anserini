c4a559825bf1d4e3a05b45a1c881ac7c3ee1ea69
Xueguang <x93ma@edu.uwaterloo.ca>
2022-08-05 20:45:04 -0400

    fix indentation

--- src/main/java/io/anserini/search/topicreader/JsonIntVectorTopicReader.java
+++ src/main/java/io/anserini/search/topicreader/JsonIntVectorTopicReader.java
@@ -13,23 +13,23 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JsonIntVectorTopicReader extends TopicReader<Integer> {
 
-    public JsonIntVectorTopicReader(Path topicFile) {
-        super(topicFile);
-    }
+  public JsonIntVectorTopicReader(Path topicFile) {
+    super(topicFile);
+  }
 
-    @Override
-    public SortedMap<Integer, Map<String, String>> read(BufferedReader reader) throws IOException {
-        SortedMap<Integer, Map<String, String>> map = new TreeMap<>();
-        String line;
-        ObjectMapper mapper = new ObjectMapper();
-        while ((line = reader.readLine()) != null) {
-            line = line.trim();
-            JsonNode lineNode = mapper.readerFor(JsonNode.class).readTree(line);
-            Integer topicID = lineNode.get("qid").asInt();
-            Map<String, String> fields = new HashMap<>();
-            fields.put("vector", lineNode.get("vector").toString());
-            map.put(topicID, fields);
-        }
-        return map;
+  @Override
+  public SortedMap<Integer, Map<String, String>> read(BufferedReader reader) throws IOException {
+    SortedMap<Integer, Map<String, String>> map = new TreeMap<>();
+    String line;
+    ObjectMapper mapper = new ObjectMapper();
+    while ((line = reader.readLine()) != null) {
+      line = line.trim();
+      JsonNode lineNode = mapper.readerFor(JsonNode.class).readTree(line);
+      Integer topicID = lineNode.get("qid").asInt();
+      Map<String, String> fields = new HashMap<>();
+      fields.put("vector", lineNode.get("vector").toString());
+      map.put(topicID, fields);
     }
+    return map;
+  }
 }
--- src/main/java/io/anserini/search/topicreader/JsonStringVectorTopicReader.java
+++ src/main/java/io/anserini/search/topicreader/JsonStringVectorTopicReader.java
@@ -13,23 +13,23 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JsonStringVectorTopicReader extends TopicReader<String> {
 
-    public JsonStringVectorTopicReader(Path topicFile) {
-        super(topicFile);
-    }
+  public JsonStringVectorTopicReader(Path topicFile) {
+    super(topicFile);
+  }
 
-    @Override
-    public SortedMap<String, Map<String, String>> read(BufferedReader reader) throws IOException {
-        SortedMap<String, Map<String, String>> map = new TreeMap<>();
-        String line;
-        ObjectMapper mapper = new ObjectMapper();
-        while ((line = reader.readLine()) != null) {
-            line = line.trim();
-            JsonNode lineNode = mapper.readerFor(JsonNode.class).readTree(line);
-            String topicID = lineNode.get("qid").asText();
-            Map<String, String> fields = new HashMap<>();
-            fields.put("vector", lineNode.get("vector").toString());
-            map.put(topicID, fields);
-        }
-        return map;
+  @Override
+  public SortedMap<String, Map<String, String>> read(BufferedReader reader) throws IOException {
+    SortedMap<String, Map<String, String>> map = new TreeMap<>();
+    String line;
+    ObjectMapper mapper = new ObjectMapper();
+    while ((line = reader.readLine()) != null) {
+      line = line.trim();
+      JsonNode lineNode = mapper.readerFor(JsonNode.class).readTree(line);
+      String topicID = lineNode.get("qid").asText();
+      Map<String, String> fields = new HashMap<>();
+      fields.put("vector", lineNode.get("vector").toString());
+      map.put(topicID, fields);
     }
+    return map;
+  }
 }
