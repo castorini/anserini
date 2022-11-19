d588fd4a6c11df4f269eac14208db17573bcc7c0
Jonathan Leitschuh <jonathan.leitschuh@gmail.com>
2022-11-19 04:03:56 -0800

    vuln-fix: Temporary File Information Disclosure (#2025)

--- src/test/java/io/anserini/ltr/RunListTest.java
+++ src/test/java/io/anserini/ltr/RunListTest.java
@@ -24,12 +24,13 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.concurrent.ExecutionException;
 
 public class RunListTest  extends BaseFeatureExtractorTest<Integer>{
     @Test
     public void testSingleDoc() throws IOException, ExecutionException, InterruptedException {
-        File tempFile = File.createTempFile("tempFile", ".txt");
+        File tempFile = Files.createTempFile("tempFile", ".txt").toFile();
         tempFile.deleteOnExit();
         BufferedWriter writer = new BufferedWriter(new FileWriter("tempFile.txt"));
         writer.write("1\tQ0\tdoc0\t1\t3.0\tanserini\n");
