1ee03a6c507e306511bdf0edf01fd576feb65256
theyorubayesian <akin.o.oladipo@gmail.com>
2023-03-26 15:21:12 -0400

    minor fixes

--- src/main/java/io/anserini/analysis/CompositeAnalyzer.java
+++ src/main/java/io/anserini/analysis/CompositeAnalyzer.java
@@ -37,71 +37,71 @@
  import java.util.Map;
  import java.util.concurrent.ConcurrentHashMap;
  
- public class CompositeAnalyzer extends Analyzer {
-   private static final Logger LOG = LogManager.getLogger(CompositeAnalyzer.class);
-   private final HuggingFaceTokenizer tokenizer;
-   private final Analyzer analyzer;
+public class CompositeAnalyzer extends Analyzer {
+  private static final Logger LOG = LogManager.getLogger(CompositeAnalyzer.class);
+  private final HuggingFaceTokenizer tokenizer;
+  private final Analyzer analyzer;
+
+  private static HuggingFaceTokenizer makeTokenizer(String tokenizerNameOrPath) throws IOException {
+    Map<String, String> options = new ConcurrentHashMap<>();
+    options.put("addSpecialTokens", "false");
+    Path path = Paths.get(tokenizerNameOrPath);
+    
+    if (Files.exists(path) == true) {
+      return HuggingFaceTokenizer.newInstance(path, options);
+    } else {
+      return HuggingFaceTokenizer.newInstance(tokenizerNameOrPath, options);
+    }
+  }
  
-   private static HuggingFaceTokenizer makeTokenizer(String tokenizerNameOrPath) throws IOException {
-     Map<String, String> options = new ConcurrentHashMap<>();
-     options.put("addSpecialTokens", "false");
-     Path path = Paths.get(tokenizerNameOrPath);
-     
-     if (Files.exists(path) == true) {
-       return HuggingFaceTokenizer.newInstance(path, options);
-     } else {
-       return HuggingFaceTokenizer.newInstance(tokenizerNameOrPath, options);
-     }
-   }
+  public CompositeAnalyzer(String tokenizerNameOrPath, Analyzer analyzer) throws IOException {
+    this(makeTokenizer(tokenizerNameOrPath), analyzer);
+  }
+
+  public CompositeAnalyzer(HuggingFaceTokenizer tokenizer, Analyzer analyzer) {
+    this.tokenizer = tokenizer;
+    this.analyzer = analyzer;	
+  }
  
-   public CompositeAnalyzer(String tokenizerNameOrPath, Analyzer analyzer) throws IOException {
-     this(makeTokenizer(tokenizerNameOrPath), analyzer);
-   }
+  private Reader applyAnalyzers(Reader reader) throws IOException {
+    String readerString = IOUtils.toString(reader);
+    TokenStream tokenStream = analyzer.tokenStream(null, readerString);
+    CharTermAttribute termAtt = tokenStream.addAttribute(CharTermAttribute.class);
+    List<String> analyzerOutput = new ArrayList<>();
+
+    tokenStream.reset();
+
+    while (tokenStream.incrementToken()) {
+      String termAttStr = termAtt.toString();
+      if (termAtt.toString().length() == 0) { continue; }
+      analyzerOutput.add("bm25_" + termAttStr);
+    }
+
+    tokenStream.end();
+    tokenStream.close();
+
+    String analyzedString = String.join(" ", analyzerOutput);
+    String[] tokens = this.tokenizer.encode(readerString).getTokens();
+    // Important for empty documents
+    String tokenizedString = tokens.length > 0 ? "bm25wp_" + String.join(" bm25wp_", tokens) : "";
+
+    return new StringReader(String.join(" ", 	new String[]{tokenizedString, analyzedString}));
+  }
  
-   public CompositeAnalyzer(HuggingFaceTokenizer tokenizer, Analyzer analyzer) {
-     this.tokenizer = tokenizer;
-     this.analyzer = analyzer;	
-   }
+  @Override
+  protected Reader initReader(String fieldName, Reader reader) {
+    try { 
+      return applyAnalyzers(reader);
+    } catch (IOException e) {
+      LOG.error("Error applying analyzers: " + e);
+    }
+    return reader;
+  }
  
-   private Reader applyAnalyzers(Reader reader) throws IOException {
-     String readerString = IOUtils.toString(reader);
-     TokenStream tokenStream = analyzer.tokenStream(null, readerString);
-     CharTermAttribute termAtt = tokenStream.addAttribute(CharTermAttribute.class);
-     List<String> analyzerOutput = new ArrayList<>();
- 
-     tokenStream.reset();
- 
-     while (tokenStream.incrementToken()) {
-       String termAttStr = termAtt.toString();
-       if (termAtt.toString().length() == 0) { continue; }
-       analyzerOutput.add("bm25_" + termAttStr);
-     }
- 
-     tokenStream.end();
-     tokenStream.close();
- 
-     String analyzedString = String.join(" ", analyzerOutput);
-     String[] tokens = this.tokenizer.encode(readerString).getTokens();
-     // Important for empty documents
-     String tokenizedString = tokens.length > 0 ? "bm25wp_" + String.join(" bm25wp_", tokens) : "";
- 
-     return new StringReader(String.join(" ", 	new String[]{tokenizedString, analyzedString}));
-   }
- 
-   @Override
-   protected Reader initReader(String fieldName, Reader reader) {
-     try { 
-       return applyAnalyzers(reader);
-     } catch (IOException e) {
-       LOG.error("Error applying analyzers: " + e);
-     }
-     return reader;
-   }
- 
-   @Override
-   protected TokenStreamComponents createComponents(String fieldName) {
-     Tokenizer wTokenizer = new WhitespaceTokenizer();
-     return new TokenStreamComponents(wTokenizer);
-   }
- }
+  @Override
+  protected TokenStreamComponents createComponents(String fieldName) {
+    Tokenizer wTokenizer = new WhitespaceTokenizer();
+    return new TokenStreamComponents(wTokenizer);
+  }
+}
  
