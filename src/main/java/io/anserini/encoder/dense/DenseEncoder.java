a699c606d5c47133062ef57e33988fc94a7e759f
Arthur Chen <chenhaonan189@gmail.com>
2023-11-07 16:53:39 +0000

    fix indentation formatting

--- src/main/java/io/anserini/encoder/dense/DenseEncoder.java
+++ src/main/java/io/anserini/encoder/dense/DenseEncoder.java
@@ -17,47 +17,47 @@ import ai.onnxruntime.OrtException;
  * DenseEncoder
  */
 public abstract class DenseEncoder {
-    static private final String CACHE_DIR = Paths.get(System.getProperty("user.home"), "/.cache/anserini/encoders/dense")
-            .toString();
+  static private final String CACHE_DIR = Paths.get(System.getProperty("user.home"), "/.cache/anserini/encoders/dense")
+      .toString();
 
-    public abstract float[] encode(String query) throws OrtException;
+  public abstract float[] encode(String query) throws OrtException;
 
-    protected static long[] convertTokensToIds(BertFullTokenizer tokenizer, List<String> tokens, Vocabulary vocab) {
-        int numTokens = tokens.size();
-        long[] tokenIds = new long[numTokens];
-        for (int i = 0; i < numTokens; ++i) {
-            tokenIds[i] = vocab.getIndex(tokens.get(i));
-        }
-        return tokenIds;
+  protected static long[] convertTokensToIds(BertFullTokenizer tokenizer, List<String> tokens, Vocabulary vocab) {
+    int numTokens = tokens.size();
+    long[] tokenIds = new long[numTokens];
+    for (int i = 0; i < numTokens; ++i) {
+      tokenIds[i] = vocab.getIndex(tokens.get(i));
     }
+    return tokenIds;
+  }
 
-    static protected Path getModelPath(String modelName, String modelURL) throws IOException {
-        File modelFile = new File(getCacheDir(), modelName);
-        if (!modelFile.exists()) {
-            System.out.println("Downloading model");
-            FileUtils.copyURLToFile(new URL(modelURL), modelFile);
-        } else {
-            System.out.println("Model already exists");
-        }
-        return modelFile.toPath();
+  static protected Path getModelPath(String modelName, String modelURL) throws IOException {
+    File modelFile = new File(getCacheDir(), modelName);
+    if (!modelFile.exists()) {
+      System.out.println("Downloading model");
+      FileUtils.copyURLToFile(new URL(modelURL), modelFile);
+    } else {
+      System.out.println("Model already exists");
     }
+    return modelFile.toPath();
+  }
 
-    static protected Path getVocabPath(String vocabName, String vocabURL) throws IOException {
-        File vocabFile = new File(getCacheDir(), vocabName);
-        if (!vocabFile.exists()) {
-            System.out.println("Downloading vocab");
-            FileUtils.copyURLToFile(new URL(vocabURL), vocabFile);
-        } else {
-            System.out.println("Vocab already exists");
-        }
-        return vocabFile.toPath();
+  static protected Path getVocabPath(String vocabName, String vocabURL) throws IOException {
+    File vocabFile = new File(getCacheDir(), vocabName);
+    if (!vocabFile.exists()) {
+      System.out.println("Downloading vocab");
+      FileUtils.copyURLToFile(new URL(vocabURL), vocabFile);
+    } else {
+      System.out.println("Vocab already exists");
     }
+    return vocabFile.toPath();
+  }
 
-    static protected String getCacheDir() {
-        File cacheDir = new File(CACHE_DIR);
-        if (!cacheDir.exists()) {
-            cacheDir.mkdir();
-        }
-        return cacheDir.getPath();
+  static protected String getCacheDir() {
+    File cacheDir = new File(CACHE_DIR);
+    if (!cacheDir.exists()) {
+      cacheDir.mkdir();
     }
+    return cacheDir.getPath();
+  }
 }
