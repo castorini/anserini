85ae209317cf45d35b4b07638fed44ba4e66d152
wuming233 <stevenchenzijian@icloud.com>
2024-04-28 23:29:36 -0400

    Added RunMsMarco V2.1, Added RunBeir

--- src/main/java/io/anserini/reproduce/RunMsMarco.java
+++ src/main/java/io/anserini/reproduce/RunMsMarco.java
@@ -20,12 +20,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
-import io.anserini.reproduce.RunMsMarco.Topic;
-import io.anserini.reproduce.RunMsMarco.TrecEvalMetricDefinitions;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.List;
@@ -39,66 +33,81 @@ public class RunMsMarco {
 
   private static final String FAIL = RED + "[FAIL]" + RESET;
 
-  private static final String COLLECTION = "msmarco-v1-passage";
- 
+  private static String COLLECTION = "msmarco-v1-passage";
+
   public static void main(String[] args) throws Exception {
-    final ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-    try (InputStream inputStream = RunMsMarco.class.getClassLoader()
-        .getResourceAsStream("reproduce/msmarco-v1-passage.yaml")) {
-      if (inputStream == null) {
-        throw new FileNotFoundException("Resource file not found. Please check the file path.");
+
+    // check for cmd option
+    if (args.length >= 2 && "-v".equals(args[0])) {
+      String collectionVersion = args[1];
+      switch (collectionVersion) {
+          case "1":
+              COLLECTION = "msmarco-v1-passage";
+              break;
+          case "2":
+              COLLECTION = "msmarco-v2.1-doc";
+              break;
+          case "2seg":
+              COLLECTION = "msmarco-v2.1-doc-segmented";
+              break;
+          default:
+              System.out.println(FAIL + " Invalid MsMarco version; please specify (1/2/2seg). Using default: " + COLLECTION);
+              break;
       }
-      Config config = mapper.readValue(inputStream, Config.class);
-      TrecEvalMetricDefinitions metricDefinitions = new TrecEvalMetricDefinitions();
+    }
+
+    final ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+
+    Config config = mapper.readValue(RunMsMarco.class.getClassLoader()
+    .getResourceAsStream("reproduce/" + COLLECTION + ".yaml"), Config.class);
   
-      for (Condition condition : config.conditions) {
-        System.out.println(String.format("# Running condition \"%s\": %s \n", condition.name, condition.display));
-        for (Topic topic : condition.topics) {
-          System.out.println("  - topic_key: " + topic.topic_key + "\n");
-  
-          final String output = String.format("runs/run.%s.%s.%s.txt", COLLECTION, condition.name, topic.topic_key);
-  
-          final String command = condition.command
-              .replace("$threads", "16")
-              .replace("$topics", topic.topic_key)
-              .replace("$output", output);
-  
-          System.out.println("    Running retrieval command: " + command);
-  
-          ProcessBuilder pb = new ProcessBuilder(command.split(" "));
-          Process process = pb.start();
-          int resultCode = process.waitFor();
-          if (resultCode == 0) {
-            System.out.println("    Run successfully completed!");
-          } else {
-            System.out.println("    Run failed!");
-          }
-          System.out.println("");
-  
-          // running the evaluation command
-          Map<String, Map<String, String>> evalCommands = metricDefinitions.getMetricDefinitions().get(COLLECTION);
-          InputStream stdout = null;
-  
-          for (Map<String, Double> expected : topic.scores) {
-            for (String metric : expected.keySet()) {
-              String evalKey = topic.eval_key;
-              String evalCmd = "bin/trec_eval " + evalCommands.get(evalKey).get(metric) + " " + evalKey + " " + output;
-  
-              pb = new ProcessBuilder(evalCmd.split(" "));
-              process = pb.start();
-  
-              resultCode = process.waitFor();
-              stdout = process.getInputStream();
-              if (resultCode == 0) {
-                String scoreString = new String(stdout.readAllBytes()).replaceAll(".*?(\\d+\\.\\d+)$", "$1").trim();
-                Double score = Double.parseDouble(scoreString);
-                Double delta = Math.abs(score - expected.get(metric));
-  
-                if (delta > 0.00005) {
-                  System.out.println(String.format("    %7s: %.4f %s expected %.4f", metric, score, FAIL, expected.get(metric)));
-                } else {
-                  System.out.println(String.format("    %7s: %.4f [OK]", metric, score));
-                }
+    TrecEvalMetricDefinitions metricDefinitions = new TrecEvalMetricDefinitions();
+
+    for (Condition condition : config.conditions) {
+      System.out.println(String.format("# Running condition \"%s\": %s \n", condition.name, condition.display));
+      for (Topic topic : condition.topics) {
+        System.out.println("  - topic_key: " + topic.topic_key + "\n");
+
+        final String output = String.format("runs/run.%s.%s.%s.txt", COLLECTION, condition.name, topic.topic_key);
+
+        final String command = condition.command
+            .replace("$threads", "16")
+            .replace("$topics", topic.topic_key)
+            .replace("$output", output);
+
+        System.out.println("    Running retrieval command: " + command);
+
+        ProcessBuilder pb = new ProcessBuilder(command.split(" "));
+        Process process = pb.start();
+        int resultCode = process.waitFor();
+        if (resultCode == 0) {
+          System.out.println("    Run successfully completed!");
+        } else {
+          System.out.println("    Run failed!");
+        }
+        System.out.println("");
+
+        // running the evaluation command
+        Map<String, Map<String, String>> evalCommands = metricDefinitions.getMetricDefinitions().get(COLLECTION);
+        InputStream stdout = null;
+
+        for (Map<String, Double> expected : topic.scores) {
+          for (String metric : expected.keySet()) {
+            String evalKey = topic.eval_key;
+            String evalCmd = "bin/trec_eval " + evalCommands.get(evalKey).get(metric) + " " + evalKey + " " + output;
+
+            pb = new ProcessBuilder(evalCmd.split(" "));
+            process = pb.start();
+
+            resultCode = process.waitFor();
+            stdout = process.getInputStream();
+            if (resultCode == 0) {
+              String scoreString = new String(stdout.readAllBytes()).replaceAll(".*?(\\d+\\.\\d+)$", "$1").trim();
+              Double score = Double.parseDouble(scoreString);
+              Double delta = Math.abs(score - expected.get(metric));
+
+              if (delta > 0.00005) {
+                System.out.println(String.format("    %7s: %.4f %s expected %.4f", metric, score, FAIL, expected.get(metric)));
               } else {
                 System.out.println("Evaluation command failed for metric: " + metric);
               }
@@ -174,6 +183,48 @@ public class RunMsMarco {
       msmarcoV1Passage.put("tools/topics-and-qrels/qrels.dl20-passage.txt", dl20PassageMetrics);
 
       metricDefinitions.put("msmarco-v1-passage", msmarcoV1Passage);
+
+      Map<String, Map<String, String>> msmarcoV2Passage = new HashMap<>();
+
+      // msmarco-v2.1-doc definitions
+      Map<String, String> msmarco2Dev1Metrics = new HashMap<>();
+      msmarco2Dev1Metrics.put("MRR@10", "-c -M 100 -m recip_rank");
+      msmarcoV2Passage.put("tools/topics-and-qrels/qrels.msmarco-v2.1-doc.dev.txt",
+          msmarco2Dev1Metrics);
+      
+      Map<String, String> msmarco2Dev2Metrics = new HashMap<>();
+      msmarco2Dev2Metrics.put("MRR@10", "-c -M 100 -m recip_rank");
+      msmarcoV2Passage.put("tools/topics-and-qrels/qrels.msmarco-v2.1-doc.dev2.txt",
+          msmarco2Dev2Metrics);
+
+      Map<String, String> dl21PassageMetrics = new HashMap<>();
+      dl21PassageMetrics.put("MAP", "-c -M 100 -m map");
+      dl21PassageMetrics.put("MRR@10", "-c -M 100 -m recip_rank");
+      dl21PassageMetrics.put("nDCG@10", "-c -m ndcg_cut.10");
+      dl21PassageMetrics.put("R@100", "-c -m recall.100");
+      dl21PassageMetrics.put("R@1K", "-c -m recall.1000");
+      msmarcoV2Passage.put("tools/topics-and-qrels/qrels.dl21-doc-msmarco-v2.1.txt", dl21PassageMetrics);
+
+      Map<String, String> dl22PassageMetrics = new HashMap<>();
+      dl22PassageMetrics.put("MAP", "-c -M 100 -m map");
+      dl22PassageMetrics.put("MRR@10", "-c -M 100 -m recip_rank");
+      dl22PassageMetrics.put("nDCG@10", "-c -m ndcg_cut.10");
+      dl22PassageMetrics.put("R@100", "-c -m recall.100");
+      dl22PassageMetrics.put("R@1K", "-c -m recall.1000");
+      msmarcoV2Passage.put("tools/topics-and-qrels/qrels.dl22-doc-msmarco-v2.1.txt", dl22PassageMetrics);
+
+      Map<String, String> dl23PassageMetrics = new HashMap<>();
+      dl23PassageMetrics.put("MAP", "-c -M 100 -m map");
+      dl23PassageMetrics.put("MRR@10", "-c -M 100 -m recip_rank");
+      dl23PassageMetrics.put("nDCG@10", "-c -m ndcg_cut.10");
+      dl23PassageMetrics.put("R@100", "-c -m recall.100");
+      dl23PassageMetrics.put("R@1K", "-c -m recall.1000");
+      msmarcoV2Passage.put("tools/topics-and-qrels/qrels.dl23-doc-msmarco-v2.1.txt", dl23PassageMetrics);
+
+      metricDefinitions.put("msmarco-v2.1-doc", msmarcoV2Passage);
+
+      // msmarco-v2.1-doc-segmented definitions; same as msmarco-v2.1-doc
+      metricDefinitions.put("msmarco-v2.1-doc-segmented", metricDefinitions.get("msmarco-v2.1-doc"));
     }
 
     public Map<String, Map<String, Map<String, String>>> getMetricDefinitions() {
