84319bd531daac4e98985126621256d3189e5620
16BitNarwhal <ericzh1616@gmail.com>
2024-05-02 13:21:27 -0400

    added all indexes and grouped them

--- src/main/frontend/pages/components/SearchBar.tsx
+++ src/main/frontend/pages/components/SearchBar.tsx
@@ -1,12 +1,82 @@
 // components/SearchBar.tsx
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { QueryResult } from '../../types/QueryResult';
 
 const SearchBar: React.FC = () => {
   const [loading, setLoading] = useState<boolean>(false);
   const [results, setResults] = useState<QueryResult[]>([]);
   const [query, setQuery] = useState<string>('');
+  
+  const [group, setGroup] = useState<string>('msmarco-v1-passage');
   const [collection, setCollection] = useState<string>('');
+  const [collections, setCollections] = useState<{[key: string]: string[]}>({
+    'msmarco-v1-passage': [
+      'msmarco-v1-passage',
+      'msmarco-v1-passage.splade-pp-ed',
+      'msmarco-v1-passage.cos-dpr-distil',
+      'msmarco-v1-passage.cos-dpr-distil.quantized',
+      'msmarco-v1-passage.bge-base-en-v1.5',
+      'msmarco-v1-passage.bge-base-en-v1.5.quantized',
+      'msmarco-v1-passage.cohere-embed-english-v3.0',
+      'msmarco-v1-passage.cohere-embed-english-v3.0.quantized',
+    ],
+    'msmarco-v2-passage': [
+      'msmarco-v2-passage',
+    ],
+    'msmarco-v2-doc': [
+      'msmarco-v2-doc',
+      'msmarco-v2-doc-segmented',
+    ],
+    'msmarco-v2.1-doc': [
+      'msmarco-v2.1-doc',
+      'msmarco-v2.1-doc-segmented',
+    ],
+  });
+
+  // Generate collections for BEIR
+  useEffect(() => {
+    const keys = [
+      'beir-v1.0.0-trec-covid',
+      'beir-v1.0.0-bioasq',
+      'beir-v1.0.0-nfcorpus',
+      'beir-v1.0.0-nq',
+      'beir-v1.0.0-hotpotqa',
+      'beir-v1.0.0-fiqa',
+      'beir-v1.0.0-signal1m',
+      'beir-v1.0.0-trec-news',
+      'beir-v1.0.0-robust04',
+      'beir-v1.0.0-arguana',
+      'beir-v1.0.0-webis-touche2020',
+      'beir-v1.0.0-cqadupstack-android',
+      'beir-v1.0.0-cqadupstack-english',
+      'beir-v1.0.0-cqadupstack-gaming',
+      'beir-v1.0.0-cqadupstack-gis',
+      'beir-v1.0.0-cqadupstack-mathematica',
+      'beir-v1.0.0-cqadupstack-physics',
+      'beir-v1.0.0-cqadupstack-programmers',
+      'beir-v1.0.0-cqadupstack-stats',
+      'beir-v1.0.0-cqadupstack-tex',
+      'beir-v1.0.0-cqadupstack-unix',
+      'beir-v1.0.0-cqadupstack-webmasters',
+      'beir-v1.0.0-cqadupstack-wordpress',
+      'beir-v1.0.0-quora',
+      'beir-v1.0.0-dbpedia-entity',
+      'beir-v1.0.0-scidocs',
+      'beir-v1.0.0-fever',
+      'beir-v1.0.0-climate-fever',
+      'beir-v1.0.0-scifact',
+    ];
+    const generatedMap: {[key: string]: string[]} = collections;
+    keys.forEach(key => {
+      generatedMap[key] = [
+        `${key}.flat`,
+        `${key}.multifield`,
+        `${key}.splade-pp-ed`,
+        `${key}.bge-base-en-v1.5`,
+      ];
+    });
+    setCollections(generatedMap);
+  }, []);
 
   const fetchResults = async (query: string, collection: string) => {
     setLoading(true);
@@ -26,6 +96,14 @@ const SearchBar: React.FC = () => {
     }
   };
 
+  const handleGroupChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setGroup(e.target.value);
+  }
+
+  const handleCollectionChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setCollection(e.target.value);
+  };
+
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     fetchResults(query, collection);
@@ -35,24 +113,28 @@ const SearchBar: React.FC = () => {
     <>
     <div className="search-container">
       <form className="search-bar" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={query}
-          placeholder="Search..."
-          className="search-input"
-          onChange={(e) => setQuery(e.target.value)}
-        />
-        <select
-          value={collection}
-          onChange={(e) => setCollection(e.target.value)}
-          className="collection-dropdown"
-        >
-          <option value="msmarco-v1-passage" selected>msmarco-v1-passage</option>
-          <option value="msmarco-v2-passage">msmarco-v2-passage</option>
-          <option value="msmarco-v2.1-doc">msmarco-v2.1-doc</option>
-          <option value="beir-v1.0.0-nfcorpus.flat">beir-v1.0.0-nfcorpus.flat</option>
-        </select>
-        <button className="search-button" type="submit" disabled={loading}>Search</button>
+        <div className="dropdown-container">
+          <select onChange={handleGroupChange} className="group-dropdown">
+            {Object.keys(collections).map((group) => (
+              <option key={group} value={group}>{group}</option>
+            ))}
+          </select>
+          <select onChange={handleCollectionChange} className="collection-dropdown">
+            {collections[group]?.map((collection) => (
+              <option key={collection} value={collection}>{collection}</option>
+            ))}
+          </select>
+        </div>
+        <div className="searchbar-container">
+          <input
+            type="text"
+            value={query}
+            placeholder="Search..."
+            className="search-input"
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <button className="search-button" type="submit" disabled={loading}>Search</button>
+        </div>
       </form>
     </div>
     {loading && <p>Loading...</p>}
