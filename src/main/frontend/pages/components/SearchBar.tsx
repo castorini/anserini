66d656768fadada4e2d18c8048d1dbf68b5a00c9
Eric Zhang <31218485+16BitNarwhal@users.noreply.github.com>
2024-06-18 23:04:18 -0400

    Webapp update (#2525)
    * expose a /indexes route for listing all info
    * use indexinfo from api in frontend dropdown
    * /status route for checking index cached
    * use cached status to highlight indexes in frontend
    * renamed /index/{index} to /indexes/{index}
    * versioned with /api/v1.0/...
    * add docs/rest-api.md

--- src/main/frontend/pages/components/SearchBar.tsx
+++ src/main/frontend/pages/components/SearchBar.tsx
@@ -26,12 +26,13 @@ const SearchBar: React.FC = () => {
   const fetchResults = async (query: string, index: string) => {
     setLoading(true);
     try {
-      let endpoint = '/api';
-      if (index != '') endpoint += `/index/${index}`;
+      let endpoint = '/api/v1.0';
+      if (index != '') endpoint += `/indexes/${index}`;
       endpoint += `/search?query=${query}`;
       
       const response = await fetch(endpoint);
       const data = await response.json();
+      console.log(data);
       setResults(data.candidates);
     } catch (error) {
       console.error("Failed to fetch data: ", error);
@@ -70,7 +71,7 @@ const SearchBar: React.FC = () => {
           <h3>Document ID: {result.docid} <span>Score: {result.score}</span></h3>
           {Object.entries(result.doc).map(([key, value]) => (
             <p key={key}>
-              <strong>{key}:</strong> {value as React.ReactNode}
+              <strong>{key}:</strong> {JSON.stringify(value) as React.ReactNode}
             </p>
           ))}
         </div>
