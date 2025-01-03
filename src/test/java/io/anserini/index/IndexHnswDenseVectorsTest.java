2d95271f8cfc223617b9fab28aed81eb42223e54
Brayden Zhong <b8zhong@uwaterloo.ca>
2025-01-03 11:03:34 -0500

    Add dynamic type support (float/double) for ParquetDenseVectorDocumentGenerator (#2667)

--- src/test/java/io/anserini/index/IndexHnswDenseVectorsTest.java
+++ src/test/java/io/anserini/index/IndexHnswDenseVectorsTest.java
@@ -177,6 +177,28 @@ public class IndexHnswDenseVectorsTest {
   }
 
   @Test
+  public void testParquetFloat() throws Exception {
+    String indexPath = "target/lucene-test-index.flat." + System.currentTimeMillis();
+    String[] indexArgs = new String[] {
+        "-collection", "ParquetDenseVectorCollection",
+        "-input", "src/test/resources/sample_docs/parquet/msmarco-passage-bge-base-en-v1.5.parquet-float",
+        "-index", indexPath,
+        "-generator", "ParquetDenseVectorDocumentGenerator",
+        "-threads", "1",
+        "-M", "16", "-efC", "100"
+    };
+
+    IndexHnswDenseVectors.main(indexArgs);
+
+    IndexReader reader = IndexReaderUtils.getReader(indexPath);
+    assertNotNull(reader);
+
+    Map<String, Object> results = IndexReaderUtils.getIndexStats(reader, Constants.VECTOR);
+    assertNotNull(results);
+    assertEquals(10, results.get("documents"));
+  }
+
+  @Test
   public void testQuantizedInt8() throws Exception {
     String indexPath = "target/lucene-test-index.hnsw." + System.currentTimeMillis();
     String[] indexArgs = new String[] {
