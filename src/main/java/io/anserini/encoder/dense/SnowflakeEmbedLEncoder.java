0b67883621f6b08cf4afc6bd6aafb388485d169d
Brayden Zhong <b8zhong@uwaterloo.ca>
2025-01-25 18:11:42 -0500

    feat: SnowflakeEmbedL boilerplate

---
+++ src/main/java/io/anserini/encoder/dense/SnowflakeEmbedLEncoder.java
@@ -0,0 +1,54 @@
+/*
+ * Anserini: A Lucene toolkit for reproducible information retrieval research
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ package io.anserini.encoder.dense;
+
+ import java.io.IOException;
+ import java.net.URISyntaxException;
+ import java.util.ArrayList;
+ import java.util.HashMap;
+ import java.util.List;
+ import java.util.Map;
+ 
+ import ai.onnxruntime.OnnxTensor;
+ import ai.onnxruntime.OrtException;
+ import ai.onnxruntime.OrtSession;
+
+public class SnowflakeEmbedLEncoder extends DenseEncoder {
+    static private final String MODEL_URL = "https://rgw.cs.uwaterloo.ca/pyserini/data/snowflake-embed-l-optimized.onnx";
+
+  static private final String VOCAB_URL = "https://rgw.cs.uwaterloo.ca/pyserini/data/snowflake-embed-l-vocab.txt";
+
+  static private final String MODEL_NAME = "snowflake-embed-l-optimized.onnx";
+
+  static private final String VOCAB_NAME = "snowflake-embed-l-vocab.txt";
+
+  static private final String INSTRUCTION = "Represent this sentence for searching relevant passages: ";
+
+  static private final int MAX_SEQ_LEN = 512;
+
+  public SnowflakeEmbedLEncoder() throws IOException, OrtException, URISyntaxException {
+    super(MODEL_NAME, MODEL_URL, VOCAB_NAME, VOCAB_URL);
+  }
+
+  @Override
+  public float[] encode(String query) throws OrtException {
+    List<String> queryTokens = new ArrayList<>();
+    queryTokens.add("[CLS]");
+    queryTokens.addAll(this.tokenizer.tokenize(INSTRUCTION + query));
+    queryTokens.add("[SEP]");
+  }
+}
