3922dbe845100be0a40d60311e9c58284890c074
vincent-4 <vincentzhongy+githubvincent4@gmail.com>
2025-01-29 11:23:08 -0500

    Undo change to testNumPrebuiltIndexes - 169 as it was before

--- src/test/java/io/anserini/index/PrebuiltIndexTest.java
+++ src/test/java/io/anserini/index/PrebuiltIndexTest.java
@@ -60,6 +60,6 @@ public class PrebuiltIndexTest {
   // test number of prebuilt-indexes
   @Test
   public void testNumPrebuiltIndexes() {
-    assertEquals(166, IndexInfo.values().length);
+    assertEquals(169, IndexInfo.values().length);
   }
 }
