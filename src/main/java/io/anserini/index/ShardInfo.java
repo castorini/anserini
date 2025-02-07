019144c14789aebdc6d2f01f2e5ecab593c61c57
vincent-4 <vincentzhongy+githubvincent4@gmail.com>
2025-02-06 19:00:20 -0500

    feat: reorganize shards and implement parallel search

---
+++ src/main/java/io/anserini/index/ShardInfo.java
@@ -0,0 +1,242 @@
+package io.anserini.index;
+
+public enum ShardInfo {
+  MSMARCO_V21_DOC_SEGMENTED_SHARD00_ARCTIC_EMBED_L_HNSW_INT8("msmarco-v2.1-doc-segmented-shard00.arctic-embed-l.hnsw-int8",
+      "Lucene quantized (int8) HNSW index of the MS MARCO V2.1 segmented document corpus (shard00) encoded by Snowflake's arctic-embed-l model.",
+      "lucene-hnsw-int8.msmarco-v2.1-doc-segmented-shard00.arctic-embed-l.20250114.4884f5.tar.gz",
+      "lucene-hnsw-int8.msmarco-v2.1-doc-segmented.arctic-embed-l.20250114.4884f5.README.md",
+      "MS MARCO V2.1 Segmented Doc (shard00)",
+      "Snowflake's arctic-embed-l w/ HNSW int8",
+      new String[] {
+          "https://rgw.cs.uwaterloo.ca/pyserini/indexes/lucene/lucene-hnsw-int8.msmarco-v2.1-doc-segmented-shard00.arctic-embed-l.20250114.4884f5.tar.gz" },
+      "aab3f8e9aa0563bd0f875584784a0845",
+      IndexInfo.IndexType.DENSE_HNSW,
+      "ArcticEmbedLEncoder.class",
+      "JsonInvertedDenseVectorQueryGenerator.class",
+      "msmarco-v2.1-doc-segmented",
+      0),
+
+  MSMARCO_V21_DOC_SEGMENTED_SHARD01_ARCTIC_EMBED_L_HNSW_INT8("msmarco-v2.1-doc-segmented-shard01.arctic-embed-l.hnsw-int8",
+      "Lucene quantized (int8) HNSW index of the MS MARCO V2.1 segmented document corpus (shard01) encoded by Snowflake's arctic-embed-l model.",
+      "lucene-hnsw-int8.msmarco-v2.1-doc-segmented-shard01.arctic-embed-l.20250114.4884f5.tar.gz",
+      "lucene-hnsw-int8.msmarco-v2.1-doc-segmented.arctic-embed-l.20250114.4884f5.README.md",
+      "MS MARCO V2.1 Segmented Doc (shard01)",
+      "Snowflake's arctic-embed-l w/ HNSW int8",
+      new String[] {
+          "https://rgw.cs.uwaterloo.ca/pyserini/indexes/lucene/lucene-hnsw-int8.msmarco-v2.1-doc-segmented-shard01.arctic-embed-l.20250114.4884f5.tar.gz" },
+      "34ea30fe72c2bc1795ae83e71b191547",
+      IndexInfo.IndexType.DENSE_HNSW,
+      "ArcticEmbedLEncoder.class",
+      "JsonInvertedDenseVectorQueryGenerator.class",
+      "msmarco-v2.1-doc-segmented",
+      1),
+
+  MSMARCO_V21_DOC_SEGMENTED_SHARD02_ARCTIC_EMBED_L_HNSW_INT8("msmarco-v2.1-doc-segmented-shard02.arctic-embed-l.hnsw-int8",
+      "Lucene quantized (int8) HNSW index of the MS MARCO V2.1 segmented document corpus (shard02) encoded by Snowflake's arctic-embed-l model.",
+      "lucene-hnsw-int8.msmarco-v2.1-doc-segmented-shard02.arctic-embed-l.20250114.4884f5.tar.gz",
+      "lucene-hnsw-int8.msmarco-v2.1-doc-segmented.arctic-embed-l.20250114.4884f5.README.md",
+      "MS MARCO V2.1 Segmented Doc (shard02)",
+      "Snowflake's arctic-embed-l w/ HNSW int8",
+      new String[] {
+          "https://rgw.cs.uwaterloo.ca/pyserini/indexes/lucene/lucene-hnsw-int8.msmarco-v2.1-doc-segmented-shard02.arctic-embed-l.20250114.4884f5.tar.gz" },
+      "b6271d6db65119977491675f74f466d5",
+      IndexInfo.IndexType.DENSE_HNSW,
+      "ArcticEmbedLEncoder.class",
+      "JsonInvertedDenseVectorQueryGenerator.class",
+      "msmarco-v2.1-doc-segmented",
+      2),
+
+  MSMARCO_V21_DOC_SEGMENTED_SHARD03_ARCTIC_EMBED_L_HNSW_INT8("msmarco-v2.1-doc-segmented-shard03.arctic-embed-l.hnsw-int8",
+      "Lucene quantized (int8) HNSW index of the MS MARCO V2.1 segmented document corpus (shard03) encoded by Snowflake's arctic-embed-l model.",
+      "lucene-hnsw-int8.msmarco-v2.1-doc-segmented-shard03.arctic-embed-l.20250114.4884f5.tar.gz",
+      "lucene-hnsw-int8.msmarco-v2.1-doc-segmented.arctic-embed-l.20250114.4884f5.README.md",
+      "MS MARCO V2.1 Segmented Doc (shard03)",
+      "Snowflake's arctic-embed-l w/ HNSW int8",
+      new String[] {
+          "https://rgw.cs.uwaterloo.ca/pyserini/indexes/lucene/lucene-hnsw-int8.msmarco-v2.1-doc-segmented-shard03.arctic-embed-l.20250114.4884f5.tar.gz" },
+      "a9cd644eb6037f67d2e9c06a8f60928d",
+      IndexInfo.IndexType.DENSE_HNSW,
+      "ArcticEmbedLEncoder.class",
+      "JsonInvertedDenseVectorQueryGenerator.class",
+      "msmarco-v2.1-doc-segmented",
+      3),
+
+  MSMARCO_V21_DOC_SEGMENTED_SHARD04_ARCTIC_EMBED_L_HNSW_INT8("msmarco-v2.1-doc-segmented-shard04.arctic-embed-l.hnsw-int8",
+      "Lucene quantized (int8) HNSW index of the MS MARCO V2.1 segmented document corpus (shard04) encoded by Snowflake's arctic-embed-l model.",
+      "lucene-hnsw-int8.msmarco-v2.1-doc-segmented-shard04.arctic-embed-l.20250114.4884f5.tar.gz",
+      "lucene-hnsw-int8.msmarco-v2.1-doc-segmented.arctic-embed-l.20250114.4884f5.README.md",
+      "MS MARCO V2.1 Segmented Doc (shard04)",
+      "Snowflake's arctic-embed-l w/ HNSW int8",
+      new String[] {
+          "https://rgw.cs.uwaterloo.ca/pyserini/indexes/lucene/lucene-hnsw-int8.msmarco-v2.1-doc-segmented-shard04.arctic-embed-l.20250114.4884f5.tar.gz" },
+      "07b7e451e0525d01c1f1f2b1c42b1bd5",
+      IndexInfo.IndexType.DENSE_HNSW,
+      "ArcticEmbedLEncoder.class",
+      "JsonInvertedDenseVectorQueryGenerator.class",
+      "msmarco-v2.1-doc-segmented",
+      4),
+
+  MSMARCO_V21_DOC_SEGMENTED_SHARD05_ARCTIC_EMBED_L_HNSW_INT8("msmarco-v2.1-doc-segmented-shard05.arctic-embed-l.hnsw-int8",
+      "Lucene quantized (int8) HNSW index of the MS MARCO V2.1 segmented document corpus (shard05) encoded by Snowflake's arctic-embed-l model.",
+      "lucene-hnsw-int8.msmarco-v2.1-doc-segmented-shard05.arctic-embed-l.20250114.4884f5.tar.gz",
+      "lucene-hnsw-int8.msmarco-v2.1-doc-segmented.arctic-embed-l.20250114.4884f5.README.md",
+      "MS MARCO V2.1 Segmented Doc (shard05)",
+      "Snowflake's arctic-embed-l w/ HNSW int8",
+      new String[] {
+          "https://rgw.cs.uwaterloo.ca/pyserini/indexes/lucene/lucene-hnsw-int8.msmarco-v2.1-doc-segmented-shard05.arctic-embed-l.20250114.4884f5.tar.gz" },
+      "2573dce175788981be2f266ebb33c96d",
+      IndexInfo.IndexType.DENSE_HNSW,
+      "ArcticEmbedLEncoder.class",
+      "JsonInvertedDenseVectorQueryGenerator.class",
+      "msmarco-v2.1-doc-segmented",
+      5),
+
+  MSMARCO_V21_DOC_SEGMENTED_SHARD06_ARCTIC_EMBED_L_HNSW_INT8("msmarco-v2.1-doc-segmented-shard06.arctic-embed-l.hnsw-int8",
+      "Lucene quantized (int8) HNSW index of the MS MARCO V2.1 segmented document corpus (shard06) encoded by Snowflake's arctic-embed-l model.",
+      "lucene-hnsw-int8.msmarco-v2.1-doc-segmented-shard06.arctic-embed-l.20250114.4884f5.tar.gz",
+      "lucene-hnsw-int8.msmarco-v2.1-doc-segmented.arctic-embed-l.20250114.4884f5.README.md",
+      "MS MARCO V2.1 Segmented Doc (shard06)",
+      "Snowflake's arctic-embed-l w/ HNSW int8",
+      new String[] {
+          "https://rgw.cs.uwaterloo.ca/pyserini/indexes/lucene/lucene-hnsw-int8.msmarco-v2.1-doc-segmented-shard06.arctic-embed-l.20250114.4884f5.tar.gz" },
+      "a644aea445a8b78cc9e99d2ce111ff11",
+      IndexInfo.IndexType.DENSE_HNSW,
+      "ArcticEmbedLEncoder.class",
+      "JsonInvertedDenseVectorQueryGenerator.class",
+      "msmarco-v2.1-doc-segmented",
+      6),
+
+  MSMARCO_V21_DOC_SEGMENTED_SHARD07_ARCTIC_EMBED_L_HNSW_INT8("msmarco-v2.1-doc-segmented-shard07.arctic-embed-l.hnsw-int8",
+      "Lucene quantized (int8) HNSW index of the MS MARCO V2.1 segmented document corpus (shard07) encoded by Snowflake's arctic-embed-l model.",
+      "lucene-hnsw-int8.msmarco-v2.1-doc-segmented-shard07.arctic-embed-l.20250114.4884f5.tar.gz",
+      "lucene-hnsw-int8.msmarco-v2.1-doc-segmented.arctic-embed-l.20250114.4884f5.README.md",
+      "MS MARCO V2.1 Segmented Doc (shard07)",
+      "Snowflake's arctic-embed-l w/ HNSW int8",
+      new String[] {
+          "https://rgw.cs.uwaterloo.ca/pyserini/indexes/lucene/lucene-hnsw-int8.msmarco-v2.1-doc-segmented-shard07.arctic-embed-l.20250114.4884f5.tar.gz" },
+      "402d37deccb44b5fc105049889e8aaea",
+      IndexInfo.IndexType.DENSE_HNSW,
+      "ArcticEmbedLEncoder.class",
+      "JsonInvertedDenseVectorQueryGenerator.class",
+      "msmarco-v2.1-doc-segmented",
+      7),
+
+  MSMARCO_V21_DOC_SEGMENTED_SHARD08_ARCTIC_EMBED_L_HNSW_INT8("msmarco-v2.1-doc-segmented-shard08.arctic-embed-l.hnsw-int8",
+      "Lucene quantized (int8) HNSW index of the MS MARCO V2.1 segmented document corpus (shard08) encoded by Snowflake's arctic-embed-l model.",
+      "lucene-hnsw-int8.msmarco-v2.1-doc-segmented-shard08.arctic-embed-l.20250114.4884f5.tar.gz",
+      "lucene-hnsw-int8.msmarco-v2.1-doc-segmented.arctic-embed-l.20250114.4884f5.README.md",
+      "MS MARCO V2.1 Segmented Doc (shard08)",
+      "Snowflake's arctic-embed-l w/ HNSW int8",
+      new String[] {
+          "https://rgw.cs.uwaterloo.ca/pyserini/indexes/lucene/lucene-hnsw-int8.msmarco-v2.1-doc-segmented-shard08.arctic-embed-l.20250114.4884f5.tar.gz" },
+      "89ebcd027f7297b26a1edc8ae5726527",
+      IndexInfo.IndexType.DENSE_HNSW,
+      "ArcticEmbedLEncoder.class",
+      "JsonInvertedDenseVectorQueryGenerator.class",
+      "msmarco-v2.1-doc-segmented",
+      8),
+
+  MSMARCO_V21_DOC_SEGMENTED_SHARD09_ARCTIC_EMBED_L_HNSW_INT8(
+      "msmarco-v2.1-doc-segmented-shard09.arctic-embed-l.hnsw-int8",
+      "Lucene quantized (int8) HNSW index of the MS MARCO V2.1 segmented document corpus (shard09) encoded by Snowflake's arctic-embed-l model.",
+      "lucene-hnsw-int8.msmarco-v2.1-doc-segmented-shard09.arctic-embed-l.20250114.4884f5.tar.gz",
+      "lucene-hnsw-int8.msmarco-v2.1-doc-segmented.arctic-embed-l.20250114.4884f5.README.md",
+      "MS MARCO V2.1 Segmented Doc (shard09)",
+      "Snowflake's arctic-embed-l w/ HNSW int8",
+      new String[] {
+          "https://rgw.cs.uwaterloo.ca/pyserini/indexes/lucene/lucene-hnsw-int8.msmarco-v2.1-doc-segmented-shard09.arctic-embed-l.20250114.4884f5.tar.gz" },
+      "5e580bb7eb9ee2bb6bfa492b3430c17d",
+      IndexInfo.IndexType.DENSE_HNSW,
+      "ArcticEmbedLEncoder.class",
+      "JsonInvertedDenseVectorQueryGenerator.class",
+      "msmarco-v2.1-doc-segmented",
+      9);
+
+  public final String indexName;
+  public final String description;
+  public final String filename;
+  public final String readme;
+  public final String corpus;
+  public final String model;
+  public final String[] urls;
+  public final String md5;
+  public final IndexInfo.IndexType indexType;
+  public final String encoder;
+  public final String queryGenerator;
+  public final String invertedIndex;
+  public final int shardId;
+
+  ShardInfo(String indexName, String description, String filename, String readme, String corpus, String model,
+           String[] urls, String md5, IndexInfo.IndexType indexType, String encoder, String queryGenerator,
+           String invertedIndex, int shardId) {
+    this.indexName = indexName;
+    this.description = description;
+    this.filename = filename;
+    this.readme = readme;
+    this.corpus = corpus;
+    this.model = model;
+    this.urls = urls;
+    this.md5 = md5;
+    this.indexType = indexType;
+    this.encoder = encoder;
+    this.queryGenerator = queryGenerator;
+    this.invertedIndex = invertedIndex;
+    this.shardId = shardId;
+  }
+
+  public enum ShardedCollection {
+    MSMARCO_V21_DOC_SEGMENTED_ARCTIC_EMBED_L_HNSW_INT8(
+      "msmarco-v2.1-doc-segmented.arctic-embed-l.hnsw-int8",
+      new ShardInfo[] {
+        MSMARCO_V21_DOC_SEGMENTED_SHARD00_ARCTIC_EMBED_L_HNSW_INT8,
+        MSMARCO_V21_DOC_SEGMENTED_SHARD01_ARCTIC_EMBED_L_HNSW_INT8,
+        MSMARCO_V21_DOC_SEGMENTED_SHARD02_ARCTIC_EMBED_L_HNSW_INT8,
+        MSMARCO_V21_DOC_SEGMENTED_SHARD03_ARCTIC_EMBED_L_HNSW_INT8,
+        MSMARCO_V21_DOC_SEGMENTED_SHARD04_ARCTIC_EMBED_L_HNSW_INT8,
+        MSMARCO_V21_DOC_SEGMENTED_SHARD05_ARCTIC_EMBED_L_HNSW_INT8,
+        MSMARCO_V21_DOC_SEGMENTED_SHARD06_ARCTIC_EMBED_L_HNSW_INT8,
+        MSMARCO_V21_DOC_SEGMENTED_SHARD07_ARCTIC_EMBED_L_HNSW_INT8,
+        MSMARCO_V21_DOC_SEGMENTED_SHARD08_ARCTIC_EMBED_L_HNSW_INT8,
+        MSMARCO_V21_DOC_SEGMENTED_SHARD09_ARCTIC_EMBED_L_HNSW_INT8
+    });
+
+    private final String identifier;
+    private final ShardInfo[] shards;
+
+    ShardedCollection(String identifier, ShardInfo[] shards) {
+      this.identifier = identifier;
+      this.shards = shards;
+    }
+  }
+
+  public static boolean hasIndex(String indexName) {
+    for (ShardedCollection collection : ShardedCollection.values()) {
+      for (ShardInfo shard : collection.shards) {
+        if (shard.indexName.equals(indexName)) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  public static ShardInfo getIndex(String indexName) {
+    for (ShardedCollection collection : ShardedCollection.values()) {
+      for (ShardInfo shard : collection.shards) {
+        if (shard.indexName.equals(indexName)) {
+          return shard;
+        }
+      }
+    }
+    throw new IllegalArgumentException("Shard index name: " + indexName + " not found!");
+  }
+
+  public static ShardInfo[] getShardedIndex(String identifier) {
+    for (ShardedCollection collection : ShardedCollection.values()) {
+      if (collection.identifier.equals(identifier)) {
+        return collection.shards;
+      }
+    }
+    throw new IllegalArgumentException("No collection found for identifier: " + identifier);
+  }
+}
---
+++ src/main/java/io/anserini/search/SearchShardedHnswDenseVectors.java
@@ -0,0 +1,237 @@
+/*
+ * Anserini: A Lucene toolkit for reproducible information retrieval research
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.anserini.search;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import io.anserini.index.ShardInfo;
+import io.anserini.search.topicreader.TopicReader;
+import io.anserini.search.topicreader.Topics;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.ParserProperties;
+import org.kohsuke.args4j.spi.StringArrayOptionHandler;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.stream.IntStream;
+
+/**
+ * Main entry point for sharded HNSW dense vector search.
+ */
+public final class SearchShardedHnswDenseVectors<K extends Comparable<K>> implements Runnable, Closeable {
+  private static final Logger LOG = LogManager.getLogger(SearchShardedHnswDenseVectors.class);
+
+  public static class Args extends HnswDenseSearcher.Args {
+    @Option(name = "-topics", metaVar = "[file]", handler = StringArrayOptionHandler.class, required = true, usage = "topics file")
+    public String[] topics;
+
+    @Option(name = "-output", metaVar = "[file]", required = true, usage = "output file")
+    public String output;
+
+    @Option(name = "-topicReader", usage = "TopicReader to use.")
+    public String topicReader = "JsonIntVector";
+
+    @Option(name = "-topicField", usage = "Topic field that should be used as the query.")
+    public String topicField = "vector";
+
+    @Option(name = "-hits", metaVar = "[number]", usage = "max number of hits to return")
+    public int hits = 1000;
+
+    @Option(name = "-runtag", metaVar = "[tag]", usage = "runtag")
+    public String runtag = "Anserini";
+
+    @Option(name = "-format", metaVar = "[output format]", usage = "Output format, default \"trec\", alternative \"msmarco\".")
+    public String format = "trec";
+
+    @Option(name = "-options", usage = "Print information about options.")
+    public Boolean options = false;
+  }
+
+  private final Args args;
+  private final List<HnswDenseSearcher<K>> searchers;
+  private final List<K> qids = new ArrayList<>();
+  private final List<String> queries = new ArrayList<>();
+  private final ShardInfo[] shards;
+  private final int threadsPerShard;
+
+  /*
+   * Constructor for sharded HNSW dense vector search.
+   * The caller must provide an identifier for the sharded index defined in ShardInfo.
+   * This identifier is used to retrieve the shards from ShardInfo.
+   */
+  public SearchShardedHnswDenseVectors(Args args) throws IOException {
+    this.args = args;
+    this.searchers = new ArrayList<>();
+    this.shards = ShardInfo.getShardedIndex(args.index);
+    this.threadsPerShard = Math.max(args.threads / shards.length, 1);
+
+    LOG.info("============ Initializing {} ============", this.getClass().getSimpleName());
+    LOG.info("Found {} shards for identifier: {}", shards.length, args.index);
+    LOG.info("Topics: {}", Arrays.toString(args.topics));
+    LOG.info("Query generator: {}", args.queryGenerator);
+    LOG.info("Encoder: {}", args.encoder);
+    LOG.info("Threads: {}", args.threads);
+    LOG.info("Threads per shard: {}", threadsPerShard);
+
+    // TODO: make this configurable
+    String indexPath = "/store/scratch/v4zhong/.cache/pyserini/indexes/";
+    
+    for (ShardInfo shard : shards) {
+      HnswDenseSearcher.Args searcherArgs = new HnswDenseSearcher.Args();
+      searcherArgs.index = indexPath + shard.indexName;
+      searcherArgs.encoder = args.encoder;
+      searcherArgs.queryGenerator = args.queryGenerator;
+      searcherArgs.efSearch = args.efSearch;
+      searcherArgs.threads = threadsPerShard;
+      searchers.add(new HnswDenseSearcher<>(searcherArgs));
+    }
+
+    // We might not be able to successfully read topics for a variety of reasons. Gather all possible
+    // exceptions together as an unchecked exception to make initialization and error reporting clearer.
+    SortedMap<K, Map<String, String>> topics = new TreeMap<>();
+    for (String topicsFile : args.topics) {
+      Path topicsFilePath = Paths.get(topicsFile);
+      if (!Files.exists(topicsFilePath) || !Files.isRegularFile(topicsFilePath) || !Files.isReadable(topicsFilePath)) {
+        Topics ref = Topics.getByName(topicsFile);
+        if (ref == null) {
+          throw new IllegalArgumentException(String.format("\"%s\" does not refer to valid topics.", topicsFilePath));
+        } else {
+          topics.putAll(TopicReader.getTopics(ref));
+        }
+      } else {
+        try {
+          @SuppressWarnings("unchecked")
+          TopicReader<K> tr = (TopicReader<K>) Class
+            .forName(String.format("io.anserini.search.topicreader.%sTopicReader", args.topicReader))
+            .getConstructor(Path.class).newInstance(topicsFilePath);
+          topics.putAll(tr.read());
+        } catch (Exception e) {
+          throw new IllegalArgumentException(String.format("Unable to load topic reader \"%s\".", args.topicReader));
+        }
+      }
+    }
+
+    // Now iterate through all the topics to pick out the right field with proper
+    // exception handling.
+    try {
+      topics.forEach((qid, topic) -> {
+        String query;
+        if ( args.encoder != null) {
+          query = topic.get("title");
+        } else {
+          query = topic.get(args.topicField);
+        }
+        assert query != null;
+        qids.add(qid);
+        queries.add(query);
+      });
+    } catch (AssertionError|Exception e) {
+      throw new IllegalArgumentException(String.format("Unable to read topic field \"%s\".", args.topicField));
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    LOG.info("Closing searchers...");
+    for (HnswDenseSearcher<K> searcher : searchers) {
+      try {
+        searcher.close();
+        LOG.info("Closed searcher: {}", searcher.getClass().getSimpleName());
+      } catch (IOException e) {
+        LOG.warn("Error closing searcher: {}", searcher.getClass().getSimpleName(), e);
+      }
+    }
+    LOG.info("All searchers closed.");
+  }
+
+  @Override
+  public void run() {
+    LOG.info("============ Running Sharded Search ============");
+
+    IntStream.range(0, searchers.size()).parallel().forEach(i -> {
+      HnswDenseSearcher<K> searcher = searchers.get(i);
+      String shardOutputPath = args.output.replaceFirst("\\.txt$", ".shard" + String.format("%02d", i) + ".txt");
+      LOG.info("Processing shard {} -> {}", i, shardOutputPath);
+
+      try {
+        SortedMap<K, ScoredDoc[]> shardResults = searcher.batch_search(queries, qids, args.hits, threadsPerShard);
+        try (RunOutputWriter<K> writer = new RunOutputWriter<>(shardOutputPath, args.format, args.runtag, null)) {
+          shardResults.forEach((qid, docs) -> {
+            try {
+              writer.writeTopic(qid, queries.get(qids.indexOf(qid)), docs);
+            } catch (JsonProcessingException e) {
+              throw new RuntimeException(e);
+            }
+          });
+        }
+        //Close searcher right after we use
+        searcher.close();
+
+        LOG.info("Closed searcher for shard {}", i);
+      } catch (IOException e) {
+        throw new RuntimeException(String.format("Error processing shard %d: %s", i, e.getMessage()), e);
+      }
+    });
+    LOG.info("All shards processed. Results written to individual shard files.");
+  }
+
+  public static void main(String[] args) throws Exception {
+    Args searchArgs = new Args();
+    CmdLineParser parser = new CmdLineParser(searchArgs, ParserProperties.defaults().withUsageWidth(120));
+
+    try {
+      parser.parseArgument(args);
+    } catch (CmdLineException e) {
+      if (searchArgs.options) {
+        System.err.printf("Options for %s:\n\n", SearchShardedHnswDenseVectors.class.getSimpleName());
+        parser.printUsage(System.err);
+
+        List<String> required = new ArrayList<>();
+        parser.getOptions().forEach((option) -> {
+          if (option.option.required()) {
+            required.add(option.option.toString());
+          }
+        });
+        System.err.printf("\nRequired options are %s\n", required);
+      } else {
+        System.err.printf("Error: %s. For help, use \"-options\" to print out information about options.\n",
+          e.getMessage());
+      }
+
+      return;
+    }
+    
+    try (SearchShardedHnswDenseVectors<?> searcher = new SearchShardedHnswDenseVectors<>(searchArgs)) {
+      searcher.run();
+    } catch (RuntimeException e) {
+      System.err.printf("Error: %s\n", e.getMessage());
+    }
+  }
+}
