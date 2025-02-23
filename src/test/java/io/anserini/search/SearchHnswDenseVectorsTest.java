0312bd4151d7be6b2a925037be82e502a72d7dcc
vincent-4 <vincentzhongy+githubvincent4@gmail.com>
2025-02-23 15:49:12 -0500

    different naming

--- src/test/java/io/anserini/search/SearchHnswDenseVectorsTest.java
+++ src/test/java/io/anserini/search/SearchHnswDenseVectorsTest.java
@@ -500,7 +500,7 @@ public class SearchHnswDenseVectorsTest {
   @Test
   @SuppressWarnings("ResultOfMethodCallIgnored")
   public void testBasicWithOnnxWithSuffix() throws Exception {
-    String indexPath = "target/idx-sample-hnsw" + System.currentTimeMillis();
+    String indexPath = "target/idx-sample-hnsw2" + System.currentTimeMillis();
     String[] indexArgs = new String[] {
         "-collection", "JsonDenseVectorCollection",
         "-input", "src/test/resources/sample_docs/cosdpr-distil/json_vector/",
@@ -541,7 +541,6 @@ public class SearchHnswDenseVectorsTest {
     });
 
     new File(runfile).delete();
-    new File(indexPath).delete();
   }
 
   @Test
