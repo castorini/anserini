7ae60a3bbf9ca71c7679f2e69ea3512ead3645bb
vincent-4 <vincentzhongy+githubvincent4@gmail.com>
2025-03-02 22:41:41 -0500

    add a test

--- src/test/java/io/anserini/index/PrebuiltIndexHandlerTest.java
+++ src/test/java/io/anserini/index/PrebuiltIndexHandlerTest.java
@@ -17,21 +17,60 @@
 package io.anserini.index;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import io.anserini.util.PrebuiltIndexHandler;
+import static org.junit.Assert.assertTrue;
 
 public class PrebuiltIndexHandlerTest {
   private PrebuiltIndexHandler handler;
+  private Path originalIndexPath;
+  private boolean usingTempHandler = false;
 
   @Test
   public void testHandler() throws Exception {
+    downloadAndDecompressIndex(handler);
+  }
+
+  @Before
+  public void setUp() throws Exception {
+    handler = new PrebuiltIndexHandler("cacm"); // we use a lightweight index for testing
+    handler.initialize();
+  }
+
+  @Test
+  public void testCustomCacheDirectory() throws Exception {
+    Path tempDir = Files.createTempDirectory("anserini-test-cache");
+    
+    if (handler.getIndexFolderPath() != null) {
+      originalIndexPath = handler.getIndexFolderPath();
+    }
+    
+    handler = new PrebuiltIndexHandler("cacm", tempDir.toString());
+    handler.initialize();
+    usingTempHandler = true;
+    
     try {
-      handler.download();
-      handler.decompressIndex();
+      downloadAndDecompressIndex(handler);
+      
+      String decompressedPath = handler.getIndexFolderPath().toString();
+      assertTrue(decompressedPath.startsWith(tempDir.toString()));
+      
+    } finally {
+      Files.deleteIfExists(tempDir);
+      usingTempHandler = false;
+    }
+  }
+  
+  private void downloadAndDecompressIndex(PrebuiltIndexHandler indexHandler) throws Exception {
+    try {
+      indexHandler.download();
+      indexHandler.decompressIndex();
     } catch (IOException e) {
       e.printStackTrace();
       throw new Exception("Failed to download index.", e);
@@ -41,17 +80,15 @@ public class PrebuiltIndexHandlerTest {
     }
   }
 
-  @Before
-  public void setUp() throws Exception {
-    handler = new PrebuiltIndexHandler("cacm"); // we use a lightweight index for testing
-    handler.initialize();
-  }
-
   @After
   public void tearDown() throws Exception {
-    // delete the index downloaded
+    // Delete the index downloaded
     if (handler.getIndexFolderPath() != null && handler.getIndexFolderPath().toFile().exists()) {
       handler.getIndexFolderPath().toFile().delete();
+    }
+    
+    if (usingTempHandler && originalIndexPath != null && originalIndexPath.toFile().exists()) {
+      originalIndexPath.toFile().delete();
     }
   }
 
