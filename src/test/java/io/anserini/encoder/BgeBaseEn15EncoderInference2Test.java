d59ae3022e5d8ec67b51f9876e8fb4c68de23510
Jimmy Lin <jimmylin@uwaterloo.ca>
2025-03-06 06:40:33 -0500

    Fix flaky tests (#2751)
    Seems like splitting a test into two fixes the Java CI error: "Error: The operation was canceled."
    Suspect it's an issue with a single test consuming too much resource.

---
+++ src/test/java/io/anserini/encoder/BgeBaseEn15EncoderInference2Test.java
@@ -0,0 +1,42 @@
+/*
+ * Anserini: A Lucene toolkit for reproducible information retrieval research
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.anserini.encoder;
+
+import ai.onnxruntime.OrtException;
+import io.anserini.encoder.dense.BgeBaseEn15Encoder;
+import io.anserini.encoder.dense.DenseEncoder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class BgeBaseEn15EncoderInference2Test {
+  // We're running into this issue on GitHub Java CI:
+  // > Error: The operation was canceled.
+  // Can't reproduce locally, but separating test cases into separate files seems to fix it...
+  @Test
+  public void testMaxLength() throws OrtException, IOException, URISyntaxException {
+    DenseEncoder encoder = new BgeBaseEn15Encoder();
+    float[] expectedWeights = (float[]) BgeBaseEn15EncoderInferenceTest.LONG_EXAMPLES[0][1];
+    String[] inputStrings = (String[]) BgeBaseEn15EncoderInferenceTest.LONG_EXAMPLES[0][0];
+
+    float[] outputs = encoder.encode(inputStrings[0]);
+    assertArrayEquals(expectedWeights, outputs, 1e-4f);
+  }
+}
