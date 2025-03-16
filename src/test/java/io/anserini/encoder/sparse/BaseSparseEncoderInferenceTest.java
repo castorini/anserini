132f6d27279efd10d5584589f5c2fb7695c9628d
lintool <jimmylin@uwaterloo.ca>
2025-03-16 12:17:57 -0400

    Refactoring

---
+++ src/test/java/io/anserini/encoder/sparse/BaseSparseEncoderInferenceTest.java
@@ -0,0 +1,30 @@
+package io.anserini.encoder.sparse;
+
+import ai.onnxruntime.OrtException;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+public class BaseSparseEncoderInferenceTest {
+  public void testExamples(SparseExampleOutputPair[] examples, SparseEncoder encoder) throws OrtException, IOException, URISyntaxException {
+    for (SparseExampleOutputPair pair: examples) {
+      Map<String, Integer> outputs = encoder.getEncodedQueryMap(pair.getExample());
+      Map<String, Integer> expectedMap = pair.getOutput();
+
+      //assertEquals(expectedMap.size(), outputs.size());
+      for (Map.Entry<String, Integer> entry : outputs.entrySet()) {
+        String key = entry.getKey();
+        Integer value = entry.getValue();
+        System.out.println(entry.getKey() + " " + entry.getValue());
+        Integer expectedValue = expectedMap.get(key);
+        //assertEquals(expectedValue, value);
+      }
+      System.out.println("----");
+    }
+  }
+
+}
