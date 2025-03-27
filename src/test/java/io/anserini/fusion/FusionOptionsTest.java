3d7bde780736b48374589d3c2d9631bb85bb7136
lilyjge <lilyjge@gmail.com>
2025-03-27 11:20:43 -0400

    Added license header and changed indenting

--- src/test/java/io/anserini/fusion/FusionOptionsTest.java
+++ src/test/java/io/anserini/fusion/FusionOptionsTest.java
@@ -1,3 +1,19 @@
+/*
+ * Anserini: A Lucene toolkit for reproducible information retrieval research
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.anserini.fusion;
 
 import static org.junit.Assert.assertEquals;
@@ -9,102 +25,102 @@ import java.io.PrintStream;
 import org.junit.Test;
 
 public class FusionOptionsTest {
-    private final ByteArrayOutputStream err = new ByteArrayOutputStream();
-    private PrintStream save;
+  private final ByteArrayOutputStream err = new ByteArrayOutputStream();
+  private PrintStream save;
 
-    private void redirectStderr() {
-        save = System.err;
-        err.reset();
-        System.setErr(new PrintStream(err));
-    }
+  private void redirectStderr() {
+    save = System.err;
+    err.reset();
+    System.setErr(new PrintStream(err));
+  }
 
-    private void restoreStderr() {
-        System.setErr(save);
-    }
+  private void restoreStderr() {
+    System.setErr(save);
+  }
 
-    @Test
-    public void testInvalidDepth() throws Exception {
-        redirectStderr();
+  @Test
+  public void testInvalidDepth() throws Exception {
+    redirectStderr();
 
-        String[] fuseArgs = new String[] {
-            "-runs", "/fake/path",
-            "-output", "runs/fused_run.test",
-            "-method", "normalize",
-            "-k", "1000",
-            "-depth", "0",
-            "-rrf_k", "60",
-            "-alpha", "0.5"};
-        FuseTrecRuns.main(fuseArgs);
+    String[] fuseArgs = new String[] {
+      "-runs", "/fake/path",
+      "-output", "runs/fused_run.test",
+      "-method", "normalize",
+      "-k", "1000",
+      "-depth", "0",
+      "-rrf_k", "60",
+      "-alpha", "0.5"};
+    FuseTrecRuns.main(fuseArgs);
 
-        assertEquals("Error: Option depth must be greater than 0. Please check the provided arguments. Use the \"-options\" flag to print out detailed information about available options and their usage.\n".trim(), err.toString().trim());
-        restoreStderr();
-    }
+    assertEquals("Error: Option depth must be greater than 0. Please check the provided arguments. Use the \"-options\" flag to print out detailed information about available options and their usage.\n".trim(), err.toString().trim());
+    restoreStderr();
+  }
 
-    @Test
-    public void testInvalidK() throws Exception {
-        redirectStderr();
+  @Test
+  public void testInvalidK() throws Exception {
+    redirectStderr();
 
-        String[] fuseArgs = new String[] {
-            "-runs", "/fake/path",
-            "-output", "runs/fused_run.test",
-            "-method", "normalize",
-            "-k", "0",
-            "-depth", "1000",
-            "-rrf_k", "60",
-            "-alpha", "0.5"};
-        FuseTrecRuns.main(fuseArgs);
+    String[] fuseArgs = new String[] {
+      "-runs", "/fake/path",
+      "-output", "runs/fused_run.test",
+      "-method", "normalize",
+      "-k", "0",
+      "-depth", "1000",
+      "-rrf_k", "60",
+      "-alpha", "0.5"};
+    FuseTrecRuns.main(fuseArgs);
 
-        assertEquals("Error: Option k must be greater than 0. Please check the provided arguments. Use the \"-options\" flag to print out detailed information about available options and their usage.\n".trim(), err.toString().trim());
-        restoreStderr();
-    }
+    assertEquals("Error: Option k must be greater than 0. Please check the provided arguments. Use the \"-options\" flag to print out detailed information about available options and their usage.\n".trim(), err.toString().trim());
+    restoreStderr();
+  }
 
-    @Test
-    public void testInvalidRuns() throws Exception {
-        redirectStderr();
+  @Test
+  public void testInvalidRuns() throws Exception {
+    redirectStderr();
 
-        String[] fuseArgs = new String[] {
-            "-runs", "/fake/path /fake/path2",
-            "-output", "runs/fused_run.test",
-            "-method", "normalize",
-            "-k", "1000",
-            "-depth", "1000",
-            "-rrf_k", "60",
-            "-alpha", "0.5"};
-        FuseTrecRuns.main(fuseArgs);
+    String[] fuseArgs = new String[] {
+      "-runs", "/fake/path /fake/path2",
+      "-output", "runs/fused_run.test",
+      "-method", "normalize",
+      "-k", "1000",
+      "-depth", "1000",
+      "-rrf_k", "60",
+      "-alpha", "0.5"};
+    FuseTrecRuns.main(fuseArgs);
 
-        assertEquals("Error: /fake/path (No such file or directory). Please check the provided arguments. Use the \"-options\" flag to print out detailed information about available options and their usage.\n".trim(), err.toString().trim());
-        restoreStderr();
-    }
+    assertEquals("Error: /fake/path (No such file or directory). Please check the provided arguments. Use the \"-options\" flag to print out detailed information about available options and their usage.\n".trim(), err.toString().trim());
+    restoreStderr();
+  }
 
-    @Test
-    public void testInvalidMethod() throws Exception {
-        redirectStderr();
+  @Test
+  public void testInvalidMethod() throws Exception {
+    redirectStderr();
 
-        String[] fuseArgs = new String[] {
-            "-runs", "src/test/resources/sample_runs/run1 src/test/resources/sample_runs/run2",
-            "-output", "runs/fused_run.test",
-            "-method", "add",
-            "-k", "1000",
-            "-depth", "1000",
-            "-rrf_k", "60",
-            "-alpha", "0.5"};
-        FuseTrecRuns.main(fuseArgs);
+    String[] fuseArgs = new String[] {
+      "-runs", "src/test/resources/sample_runs/run1 src/test/resources/sample_runs/run2",
+      "-output", "runs/fused_run.test",
+      "-method", "add",
+      "-k", "1000",
+      "-depth", "1000",
+      "-rrf_k", "60",
+      "-alpha", "0.5"};
+    FuseTrecRuns.main(fuseArgs);
 
-        assertEquals("Unknown fusion method: add. Supported methods are: average, rrf, interpolation.".trim(), err.toString().trim());
-        restoreStderr();
-    }
+    assertEquals("Unknown fusion method: add. Supported methods are: average, rrf, interpolation.".trim(), err.toString().trim());
+    restoreStderr();
+  }
 
-    @Test
-    public void testOptions() throws Exception {
-        redirectStderr();
+  @Test
+  public void testOptions() throws Exception {
+    redirectStderr();
 
-        String[] fuseArgs = new String[] {
-            "-runs", "/fake/path /fake/path2",
-            "-options"
-        };
-        FuseTrecRuns.main(fuseArgs);
+    String[] fuseArgs = new String[] {
+      "-runs", "/fake/path /fake/path2",
+      "-options"
+    };
+    FuseTrecRuns.main(fuseArgs);
 
-        assertTrue(err.toString().contains("Options for FuseTrecRuns:"));
-        restoreStderr();
-    }
+    assertTrue(err.toString().contains("Options for FuseTrecRuns:"));
+    restoreStderr();
+  }
 }
--- src/test/java/io/anserini/fusion/FusionTest.java
+++ src/test/java/io/anserini/fusion/FusionTest.java
@@ -1,3 +1,19 @@
+/*
+ * Anserini: A Lucene toolkit for reproducible information retrieval research
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.anserini.fusion;
 
 import static org.junit.Assert.assertTrue;
@@ -9,107 +25,107 @@ import org.junit.Test;
 import io.anserini.TestUtils;
 
 public class FusionTest {
-    @Test
-    public void testFuseInterpolation() throws Exception {
-        String[] fuseArgs = new String[] {
-            "-runs", "src/test/resources/sample_runs/run1 src/test/resources/sample_runs/run2",
-            "-output", "runs/fused_run_interp.test",
-            "-method", "interpolation",
-            "-k", "1000",
-            "-depth", "1000",
-            "-rrf_k", "60",
-            "-alpha", "0.5"};
-        FuseTrecRuns.main(fuseArgs);
-        
-        TestUtils.checkFile("runs/fused_run_interp.test", new String[]{
-            "query1 Q0 doc2 1 5.500000 anserini.fusion",
-            "query1 Q0 doc1 2 5.500000 anserini.fusion",
-            "query1 Q0 doc3 3 2.500000 anserini.fusion",
-            "query1 Q0 doc4 4 1.500000 anserini.fusion",
-            "query2 Q0 doc3 1 10.500000 anserini.fusion",
-            "query2 Q0 doc1 2 7.000000 anserini.fusion",
-            "query2 Q0 doc2 3 6.500000 anserini.fusion",
-            "query2 Q0 doc5 4 4.000000 anserini.fusion",
-            "query2 Q0 doc6 5 3.500000 anserini.fusion"
-        });
-        assertTrue(new File("runs/fused_run_interp.test").delete());
-    }
+  @Test
+  public void testFuseInterpolation() throws Exception {
+    String[] fuseArgs = new String[] {
+      "-runs", "src/test/resources/sample_runs/run1 src/test/resources/sample_runs/run2",
+      "-output", "runs/fused_run_interp.test",
+      "-method", "interpolation",
+      "-k", "1000",
+      "-depth", "1000",
+      "-rrf_k", "60",
+      "-alpha", "0.5"};
+    FuseTrecRuns.main(fuseArgs);
+    
+    TestUtils.checkFile("runs/fused_run_interp.test", new String[]{
+      "query1 Q0 doc2 1 5.500000 anserini.fusion",
+      "query1 Q0 doc1 2 5.500000 anserini.fusion",
+      "query1 Q0 doc3 3 2.500000 anserini.fusion",
+      "query1 Q0 doc4 4 1.500000 anserini.fusion",
+      "query2 Q0 doc3 1 10.500000 anserini.fusion",
+      "query2 Q0 doc1 2 7.000000 anserini.fusion",
+      "query2 Q0 doc2 3 6.500000 anserini.fusion",
+      "query2 Q0 doc5 4 4.000000 anserini.fusion",
+      "query2 Q0 doc6 5 3.500000 anserini.fusion"
+    });
+    assertTrue(new File("runs/fused_run_interp.test").delete());
+  }
 
-    @Test
-    public void testFuseAvg() throws Exception {
-        String[] fuseArgs = new String[] {
-            "-runs", "src/test/resources/sample_runs/run1 src/test/resources/sample_runs/run2",
-            "-output", "runs/fused_run_avg.test",
-            "-method", "average",
-            "-k", "1000",
-            "-depth", "1000",
-            "-rrf_k", "60",
-            "-alpha", "0.5"};
-        FuseTrecRuns.main(fuseArgs);
-        
-        TestUtils.checkFile("runs/fused_run_avg.test", new String[]{
-            "query1 Q0 doc2 1 5.500000 anserini.fusion",
-            "query1 Q0 doc1 2 5.500000 anserini.fusion",
-            "query1 Q0 doc3 3 2.500000 anserini.fusion",
-            "query1 Q0 doc4 4 1.500000 anserini.fusion",
-            "query2 Q0 doc3 1 10.500000 anserini.fusion",
-            "query2 Q0 doc1 2 7.000000 anserini.fusion",
-            "query2 Q0 doc2 3 6.500000 anserini.fusion",
-            "query2 Q0 doc5 4 4.000000 anserini.fusion",
-            "query2 Q0 doc6 5 3.500000 anserini.fusion"
-        });
-        assertTrue(new File("runs/fused_run_avg.test").delete());
-    }
+  @Test
+  public void testFuseAvg() throws Exception {
+    String[] fuseArgs = new String[] {
+      "-runs", "src/test/resources/sample_runs/run1 src/test/resources/sample_runs/run2",
+      "-output", "runs/fused_run_avg.test",
+      "-method", "average",
+      "-k", "1000",
+      "-depth", "1000",
+      "-rrf_k", "60",
+      "-alpha", "0.5"};
+    FuseTrecRuns.main(fuseArgs);
+    
+    TestUtils.checkFile("runs/fused_run_avg.test", new String[]{
+      "query1 Q0 doc2 1 5.500000 anserini.fusion",
+      "query1 Q0 doc1 2 5.500000 anserini.fusion",
+      "query1 Q0 doc3 3 2.500000 anserini.fusion",
+      "query1 Q0 doc4 4 1.500000 anserini.fusion",
+      "query2 Q0 doc3 1 10.500000 anserini.fusion",
+      "query2 Q0 doc1 2 7.000000 anserini.fusion",
+      "query2 Q0 doc2 3 6.500000 anserini.fusion",
+      "query2 Q0 doc5 4 4.000000 anserini.fusion",
+      "query2 Q0 doc6 5 3.500000 anserini.fusion"
+    });
+    assertTrue(new File("runs/fused_run_avg.test").delete());
+  }
 
-    @Test
-    public void testFuseRRF() throws Exception {
-        String[] fuseArgs = new String[] {
-            "-runs", "src/test/resources/sample_runs/run1 src/test/resources/sample_runs/run2",
-            "-output", "runs/fused_run_rrf.test",
-            "-method", "rrf",
-            "-k", "1000",
-            "-depth", "1000",
-            "-rrf_k", "60",
-            "-alpha", "0.5"};
-        FuseTrecRuns.main(fuseArgs);
-        
-        TestUtils.checkFile("runs/fused_run_rrf.test", new String[]{
-            "query1 Q0 doc2 1 0.032522 anserini.fusion",
-            "query1 Q0 doc1 2 0.032522 anserini.fusion",
-            "query1 Q0 doc4 3 0.015873 anserini.fusion",
-            "query1 Q0 doc3 4 0.015873 anserini.fusion",
-            "query2 Q0 doc3 1 0.032266 anserini.fusion",
-            "query2 Q0 doc1 2 0.016393 anserini.fusion",
-            "query2 Q0 doc5 3 0.016129 anserini.fusion",
-            "query2 Q0 doc2 4 0.016129 anserini.fusion",
-            "query2 Q0 doc6 5 0.015873 anserini.fusion"
-        });
-        assertTrue(new File("runs/fused_run_rrf.test").delete());
-    }
+  @Test
+  public void testFuseRRF() throws Exception {
+    String[] fuseArgs = new String[] {
+      "-runs", "src/test/resources/sample_runs/run1 src/test/resources/sample_runs/run2",
+      "-output", "runs/fused_run_rrf.test",
+      "-method", "rrf",
+      "-k", "1000",
+      "-depth", "1000",
+      "-rrf_k", "60",
+      "-alpha", "0.5"};
+    FuseTrecRuns.main(fuseArgs);
+    
+    TestUtils.checkFile("runs/fused_run_rrf.test", new String[]{
+      "query1 Q0 doc2 1 0.032522 anserini.fusion",
+      "query1 Q0 doc1 2 0.032522 anserini.fusion",
+      "query1 Q0 doc4 3 0.015873 anserini.fusion",
+      "query1 Q0 doc3 4 0.015873 anserini.fusion",
+      "query2 Q0 doc3 1 0.032266 anserini.fusion",
+      "query2 Q0 doc1 2 0.016393 anserini.fusion",
+      "query2 Q0 doc5 3 0.016129 anserini.fusion",
+      "query2 Q0 doc2 4 0.016129 anserini.fusion",
+      "query2 Q0 doc6 5 0.015873 anserini.fusion"
+    });
+    assertTrue(new File("runs/fused_run_rrf.test").delete());
+  }
 
-    @Test
-    public void testFuseNormalize() throws Exception {
-        String[] fuseArgs = new String[] {
-            "-runs", "src/test/resources/sample_runs/run1 src/test/resources/sample_runs/run2",
-            "-output", "runs/fused_run_norm.test",
-            "-method", "normalize",
-            "-k", "1000",
-            "-depth", "1000",
-            "-rrf_k", "60",
-            "-alpha", "0.5"};
-        FuseTrecRuns.main(fuseArgs);
-        
-        TestUtils.checkFile("runs/fused_run_norm.test", new String[]{
-            "query1 Q0 doc2 1 0.750000 anserini.fusion",
-            "query1 Q0 doc1 2 0.750000 anserini.fusion",
-            "query1 Q0 doc4 3 0.000000 anserini.fusion",
-            "query1 Q0 doc3 4 0.000000 anserini.fusion",
-            "query2 Q0 doc3 1 0.500000 anserini.fusion",
-            "query2 Q0 doc1 2 0.500000 anserini.fusion",
-            "query2 Q0 doc5 3 0.250000 anserini.fusion",
-            "query2 Q0 doc2 4 0.250000 anserini.fusion",
-            "query2 Q0 doc6 5 0.000000 anserini.fusion"
-        });
-        assertTrue(new File("runs/fused_run_norm.test").delete());
-    }
+  @Test
+  public void testFuseNormalize() throws Exception {
+    String[] fuseArgs = new String[] {
+      "-runs", "src/test/resources/sample_runs/run1 src/test/resources/sample_runs/run2",
+      "-output", "runs/fused_run_norm.test",
+      "-method", "normalize",
+      "-k", "1000",
+      "-depth", "1000",
+      "-rrf_k", "60",
+      "-alpha", "0.5"};
+    FuseTrecRuns.main(fuseArgs);
+    
+    TestUtils.checkFile("runs/fused_run_norm.test", new String[]{
+      "query1 Q0 doc2 1 0.750000 anserini.fusion",
+      "query1 Q0 doc1 2 0.750000 anserini.fusion",
+      "query1 Q0 doc4 3 0.000000 anserini.fusion",
+      "query1 Q0 doc3 4 0.000000 anserini.fusion",
+      "query2 Q0 doc3 1 0.500000 anserini.fusion",
+      "query2 Q0 doc1 2 0.500000 anserini.fusion",
+      "query2 Q0 doc5 3 0.250000 anserini.fusion",
+      "query2 Q0 doc2 4 0.250000 anserini.fusion",
+      "query2 Q0 doc6 5 0.000000 anserini.fusion"
+    });
+    assertTrue(new File("runs/fused_run_norm.test").delete());
+  }
 }
