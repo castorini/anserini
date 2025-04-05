158659ee2ba2b78c1ee28c585dc6604cff8591e9
vincent-4 <vincentzhongy+githubvincent4@gmail.com>
2025-04-05 16:54:38 -0400

    Remove unnecessary try-catch block; remove msgs

--- src/main/java/io/anserini/search/SearchShardedHnswDenseVectors.java
+++ src/main/java/io/anserini/search/SearchShardedHnswDenseVectors.java
@@ -136,13 +136,8 @@ public final class SearchShardedHnswDenseVectors<K extends Comparable<K>> implem
       shardOutputPaths.add(shardOutputPath);
       LOG.info("Processing shard {} -> {}", i, shardOutputPath);
 
-      try {
-        searcher.args.output = shardOutputPath;
-        searcher.run();
-        LOG.info("Closed searcher for shard {}", i);
-      } catch (RuntimeException e) {
-        throw new RuntimeException(String.format("Error processing shard %d: %s", i, e.getMessage()), e);
-      }
+      searcher.args.output = shardOutputPath;
+      searcher.run();
     });
 
     LOG.info("Concatenating shard results into {}", args.output);
