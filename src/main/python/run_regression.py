334bad6258950cba348dfd663310e1fd42a4eee2
Jimmy Lin <jimmylin@uwaterloo.ca>
2025-04-06 10:23:55 -0400

    Remove msmarco-v2-doc-segmented.unicoil-0shot regressions + related variants (#2770)

--- src/main/python/run_regression.py
+++ src/main/python/run_regression.py
@@ -50,6 +50,7 @@ CORPUS_ROOTS = [
     '/mnt/',                      # on tjena
     '/tuna1/',                    # on tuna
     '/store/',                    # on orca
+    '/scratch/',                  # on watgpu608s
     '/u4/jimmylin/',              # on linux.cs
     '/System/Volumes/Data/store'  # for new organization of directories in macOS Monterey
 ]
