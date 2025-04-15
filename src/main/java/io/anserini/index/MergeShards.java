eb53f0142d5d486d1fa36deb3c549bb4fd39d0dc
J Mackenzie <JMMackenzie@users.noreply.github.com>
2025-04-15 13:50:19 +1000

    Update MergeShards.java
    Haus stylin

--- src/main/java/io/anserini/index/MergeShards.java
+++ src/main/java/io/anserini/index/MergeShards.java
@@ -25,26 +25,27 @@ import java.nio.file.Paths;
 
 public class MergeShards {
 
-    public static void main(String[] args) throws Exception {
-        if (args.length < 2) {
-            System.err.println("Usage: MergeShards <output_dir> <shard_dir1> [<shard_dir2> ...]");
-            System.exit(1);
-        }
+public static void main(String[] args) throws Exception {
+  if (args.length < 2) {
+    System.err.println("Usage: MergeShards <output_dir> <shard_dir1> [<shard_dir2> ...]");
+    System.exit(1);
+  }
 
-        String outputDir = args[0];
-        FSDirectory mergedDir = FSDirectory.open(Paths.get(outputDir));
-        IndexWriterConfig config = new IndexWriterConfig(new StandardAnalyzer());
-        IndexWriter writer = new IndexWriter(mergedDir, config);
+  String outputDir = args[0];
+  FSDirectory mergedDir = FSDirectory.open(Paths.get(outputDir));
+  IndexWriterConfig config = new IndexWriterConfig(new StandardAnalyzer());
+  IndexWriter writer = new IndexWriter(mergedDir, config);
 
-        for (int i = 1; i < args.length; i++) {
-            System.out.println("Adding index: " + args[i]);
-            FSDirectory shardDir = FSDirectory.open(Paths.get(args[i]));
-            writer.addIndexes(shardDir);
-        }
+  for (int i = 1; i < args.length; i++) {
+    System.out.println("Adding index: " + args[i]);
+    FSDirectory shardDir = FSDirectory.open(Paths.get(args[i]));
+    writer.addIndexes(shardDir);
+  }
 
-        System.out.println("Merging...");
-        writer.forceMerge(1);
-        writer.close();
-        System.out.println("Done. Merged index at: " + outputDir);
-    }
+  System.out.println("Merging...");
+  writer.forceMerge(1);
+  writer.close();
+  System.out.println("Done. Merged index at: " + outputDir);
+  }
+    
 }
