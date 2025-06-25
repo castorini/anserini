d0cc84889c3b5895698a446491bbb977adfe051e
sisixili <113862606+sisixili@users.noreply.github.com>
2025-06-24 21:16:22 -0400

    rip out spring boot server and frontend (#2857)

--- src/main/frontend/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  reactStrictMode: true,
-  swcMinify: true,
-  images: { unoptimized: true }
-}
-
-module.exports = nextConfig
