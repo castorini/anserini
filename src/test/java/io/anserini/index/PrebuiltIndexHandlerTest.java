1e10c3cf87d58f4f8b8fd2d44330d2e07bbb74fb
lintool <jimmylin@uwaterloo.ca>
2025-11-22 12:57:41 -0500

    redirects.

--- src/test/java/io/anserini/index/PrebuiltIndexHandlerTest.java
+++ src/test/java/io/anserini/index/PrebuiltIndexHandlerTest.java
@@ -18,15 +18,66 @@ package io.anserini.index;
 
 import static org.junit.Assert.assertTrue;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.core.config.Configurator;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import io.anserini.util.PrebuiltIndexHandler;
 
 public class PrebuiltIndexHandlerTest {
+  // Note, cannot extend StdOutStdErrRedirectableLuceneTestCase due to concurrency issues.
+  // So, we have to duplicate code to save/restore stderr/stdout.
+
+  protected final ByteArrayOutputStream out = new ByteArrayOutputStream();
+  protected final ByteArrayOutputStream err = new ByteArrayOutputStream();
+  protected PrintStream saveOut;
+  protected PrintStream saveErr;
+
+  protected void redirectStdErr() {
+    saveErr = System.err;
+    err.reset();
+    System.setErr(new PrintStream(err));
+  }
+
+  protected void restoreStdErr() {
+    System.setErr(saveErr);
+  }
+
+  protected void redirectStdOut() {
+    saveOut = System.out;
+    out.reset();
+    System.setOut(new PrintStream(out));
+  }
+
+  protected void restoreStdOut() {
+    System.setOut(saveOut);
+  }
+
+  @BeforeClass
+  public static void setupClass() {
+    Configurator.setLevel(PrebuiltIndexHandler.class.getName(), Level.ERROR);
+  }
+
+  @Before
+  public void setUp() throws Exception {
+    redirectStdOut();
+    redirectStdErr();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    restoreStdOut();
+    restoreStdErr();
+  }
 
   @Test
   public void testDownload() throws Exception {
