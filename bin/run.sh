8ba7edf5ebd43a3cb9f912859f668d0f6af3a0a3
Muhammad Ali <m.ali8.aj8@gmail.com>
2025-12-19 00:27:36 +0500

    restore heap memory params to original level

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -cp `ls target/*-fatjar.jar` -Xms1G -Xmx32G --add-modules jdk.incubator.vector $@
+java -cp `ls target/*-fatjar.jar` -Xms512M -Xmx192G --add-modules jdk.incubator.vector $@
