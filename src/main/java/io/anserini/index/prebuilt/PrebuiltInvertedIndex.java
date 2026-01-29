5d5cee4cc43f4f00f12c41a66e5e985f408aa1c4
lintool <jimmylin@uwaterloo.ca>
2026-01-29 07:26:48 -0500

    Refactor.

--- src/main/java/io/anserini/index/prebuilt/PrebuiltInvertedIndex.java
+++ src/main/java/io/anserini/index/prebuilt/PrebuiltInvertedIndex.java
@@ -16,147 +16,26 @@
 
 package io.anserini.index.prebuilt;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.JarURLConnection;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.core.json.JsonReadFeature;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.json.JsonMapper;
 
-public class PrebuiltInvertedIndex {
-  private static final String RESOURCE_DIR = "prebuilt-indexes";
-  private static final String RESOURCE_SUFFIX = ".json";
+public class PrebuiltInvertedIndex extends PrebuiltIndex {
   private static final TypeReference<List<Entry>> ENTRY_LIST_TYPE = new TypeReference<List<Entry>>() {};
-  private static final ObjectMapper MAPPER = JsonMapper.builder()
-      .enable(JsonReadFeature.ALLOW_TRAILING_COMMA)
-      .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
-      .build();
 
   // This is the singleton instance of this class.
   private static PrebuiltInvertedIndex INSTANCE;
 
-  @JsonIgnoreProperties(ignoreUnknown = true)
-  public static class Entry {
-    @JsonProperty("name")
-    public String name;
-
-    @JsonProperty("type")
-    public String type;
-
-    @JsonProperty("corpus_index")
-    public String corpusIndex;
-
-    @JsonProperty("description")
-    public String description;
-
-    @JsonProperty("filename")
-    public String filename;
-
-    @JsonProperty("readme")
-    public String readme;
-
-    @JsonProperty("urls")
-    public String[] urls;
-
-    @JsonProperty("md5")
-    public String md5;
-
-    @JsonProperty("compressed_size")
-    public long compressedSize;
-
-    @JsonProperty("total_terms")
-    public long totalTerms;
-
-    @JsonProperty("documents")
-    public int documents;
-
-    @JsonProperty("unique_terms")
-    public long uniqueTerms;
-  }
+  public static class Entry extends PrebuiltIndex.Entry {}
 
   private final List<Entry> entries;
   private final Map<String, Entry> byName;
 
   private PrebuiltInvertedIndex() {
-    List<Entry> loadedEntries = new ArrayList<>();
-    ClassLoader classLoader = PrebuiltInvertedIndex.class.getClassLoader();
-
-    try {
-      Enumeration<URL> urls = classLoader.getResources(RESOURCE_DIR);
-      while (urls.hasMoreElements()) {
-        URL url = urls.nextElement();
-        String protocol = url.getProtocol();
-        if ("file".equals(protocol)) {
-          Path dir = Paths.get(url.toURI());
-          try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir, "*" + RESOURCE_SUFFIX)) {
-            for (Path path : stream) {
-              try (InputStream input = Files.newInputStream(path)) {
-                List<Entry> entriesInFile = MAPPER.readValue(input, ENTRY_LIST_TYPE);
-                for (Entry entry : entriesInFile) {
-                  if (entry != null && "inverted".equals(entry.type)) {
-                    loadedEntries.add(entry);
-                  }
-                }
-              }
-            }
-          }
-        } else if ("jar".equals(protocol)) {
-          JarURLConnection connection = (JarURLConnection) url.openConnection();
-          String entryPrefix = connection.getEntryName();
-          if (entryPrefix == null) {
-            entryPrefix = RESOURCE_DIR;
-          }
-          if (!entryPrefix.endsWith("/")) {
-            entryPrefix = entryPrefix + "/";
-          }
-          try (JarFile jarFile = connection.getJarFile()) {
-            Enumeration<JarEntry> jarEntries = jarFile.entries();
-            while (jarEntries.hasMoreElements()) {
-              JarEntry jarEntry = jarEntries.nextElement();
-              if (jarEntry.isDirectory()) {
-                continue;
-              }
-              String name = jarEntry.getName();
-              if (!name.startsWith(entryPrefix) || !name.endsWith(RESOURCE_SUFFIX)) {
-                continue;
-              }
-              try (InputStream input = jarFile.getInputStream(jarEntry)) {
-                List<Entry> entriesInFile = MAPPER.readValue(input, ENTRY_LIST_TYPE);
-                for (Entry entry : entriesInFile) {
-                  if (entry != null && "inverted".equals(entry.type)) {
-                    loadedEntries.add(entry);
-                  }
-                }
-              }
-            }
-          }
-        } else {
-          throw new IllegalStateException("Unsupported resource protocol: " + protocol);
-        }
-      }
-    } catch (IOException | URISyntaxException e) {
-      throw new IllegalStateException("Failed to read resources under " + RESOURCE_DIR, e);
-    }
-
+    List<Entry> loadedEntries = loadEntries("inverted", ENTRY_LIST_TYPE, PrebuiltInvertedIndex.class);
     entries = Collections.unmodifiableList(loadedEntries);
 
     Map<String, Entry> map = new HashMap<>(Math.max(16, entries.size() * 2));
