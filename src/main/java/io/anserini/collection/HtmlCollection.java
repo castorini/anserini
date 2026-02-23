fe451aa2bebce2cdcc5475f88d2883863307a9e2
lintool <jimmylin@uwaterloo.ca>
2026-02-23 15:30:16 -0700

    Initial commit of code for reading from S3.

--- src/main/java/io/anserini/collection/HtmlCollection.java
+++ src/main/java/io/anserini/collection/HtmlCollection.java
@@ -26,6 +26,7 @@ import org.apache.logging.log4j.Logger;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
@@ -58,23 +59,39 @@ public class HtmlCollection extends DocumentCollection<HtmlCollection.Document> 
     return new Segment(bufferedReader);
   }
 
+  public FileSegment<HtmlCollection.Document> createFileSegment(InputStream inputStream, String segmentName) throws IOException {
+    return new Segment(inputStream, segmentName);
+  }
+
   /**
    * An individual file in {@code HtmlCollection}.
    */
   public static class Segment extends FileSegment<HtmlCollection.Document> {
+    private static final String HTML_EXTENSION = ".html";
+    private static final String TGZ_EXTENSION = ".tgz";
+    private static final String TAR_GZ_EXTENSION = ".tar.gz";
+    private static final String STREAM_SEGMENT_NAME = "s3-object";
+
     private TarArchiveInputStream inputStream = null;
+    private final boolean archiveSegment;
+    private final String segmentName;
     private ArchiveEntry nextEntry = null;
 
     public Segment(Path path) throws IOException {
       super(path);
       this.bufferedReader = null;
-      if (path.toString().endsWith(".tgz") || path.toString().endsWith(".tar.gz")) {
+      String pathString = path.toString();
+      this.archiveSegment = pathString.endsWith(TGZ_EXTENSION) || pathString.endsWith(TAR_GZ_EXTENSION);
+      this.segmentName = path.getFileName() == null ? pathString : path.getFileName().toString();
+      if (archiveSegment) {
         inputStream = new TarArchiveInputStream(new GzipCompressorInputStream(new FileInputStream(path.toFile())));
       }
     }
 
     public Segment(BufferedReader bufferedReader) throws IOException {
       super(bufferedReader);
+      this.archiveSegment = true;
+      this.segmentName = STREAM_SEGMENT_NAME;
       inputStream = new TarArchiveInputStream(
         ReaderInputStream.builder()
           .setReader(bufferedReader)
@@ -83,24 +100,45 @@ public class HtmlCollection extends DocumentCollection<HtmlCollection.Document> 
       );
     }
 
+    public Segment(InputStream inputStream, String segmentName) throws IOException {
+      super((BufferedReader) null);
+      this.bufferedReader = null;
+      this.segmentName = segmentName == null ? STREAM_SEGMENT_NAME : segmentName;
+      this.archiveSegment = this.segmentName.endsWith(TGZ_EXTENSION) || this.segmentName.endsWith(TAR_GZ_EXTENSION);
+      if (archiveSegment) {
+        this.inputStream = new TarArchiveInputStream(new GzipCompressorInputStream(inputStream));
+      } else {
+        this.bufferedReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+      }
+    }
+
     @Override
     public void readNext() throws IOException {
       try {
-        if (path.toString().endsWith(".tgz") || path.toString().endsWith(".tar.gz")) {
+        if (archiveSegment) {
           getNextEntry();
           bufferedReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
-          bufferedRecord = new Document(bufferedReader, Paths.get(nextEntry.getName()).getFileName().toString().replaceAll("\\.html$", ""));
+          bufferedRecord = new Document(bufferedReader, stripHtmlExtension(Paths.get(nextEntry.getName()).getFileName().toString()));
         } else {
-          bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(path.toFile()), StandardCharsets.UTF_8));
-          bufferedRecord = new Document(bufferedReader, path.getFileName().toString().replaceAll("\\.html$", ""));
+          if (bufferedReader == null) {
+            bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(path.toFile()), StandardCharsets.UTF_8));
+          }
+          bufferedRecord = new Document(bufferedReader, stripHtmlExtension(segmentName));
           atEOF = true;
         }
       } catch (IOException e1) {
-        if (path.toString().endsWith(".html")) {
+        if (!archiveSegment && segmentName.endsWith(HTML_EXTENSION)) {
           atEOF = true;
         }
         throw e1;
       }
+    }
+
+    private static String stripHtmlExtension(String fileName) {
+      if (fileName.endsWith(HTML_EXTENSION)) {
+        return fileName.substring(0, fileName.length() - HTML_EXTENSION.length());
+      }
+      return fileName;
     }
 
     private void getNextEntry() throws IOException {
